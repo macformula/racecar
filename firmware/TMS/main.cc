585abf9c2b7e6e6cf40ab15ab7b730e263f5f3b3
freerb <59676067+l0ckay@users.noreply.github.com>
2023-11-01 19:09:46 -0400

    blinky script

--- firmware/TMS/main.cc
+++ firmware/TMS/main.cc
@@ -1,19 +1,82 @@
 #include "input/digital_input_pin.h"
+#include "output/digital_output_pin.h"
 #include "gpio.h"
 
-using mypin = mcal::gpio::pin<mcal::gpio::port::a,
-							  mcal::gpio::pin_num::p10,
-							  mcal::gpio::mode::input,
-							  mcal::gpio::pull::up>;
+// remove this region later
+#pragma region 
+#include "main.h"
 
-infra::input::DigitalInputPin<mypin> pin;
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
+}
+#pragma endregion
+
+using led_pin = mcal::gpio::pin<mcal::gpio::port::b,
+							  mcal::gpio::pin_num::p7,
+							  mcal::gpio::mode::output_pp,
+							  mcal::gpio::pull::nopull>;
+
+using btn_pin = mcal::gpio::pin<mcal::gpio::port::c,
+								mcal::gpio::pin_num::p13,
+								mcal::gpio::mode::input,
+								mcal::gpio::pull::down>;
+
+infra::output::DigitalOutputPin<led_pin> led;
+infra::input::DigitalInputPin<btn_pin> btn;
 
 int main(void) {
+    HAL_Init();
+    SystemClock_Config();
 
-	mypin::init();
+	led.Init();
+	btn.init();
 
-	pin.read();
-	// mypin::set();
+	// uint32_t period = 200000;
+	// volatile uint32_t counter = period;
+
+	while (true) {
+		
+		led.Set(btn.read());
+
+	}
 	
 	return 0;
 }
