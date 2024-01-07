24bd5e592e11f7dd8664730af9fa01cd0709a739
freerb <59676067+l0ckay@users.noreply.github.com>
2024-01-06 19:08:51 -0500

    removes old build files and moves platform specific cmake fiels to mcal/

---
+++ firmware/mcal/stm32f767/PostBuild.cmake
@@ -0,0 +1,11 @@
+# Blake Freer
+# December 25, 2023 (Merry Christmas!)
+
+add_custom_command(
+	TARGET main
+	POST_BUILD
+	COMMAND ${CMAKE_SIZE} $<TARGET_FILE:main>
+	COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:main> main.hex
+	COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:main> main.bin
+	# COMMAND ${CMAKE_OBJDUMP} -D -C $<TARGET_FILE:main> > main.s
+)
