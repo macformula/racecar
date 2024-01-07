9bf96a726498c306a20ab084154b195946b1fab7
freerb <59676067+l0ckay@users.noreply.github.com>
2024-01-06 19:49:49 -0500

    new build process

--- firmware/TMS/app.h
+++ firmware/TMS/app.h
@@ -5,63 +5,60 @@
 #ifndef TMS_APP_H_
 #define TMS_APP_H_
 
+#include <shared/util/data_structures/lookup_table.h>
+
 #include <cstdint>
 #include <string>
 
-#include <shared/util/data_structures/lookup_table.h>
-
-#include "shared/periph/gpio.h"
 #include "shared/periph/adc.h"
+#include "shared/periph/gpio.h"
 #include "shared/periph/pwm.h"
 
 /***************************************************************
-	Function definititions (for those that need an mcal binding)
+    Function definititions (for those that need an mcal binding)
 ***************************************************************/
 
 void Initialize();
 void Log(std::string message);
 
 /***************************************************************
-	App-level objects
+    App-level objects
 ***************************************************************/
 
 template <shared::periph::ADCInput ADCInput>
 class TempSensor {
-
-	using LUT = shared::util::LookupTable;
+    using LUT = shared::util::LookupTable;
 
 private:
-	ADCInput adc_;
-	LUT& adc_to_temp_;
+    ADCInput& adc_;
+    LUT& adc_to_temp_;
 
 public:
-	TempSensor(ADCInput adc, LUT& adc_to_temp) : adc_(adc), adc_to_temp_(adc_to_temp) {}
+    TempSensor(ADCInput& adc, LUT& adc_to_temp)
+        : adc_(adc), adc_to_temp_(adc_to_temp) {}
 
-	float Read() {
-		uint32_t adc_value = adc_.Read();
-		float temperature = adc_to_temp_.Interpolate(float(adc_value));
-		return temperature;
-	}
+    float Read() {
+        uint32_t adc_value = adc_.Read();
+        float temperature = adc_to_temp_.Interpolate(float(adc_value));
+        return temperature;
+    }
 };
 
 template <shared::periph::PWMOutput PWMOutput>
 class FanContoller {
-
 private:
-	PWMOutput pwm_;
+    PWMOutput& pwm_;
 
 public:
-	FanContoller(PWMOutput pwm) : pwm_(pwm) {}
+    FanContoller(PWMOutput& pwm) : pwm_(pwm) {}
 
-	void Set(float value) {
-		pwm_.SetDutyCycle(value);
-	}
+    void Set(float value) {
+        pwm_.SetDutyCycle(value);
+    }
 
-	void StartPWM() {
-		pwm_.Start();
-	}
-
+    void StartPWM() {
+        pwm_.Start();
+    }
 };
-
 
 #endif
