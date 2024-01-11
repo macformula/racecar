1c8c701c5afb650ca2be000cabd5690b8d277422
freerb <59676067+l0ckay@users.noreply.github.com>
2024-01-10 21:16:51 -0500

    removes old TMS code

--- firmware/projects/TMS_old/platforms/stm32f767/bindings.h
+++
@@ -1,7 +0,0 @@
-/// @author Blake Freer
-/// @date 2023-11-08
-/// @brief Includes the peripheral implementation files so that main.cc has the
-/// necessary types to specify the App-level object template parameters
-
-#include "mcal/stm32f767/periph/adc.h"
-#include "mcal/stm32f767/periph/pwm.h"
--- firmware/projects/TMS_old/platforms/windows/bindings.h
+++
@@ -1,5 +0,0 @@
-/// @author Blake Freer
-/// @date 2023-12-10
-
-#include "mcal/windows/periph/adc.h"
-#include "mcal/windows/periph/pwm.h"
