a3c2bff166a3e003fff53042bf575f68f9efa0c5
Matteo Tullo <tullom@mcmaster.ca>
2024-01-20 11:11:48 -0500

    two LEDs blinking in parallel rtos

---
+++ firmware/infra/input/digital_input_pin.h
@@ -0,0 +1,32 @@
+/// @author Blake Freer
+/// @date 2023-10-17
+
+#ifndef INFRA_INPUT_DIGITAL_INPUT_PIN_H_
+#define INFRA_INPUT_DIGITAL_INPUT_PIN_H_
+
+#include "input/digital_input_base.h"
+#include "mcal_type.h"
+#include "gpio.h"
+
+namespace infra {
+namespace input {
+
+
+/// @brief Digital input from a physical pin
+/// @tparam pin Must be a class conforming to the `GpioPin` concept
+template <mcal::gpio::GpioPin pin>
+class DigitalInputPin : public DigitalInputBase {
+
+public:
+	void Init() {
+		pin::Init();
+	}
+    bool Read() noexcept override  {
+		return pin::Read();
+	}
+};
+
+}  // namespace input
+}  // namespace mcal
+
+#endif
