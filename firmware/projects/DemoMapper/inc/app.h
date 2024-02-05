3f034b81e4a3f14110e91b9b03001b493fcc03c3
freerb <59676067+l0ckay@users.noreply.github.com>
2024-02-04 19:25:02 -0500

    removes peripherals from demomapper, they were unused

--- firmware/projects/DemoMapper/inc/app.h
+++ firmware/projects/DemoMapper/inc/app.h
@@ -1,36 +1,3 @@
 /// @author Blake Freer
-/// @date 2023-12-25
-
-#include "shared/periph/gpio.h"
-
-class Button {
-public:
-    Button(shared::periph::DigitalInput& di) : di_(di){};
-
-    bool Read() {
-        return di_.Read();
-    }
-
-private:
-    shared::periph::DigitalInput& di_;
-};
-
-class Indicator {
-private:
-public:
-    shared::periph::DigitalOutput& dig_output_;
-    Indicator(shared::periph::DigitalOutput& dig_output)
-        : dig_output_(dig_output) {}
-
-    void Set(bool value) {
-        dig_output_.Set(value);
-    }
-
-    void High() {
-        dig_output_.SetHigh();
-    }
-
-    void Low() {
-        dig_output_.SetLow();
-    }
-};
+/// @date 2024-02-04
+/// @note No app objects are required for this demo
