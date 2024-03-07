3487c1d9d3f0de1a32e087a37cc87ce7f268f3b7
BlakeFreer <freerb@mcmaster.ca>
2024-03-06 23:11:15 -0500

    adds LV controller by copying DemoProject

---
+++ firmware/projects/LVController/inc/app.h
@@ -0,0 +1,36 @@
+/// @author Blake Freer
+/// @date 2023-12-25
+
+#include "shared/periph/gpio.h"
+
+class Button {
+private:
+    shared::periph::DigitalInput& di_;
+
+public:
+    Button(shared::periph::DigitalInput& di) : di_(di){};
+
+    bool Read() {
+        return di_.Read();
+    }
+};
+
+class Indicator {
+private:
+public:
+    shared::periph::DigitalOutput& dig_output_;
+    Indicator(shared::periph::DigitalOutput& dig_output)
+        : dig_output_(dig_output) {}
+
+    void Set(bool value) {
+        dig_output_.Set(value);
+    }
+
+    void High() {
+        dig_output_.SetHigh();
+    }
+
+    void Low() {
+        dig_output_.SetLow();
+    }
+};
---
+++ firmware/projects/LVController/platforms/windows/bindings.cc
@@ -0,0 +1,23 @@
+/// @author Blake Freer
+/// @date 2023-12-25
+
+#include <iostream>
+
+#include "mcal/windows/periph/gpio.h"
+#include "shared/periph/gpio.h"
+
+namespace mcal {
+periph::DigitalInput button_di{"Button"};
+periph::DigitalOutput indicator_do{"Indicator"};
+}  // namespace mcal
+
+namespace bindings {
+
+const shared::periph::DigitalInput& button_di = mcal::button_di;
+const shared::periph::DigitalOutput& indicator_do = mcal::indicator_do;
+
+void Initialize() {
+    std::cout << "Initializing Windows..." << std::endl;
+}
+
+}  // namespace bindings
