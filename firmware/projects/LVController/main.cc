06c0d9b2bbe012b888d2c16e7a2d248e0441a89a
BlakeFreer <freerb@mcmaster.ca>
2024-03-06 23:11:15 -0500

    adds LV controller by copying DemoProject

---
+++ firmware/projects/LVController/main.cc
@@ -0,0 +1,26 @@
+/// @author Blake Freer
+/// @date 2023-12-25
+
+#include "app.h"
+
+namespace bindings {
+extern shared::periph::DigitalInput& button_di;
+extern shared::periph::DigitalOutput& indicator_do;
+extern void Initialize();
+}  // namespace bindings
+
+Button button{bindings::button_di};
+Indicator indicator{bindings::indicator_do};
+
+bool btn_value;
+
+int main(void) {
+    bindings::Initialize();
+
+    while (1) {
+        btn_value = button.Read();
+        indicator.Set(btn_value);
+    }
+
+    return 0;
+}
---
+++ firmware/projects/LVController/platforms/stm32f767/bindings.cc
@@ -0,0 +1,35 @@
+/// @author Blake Freer
+/// @date 2023-12-25
+
+// cubemx files
+#include "gpio.h"
+#include "main.h"
+
+// fw includes
+#include "mcal/stm32f767/periph/gpio.h"
+#include "shared/periph/gpio.h"
+
+extern "C" {
+/**
+ * This requires extern since it is not declared in a header, only defined
+ * in cubemx/../main.c
+ */
+void SystemClock_Config();
+}
+
+namespace mcal {
+periph::DigitalInput button_di{ButtonPin_GPIO_Port, ButtonPin_Pin};
+periph::DigitalOutput indicator_do{LedPin_GPIO_Port, LedPin_Pin};
+}  // namespace mcal
+
+namespace bindings {
+const shared::periph::DigitalInput& button_di = mcal::button_di;
+const shared::periph::DigitalOutput& indicator_do = mcal::indicator_do;
+
+void Initialize() {
+    SystemClock_Config();
+    HAL_Init();
+    MX_GPIO_Init();
+}
+
+}  // namespace bindings
