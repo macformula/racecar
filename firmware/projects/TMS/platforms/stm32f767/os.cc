98810d0cc497aaef360b51e7e148c524cfc423a3
Matteo Tullo <tullom@mcmaster.ca>
2024-03-10 03:08:34 -0400

    dirty

---
+++ firmware/projects/TMS/platforms/stm32f767/os.cc
@@ -0,0 +1,34 @@
+/// @author Matteo Tullo
+/// @date 2024-02-24
+
+#include "os.h"
+#include "cmsis_os2.h"
+
+extern "C" {
+/**
+ * This requires extern since it is not declared in a header, only defined
+ * in cubemx/../freertos.c
+ */
+void MX_FREERTOS_Init(void);
+// extern osSemaphoreId_t testSemaphoreHandle;
+// extern osSemaphoreId_t testBinarySemaphoreHandle;
+// extern osMutexId_t testMutexHandle;
+}
+
+namespace os {
+// mcal::os::Semaphore sem_test{testSemaphoreHandle};
+mcal::os::Semaphore sem_test{&testBinarySemaphoreHandle};
+mcal::os::Mutex mutex_test{&testMutexHandle};
+
+    void InitializeKernel(void) {
+        // TODO: Implement error handling
+        osKernelInitialize();
+        MX_FREERTOS_Init();
+    }
+
+    void StartKernel(void) {
+        // TODO: Implement error handling
+        osKernelStart();
+    }
+
+} // namespace os
