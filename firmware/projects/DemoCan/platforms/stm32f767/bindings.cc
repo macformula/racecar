92557754bab0619af0ff1f2cde6838f907ec3e9a
clara <clarawong20@gmail.com>
2024-03-21 00:57:50 -0400

    Created a child class of CanBase for stm32

---
+++ firmware/projects/DemoCan/platforms/stm32f767/bindings.cc
@@ -0,0 +1,23 @@
+/// @author Clara Wong
+/// @date 2024-03-20
+
+#include "bindings.h"
+#include <iostream>
+
+
+extern "C" {
+void SystemClock_Config();
+}
+namespace bindings {
+
+mcal::periph::CanBase veh_can_base{"vcan0"};
+
+void Initialize() {
+    SystemClock_Config();
+    HAL_Init();
+    MX_CAN_Init();
+    veh_can_base.Setup();
+    std::cout << "Initializing STM32..." << std::endl;
+}
+
+} // namespace bindings
---
+++ firmware/projects/DemoCan/platforms/stm32f767/bindings.h
@@ -0,0 +1,5 @@
+/// @author Clara Wong
+/// @date 2024-03-20
+
+#include "mcal/raspi/periph/can.h"
+#include "shared/comms/can/msg_registry.h"
