4de4ec6be542ccbb700725bc0287029c45ca790b
BlakeFreer <freerb@mcmaster.ca>
2024-03-21 20:17:44 -0400

    writes tests for some mappers

---
+++ firmware/tests/firmware/shared/util/mappers/constant_test.cc
@@ -0,0 +1,24 @@
+/// @author Blake Freer
+/// @date 2024-03-21
+
+#include "shared/util/mappers/constant.h"
+
+#include "gtest/gtest.h"
+
+namespace shared::util {
+
+TEST(ConstantTest, Integer) {
+    EXPECT_EQ(ConstantMap<int>::Evaluate(100, 4), 4);
+}
+
+TEST(ConstantTest, Float) {
+    EXPECT_EQ(ConstantMap<float>::Evaluate(1.5e7f, -0.1f), -0.1f);
+}
+
+TEST(ConstantTest, FloatToInt) {
+    using Cif = ConstantMap<int, float>;  // Template commas in EXPECT_ macro
+                                          // confuse the preprocessor
+    EXPECT_EQ(Cif::Evaluate(-2e9f, 1), 1);
+}
+
+}  // namespace shared::util
