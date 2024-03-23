67441892c852396a01657bbe7898cb435f3f71aa
Michael Nasr <nasrm4@mcmaster.ca>
2024-03-23 02:25:15 -0400

    changing file

--- firmware/format-test.cpp
+++ firmware/format-test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int a=10; int b=20;
-    std::cout <<"a: "<<a<<", b: "<<b<< std::endl;
+                            int a=10; int b=20;
+                            std::cout <<"a: "<<a<<", b: "<<b<< std::endl;
 return 0;
 }
