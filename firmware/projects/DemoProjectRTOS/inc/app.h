da231bdd4296821060a9dc38b0d0d331a2ae05c2
BlakeFreer <freerb@mcmaster.ca>
2024-03-24 20:36:04 -0400

    removes unused imports and fixes private / public section ordering

--- firmware/projects/DemoProjectRTOS/inc/app.h
+++ firmware/projects/DemoProjectRTOS/inc/app.h
@@ -1,29 +1,23 @@
 /// @author Blake Freer
 /// @date 2023-12-25
 
-#include "shared/periph/gpio.h"
 #include "shared/os/semaphore.h"
-#include "shared/os/mutex.h"
-#include "shared/os/fifo.h"
-#include "shared/os/timer.h"
-#include "shared/util/os.h"
+#include "shared/periph/gpio.h"
 
 class Button {
-private:
-    shared::periph::DigitalInput& di_;
-
 public:
     Button(shared::periph::DigitalInput& di) : di_(di){};
 
     bool Read() {
         return di_.Read();
     }
+
+private:
+    shared::periph::DigitalInput& di_;
 };
 
 class Indicator {
-private:
 public:
-    shared::periph::DigitalOutput& dig_output_;
     Indicator(shared::periph::DigitalOutput& dig_output)
         : dig_output_(dig_output) {}
 
@@ -38,4 +32,7 @@ public:
     void Low() {
         dig_output_.SetLow();
     }
+
+private:
+    shared::periph::DigitalOutput& dig_output_;
 };
