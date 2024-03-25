a576c29225fcfaa84ad0012ee5ac9a108a284856
BlakeFreer <freerb@mcmaster.ca>
2024-03-24 21:39:04 -0400

    clang format and styleguide updates

--- firmware/mcal/raspi/Toolchain.cmake
+++ firmware/mcal/raspi/Toolchain.cmake
@@ -1,3 +1,3 @@
 # Samuel Parent
 # January 16, 2024
-# No cmake instructions as the default PC compiler should work.
+# No cmake instructions as the default RASPI compiler should work.
--- firmware/mcal/raspi/periph/can.h
+++ firmware/mcal/raspi/periph/can.h
@@ -1,31 +1,89 @@
 /// @author Samuel Parent
 /// @date 2023-01-12
 
-#ifndef MCAL_RASPI_PERIPH_CAN_H_
-#define MCAL_RASPI_PERIPH_CAN_H_
+#pragma once
 
-#include <cstdint>
-#include <iostream>
-#include <cstdint>
-#include <cstring>
-#include <queue>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <net/if.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <thread>
-#include <unistd.h>
 #include <sys/ioctl.h>
-#include <net/if.h>
-#include <linux/can.h>
-#include <linux/can/raw.h>
+#include <unistd.h>
 
-#include "third-party/etl/include/etl/queue.h"
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <thread>
+
 #include "shared/comms/can/raw_can_msg.h"
 #include "shared/periph/can.h"
+#include "third-party/etl/include/etl/queue.h"
 
 namespace mcal::periph {
 
 class CanBase : public shared::periph::CanBase {
+public:
+    CanBase(std::string can_iface) : iface_(can_iface){};
+
+    void Setup() {
+        // Create a socket
+        if ((sock_ = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+            perror("Error creating socket");
+            return;
+        }
+
+        // Specify the can interface
+        strncpy(ifreq_.ifr_name, iface_.c_str(), sizeof(ifreq_.ifr_name) - 1);
+
+        std::cout << "can interface: " << iface_ << std::endl;
+
+        ioctl(sock_, SIOCGIFINDEX, &ifreq_);
+
+        // Bind the socket to the can interface
+        sock_addr_.can_family = AF_CAN;
+        sock_addr_.can_ifindex = ifreq_.ifr_ifindex;
+        if (bind(sock_, (struct sockaddr*)&sock_addr_, sizeof(sock_addr_)) <
+            0) {
+            perror("Error binding socket");
+            close(sock_);
+        }
+
+        reader_thread = std::thread(&CanBase::StartReading, this);
+
+        return;
+    }
+
+    void Send(const shared::comms::can::RawCanMsg& can_tx_msg) {
+        frame_.can_id = can_tx_msg.header.id;
+        frame_.can_dlc = can_tx_msg.header.data_len;
+        memcpy((uint8_t*)frame_.data, can_tx_msg.data, 8);
+
+        ssize_t bytes_written = write(sock_, &frame_, sizeof(struct can_frame));
+        if (bytes_written != sizeof(struct can_frame)) {
+            perror("Error writing to socket");
+            close(sock_);
+        }
+
+        return;
+    }
+
+    void ReadQueue(shared::comms::can::RawCanMsg can_rx_msgs[], size_t len) {
+        uint16_t msg_idx = 0;
+        {
+            std::lock_guard<std::mutex> lock(queue_mtx_);
+            while (!can_queue_.empty() && (msg_idx < len)) {
+                can_rx_msgs[msg_idx] = can_queue_.front();
+                can_queue_.pop();
+                msg_idx++;
+            }
+        }
+
+        return;
+    }
+
 private:
     using RawCanMsg = shared::comms::can::RawCanMsg;
 
@@ -39,13 +97,14 @@ private:
 
     std::queue<RawCanMsg> can_queue_;
     std::mutex queue_mtx_;
-    std::thread readerThread;
+    std::thread reader_thread;
 
     void StartReading() {
         struct can_frame frame;
 
-        while(true) {
-            ssize_t bytesRead = recv(sock_, &frame, sizeof(struct can_frame), 0);
+        while (true) {
+            ssize_t bytesRead =
+                recv(sock_, &frame, sizeof(struct can_frame), 0);
             if (bytesRead == -1) {
                 perror("Error reading from CAN socket");
                 continue;
@@ -56,8 +115,9 @@ private:
             rawMsg.header.data_len = frame.can_dlc;
             rawMsg.header.is_extended_frame = frame.can_id & CAN_EFF_FLAG;
 
-            std::cout << "Can base frame " << std::hex << rawMsg.header.id << " data len " << rawMsg.header.data_len << std::endl;
- 
+            std::cout << "Can base frame " << std::hex << rawMsg.header.id
+                      << " data len " << rawMsg.header.data_len << std::endl;
+
             std::copy(frame.data, frame.data + kMaxMsgBytes, rawMsg.data);
 
             // Add to the queue
@@ -67,64 +127,6 @@ private:
             }
         }
     }
-public:
-	CanBase(std::string can_iface) : iface_(can_iface) {};
-
-    void Setup() {
-        // Create a socket
-        if ((sock_ = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-            perror("Error creating socket");
-            return;
-        }
-
-        // Specify the can interface
-        strncpy(ifreq_.ifr_name, iface_.c_str(), sizeof(ifreq_.ifr_name) - 1);
-
-        std::cout << "can interface: " << iface_ << std::endl;
-        
-        ioctl(sock_, SIOCGIFINDEX, &ifreq_);
-
-        // Bind the socket to the can interface
-        sock_addr_.can_family = AF_CAN;
-        sock_addr_.can_ifindex = ifreq_.ifr_ifindex;
-        if (bind(sock_, (struct sockaddr *)&sock_addr_, sizeof(sock_addr_)) < 0) {
-            perror("Error binding socket");
-            close(sock_);
-        }
-
-        readerThread = std::thread(&CanBase::StartReading, this);
-
-        return;
-    }
-    
-	void Send(const shared::comms::can::RawCanMsg& can_tx_msg) {
-        frame_.can_id = can_tx_msg.header.id;
-        frame_.can_dlc = can_tx_msg.header.data_len;
-        memcpy((uint8_t *)frame_.data, can_tx_msg.data, 8);
-
-        if (write(sock_, &frame_, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
-            perror("Error writing to socket");
-            close(sock_);
-        }            
-        
-        return;
-    }
-
-    void ReadQueue(shared::comms::can::RawCanMsg can_rx_msgs[], size_t len) {
-        uint16_t msg_idx = 0;
-        {
-            std::lock_guard<std::mutex> lock(queue_mtx_);
-            while(!can_queue_.empty() && (msg_idx < len)) {
-                can_rx_msgs[msg_idx] = can_queue_.front();
-                can_queue_.pop();
-                msg_idx++;
-            }
-        }
-
-        return;
-    }
 };
 
-} // namespace mcal::periph
-
-#endif
+}  // namespace mcal::periph
