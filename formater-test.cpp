c474ae78640fd1ae358636f84a83e3a540e9b46e
Michael Nasr <nasrm4@mcmaster.ca>
2024-03-27 18:22:49 -0400

    new file but good formatting test

--- formater-test.cpp
+++ formater-test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int a=10; 
-    int b=20;
-    std::cout <<"a: "<<a<<", b: "<<b<< std::endl;
+    int a = 10;
+    int b = 20;
+    std::cout << "a: " << a << ", b: " << b << std::endl;
     return 0;
 }
