e9c98936ceb4a15832d59018ff56af09eec29e3a
BlakeFreer <freerb@mcmaster.ca>
2024-04-07 12:49:29 -0400

    renames stm binary

--- firmware/mcal/stm32f767/PostBuild.cmake
+++ firmware/mcal/stm32f767/PostBuild.cmake
@@ -5,7 +5,7 @@ add_custom_command(
 	TARGET main
 	POST_BUILD
 	COMMAND ${CMAKE_SIZE} $<TARGET_FILE:main>
-	COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:main> main.hex
-	COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:main> main.bin
+	COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:main> ${PROJECT}.hex
+	COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:main> ${PROJECT}.bin
 	# COMMAND ${CMAKE_OBJDUMP} -D -C $<TARGET_FILE:main> > main.s
 )
