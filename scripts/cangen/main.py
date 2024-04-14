ce3846558a82c9b909ab5903373117c0a3ebbce0
Blake Freer <59676067+BlakeFreer@users.noreply.github.com>
2024-04-13 23:35:54 -0400

    Fix cmake (#103)
    * adds some cmake to rpi
    
    * works on can
    
    * windows builds can

--- scripts/cangen/main.py
+++ scripts/cangen/main.py
@@ -15,10 +15,9 @@ SCRIPTS_TO_TEMPLATES_DIR = "templates"
 CONFIG_FILE_PATH = "config.yaml"
 CAN_MESSAGES_TEMPLATE_FILENAME = "can_messages.h.jinja2"
 MSG_REGISTRY_FILENAME = "msg_registry.h.jinja2"
-
+OUTPUT_DIR = "generated/can"
 
 logging.basicConfig(level="INFO", format="%(levelname)-8s| (%(name)s) %(message)s")
-
 
 def read_yaml_file(yaml_file):
     with open(yaml_file, "r") as file:
@@ -27,7 +26,7 @@ def read_yaml_file(yaml_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DBC to C code generator")
-    parser.add_argument("--project", type=str, help="Name of the project")
+    parser.add_argument("--project", type=str, required=True, help="Name of the project")
 
     try:
         args = parser.parse_args()
@@ -46,7 +45,7 @@ if __name__ == "__main__":
     our_node = config["canGen"]["ourNode"].upper()
     bus_name = config["canGen"]["busName"].capitalize()
     dbc_files = config["canGen"]["dbcFiles"]
-    output_dir = config["canGen"]["outputDir"]
+    output_dir = OUTPUT_DIR
 
     dbc_file_paths = [
         os.path.join(this_script_dir, SCRIPTS_TO_DBCS, dbc) for dbc in dbc_files
