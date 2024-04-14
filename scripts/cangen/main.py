b6dea23431f3c0e8dc84fa789dc045b563b2ed4c
BlakeFreer <freerb@mcmaster.ca>
2024-04-14 15:56:39 -0400

    refactor(can-gen): :recycle: Improve CAN generation and build support.
    Convert ETL from an include path to a proper target in CMake. Refactor cangen Python code to use a functional paradigm and be more "Pythonic"

--- scripts/cangen/main.py
+++ scripts/cangen/main.py
@@ -3,69 +3,71 @@ Author: Samuel Parent
 Date: 2024-04-13
 """
 
-from can_generator import CanGenerator
-import yaml
 import argparse
 import logging
 import os
 
-SCRIPTS_TO_PROJECTS_DIR = "../../firmware/projects"
-SCRIPTS_TO_DBCS = "../../firmware/dbcs"
-SCRIPTS_TO_TEMPLATES_DIR = "templates"
-CONFIG_FILE_PATH = "config.yaml"
-CAN_MESSAGES_TEMPLATE_FILENAME = "can_messages.h.jinja2"
-MSG_REGISTRY_FILENAME = "msg_registry.h.jinja2"
+import can_generator
+import yaml
+
+# Generate a set of directory paths, all based on this file's location
+DIR_THIS_FILE = os.path.abspath(os.path.dirname(__file__))
+
+DIR_FIRMWARE = os.path.join(DIR_THIS_FILE, os.pardir, os.pardir, "firmware")
+DIR_PROJECTS = os.path.join(DIR_FIRMWARE, "projects")
+DIR_DBCS = os.path.join(DIR_FIRMWARE, "dbcs")
+
+CONFIG_FILE_NAME = "config.yaml"
 OUTPUT_DIR = "generated/can"
+
+DIR_TEMPLATES = os.path.join(DIR_THIS_FILE, "templates")
+CAN_MESSAGES_TEMPLATE_FILENAME = "can_messages.h.jinja2"
+MSG_REGISTRY_TEMPLATE_FILENAME = "msg_registry.h.jinja2"
+
 
 logging.basicConfig(level="INFO", format="%(levelname)-8s| (%(name)s) %(message)s")
 
-def read_yaml_file(yaml_file):
-    with open(yaml_file, "r") as file:
-        return yaml.safe_load(file)
+
+def parse():
+    parser = argparse.ArgumentParser(description="DBC to C code generator")
+    parser.add_argument(
+        "--project", type=str, required=True, help="Name of the project"
+    )
+
+    # If parsing fails (ex incorrect or no arguments provided) then this exits with
+    # code 2.
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="DBC to C code generator")
-    parser.add_argument("--project", type=str, required=True, help="Name of the project")
-
-    try:
-        args = parser.parse_args()
-    except argparse.ArgumentError:
-        parser.print_help()
-        exit(1)
-
-    project_dir = args.project
-    this_script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Change directory to the project folder
-    os.chdir(os.path.join(this_script_dir, SCRIPTS_TO_PROJECTS_DIR, project_dir))
+    args = parse()
+    project_folder_name = args.project
+    os.chdir(os.path.join(DIR_PROJECTS, project_folder_name))
 
-    config = read_yaml_file(CONFIG_FILE_PATH)
+    # Read & Parse the config file
+    with open(CONFIG_FILE_NAME, "r") as file:
+        config = yaml.safe_load(file)
 
     our_node = config["canGen"]["ourNode"].upper()
     bus_name = config["canGen"]["busName"].capitalize()
     dbc_files = config["canGen"]["dbcFiles"]
-    output_dir = OUTPUT_DIR
 
-    dbc_file_paths = [
-        os.path.join(this_script_dir, SCRIPTS_TO_DBCS, dbc) for dbc in dbc_files
-    ]
+    dbc_file_paths = [os.path.join(DIR_DBCS, dbc) for dbc in dbc_files]
 
-    can_msg_template_path = os.path.join(
-        this_script_dir, SCRIPTS_TO_TEMPLATES_DIR, CAN_MESSAGES_TEMPLATE_FILENAME
+    can_messages_template_path = os.path.join(
+        DIR_TEMPLATES, CAN_MESSAGES_TEMPLATE_FILENAME
     )
     msg_registry_template_path = os.path.join(
-        this_script_dir, SCRIPTS_TO_TEMPLATES_DIR, MSG_REGISTRY_FILENAME
-    )
-
-    can_generator = CanGenerator(
-        can_messages_template_path=can_msg_template_path,
-        msg_registry_template_path=msg_registry_template_path,
+        DIR_TEMPLATES, MSG_REGISTRY_TEMPLATE_FILENAME
     )
 
     can_generator.generate_code(
-        dbc_files=dbc_file_paths,
-        our_node=our_node,
-        bus_name=bus_name,
-        output_dir=output_dir,
+        dbc_file_paths,
+        our_node,
+        bus_name,
+        OUTPUT_DIR,
+        can_messages_template_path,
+        msg_registry_template_path,
     )
