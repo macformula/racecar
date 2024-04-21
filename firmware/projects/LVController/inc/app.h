3d10c1b498a47fec8c6255865807eebf2958e533
BlakeFreer <freerb@mcmaster.ca>
2024-04-21 18:13:05 -0400

    LV controller gpio

--- firmware/projects/LVController/inc/app.h
+++ firmware/projects/LVController/inc/app.h
@@ -1,36 +1,128 @@
 /// @author Blake Freer
 /// @date 2023-12-25
 
+#include <cmath>
+
 #include "shared/periph/gpio.h"
+#include "shared/periph/pwm.h"
+#include "shared/util/mappers/clamper.h"
+#include "shared/util/mappers/mapper.h"
 
-class Button {
-private:
-    shared::periph::DigitalInput& di_;
-
+/**
+ * @brief A Subsytem which can be enabled / disabled.
+ */
+class Subsystem {
 public:
-    Button(shared::periph::DigitalInput& di) : di_(di){};
+    /**
+     * @brief Construct a new Subsystem object
+     *
+     * @param enable_output Digital Output which enables the subsystem.
+     * @param invert_output Set to true for inverse logic (output low =
+     * enabled).
+     */
+    Subsystem(shared::periph::DigitalOutput& enable_output, bool invert_output)
+        : enable_output_(enable_output), high_is_enable_(!invert_output) {}
 
-    bool Read() {
-        return di_.Read();
+    inline void Enable() const {
+        enable_output_.Set(high_is_enable_);
     }
+
+    inline void Disable() const {
+        enable_output_.Set(!high_is_enable_);
+    }
+
+private:
+    shared::periph::DigitalOutput& enable_output_;
+    bool high_is_enable_;
 };
 
 class Indicator {
-private:
 public:
-    shared::periph::DigitalOutput& dig_output_;
-    Indicator(shared::periph::DigitalOutput& dig_output)
-        : dig_output_(dig_output) {}
+    Indicator(shared::periph::DigitalOutput& output, bool invert_output)
+        : output_(output), high_is_on_(!invert_output) {}
 
-    void Set(bool value) {
-        dig_output_.Set(value);
+    inline void TurnOn() {
+        output_.Set(high_is_on_);
     }
 
-    void High() {
-        dig_output_.SetHigh();
+    inline void TurnOff() {
+        output_.Set(!high_is_on_);
     }
 
-    void Low() {
-        dig_output_.SetLow();
+private:
+    shared::periph::DigitalOutput& output_;
+    bool high_is_on_;
+};
+
+class Fan : public Subsystem {
+public:
+    Fan(shared::periph::DigitalOutput& enable_output, bool invert_enable,
+        shared::periph::PWMOutput& pwm_output,
+        shared::util::Mapper<float>& power_to_duty)
+        : Subsystem(enable_output, invert_enable),
+          pwm_output_(pwm_output),
+          power_to_duty_(power_to_duty) {}
+
+    void Enable() {
+        SetPower(0.0f);
+        Subsystem::Enable();
     }
+
+    void Disable() {
+        SetPower(0.0f);
+        Subsystem::Disable();
+    }
+
+    void SetTargetPower(float power) {
+        target_duty_ = power_to_duty_.Evaluate(power);
+    }
+
+    void Update() const {
+        float current_duty = pwm_output_.GetDutyCycle();
+        float duty_step = shared::util::Clamper<float>::Evaluate(
+            target_duty_ - current_duty, -duty_step_size_, +duty_step_size_);
+        pwm_output_.SetDutyCycle(current_duty + duty_step);
+    }
+
+    bool IsAtTarget() {
+        float error = std::abs(pwm_output_.GetDutyCycle() - target_duty_);
+        return error < 0.1f;
+    }
+
+private:
+    shared::periph::PWMOutput& pwm_output_;
+    shared::util::Mapper<float>& power_to_duty_;
+    float target_duty_ = 0.0f;
+    float duty_step_size_ = 1.0f;
+
+    void SetPower(float power) {
+        pwm_output_.SetDutyCycle(power_to_duty_.Evaluate(power));
+    }
+};
+
+class Status {
+public:
+    Status(shared::periph::DigitalInput& input, bool valid_state)
+        : input_(input), valid_state_(valid_state) {}
+
+    bool IsValid() {
+        return input_.Read() == valid_state_;
+    }
+
+private:
+    shared::periph::DigitalInput& input_;
+    bool valid_state_;
+};
+
+class DCDC : public Subsystem {
+public:
+    DCDC(shared::periph::DigitalOutput enable_output, bool invert_enable,
+         shared::periph::DigitalInput valid_input, bool inve);
+    bool IsValid() const {
+        return valid_input_.Read() == valid_state_;
+    }
+
+private:
+    shared::periph::DigitalInput& valid_input_;
+    bool valid_state_;
 };
--- firmware/projects/LVController/platforms/stm32f767/bindings.cc
+++ firmware/projects/LVController/platforms/stm32f767/bindings.cc
@@ -2,12 +2,18 @@
 /// @date 2023-12-25
 
 // cubemx files
-#include "gpio.h"
-#include "main.h"
-
 // fw includes
+
+#include <chrono>
+
+#include "gpio.h"
+#include "inc/app.h"
+#include "main.h"
 #include "mcal/stm32f767/periph/gpio.h"
 #include "shared/periph/gpio.h"
+#include "shared/util/mappers/identity.h"
+#include "shared/util/mappers/mapper.h"
+#include "stm32f7xx_hal.h"
 
 extern "C" {
 /**
@@ -18,13 +24,43 @@ void SystemClock_Config();
 }
 
 namespace mcal {
-periph::DigitalInput button_di{ButtonPin_GPIO_Port, ButtonPin_Pin};
-periph::DigitalOutput indicator_do{LedPin_GPIO_Port, LedPin_Pin};
+using namespace stm32f767::periph;
+
+DigitalOutput tsal_on_enable_en{TSAL_EN_GPIO_Port, TSAL_EN_Pin};
+DigitalOutput raspberry_pi_en{RASPI_EN_GPIO_Port, RASPI_EN_Pin};
+DigitalOutput front_controller_en{FRONT_CONTROLLER_EN_GPIO_Port,
+                                  FRONT_CONTROLLER_EN_Pin};
+DigitalOutput speedgoat_en{SPEEDGOAT_EN_GPIO_Port, SPEEDGOAT_EN_Pin};
+DigitalOutput accumulator_en{ACCUMULATOR_EN_GPIO_Port, ACCUMULATOR_EN_Pin};
+DigitalOutput motor_ctrl_precharge_en{MOTOR_CONTROLLER_PRECHARGE_EN_GPIO_Port,
+                                      MOTOR_CONTROLLER_PRECHARGE_EN_Pin};
+DigitalOutput motor_ctrl_en{MOTOR_CONTROLLER_EN_GPIO_Port,
+                            MOTOR_CONTROLLER_EN_Pin};
+DigitalOutput imu_gps_en{IMU_GPS_EN_GPIO_Port, IMU_GPS_EN_Pin};
+
+DigitalOutput dcdc_en DigitalInput dcdc_valid{MUX_DCDC_VALID_GPIO_Port,
+                                              MUX_DCDC_VALID_Pin};
+
+shared::util::IdentityMap<float> powertrain_fan_power_to_duty{};
+
 }  // namespace mcal
 
 namespace bindings {
-const shared::periph::DigitalInput& button_di = mcal::button_di;
-const shared::periph::DigitalOutput& indicator_do = mcal::indicator_do;
+
+shared::periph::DigitalOutput& tsal_en = mcal::tsal_on_enable_en;
+shared::periph::DigitalOutput& raspberry_pi_en = mcal::raspberry_pi_en;
+shared::periph::DigitalOutput& front_controller_en = mcal::front_controller_en;
+shared::periph::DigitalOutput& speedgoat_en = mcal::speedgoat_en;
+shared::periph::DigitalOutput& accumulator_en = mcal::accumulator_en;
+shared::periph::DigitalOutput& motor_ctrl_precharge_en =
+    mcal::motor_ctrl_precharge_en;
+shared::periph::DigitalOutput& motor_ctrl_en = mcal::motor_ctrl_en;
+shared::periph::DigitalOutput& imu_gps_en = mcal::imu_gps_en;
+
+shared::periph::DigitalOutput& dcdc_en = mcal::dcdc_en;
+Status dcdc_valid{mcal::dcdc_valid, true};
+shared::util::Mapper<float>& powertrain_fan_power_to_duty =
+    mcal::powertrain_fan_power_to_duty;
 
 void Initialize() {
     SystemClock_Config();
@@ -33,3 +69,7 @@ void Initialize() {
 }
 
 }  // namespace bindings
+
+void DelayMS(uint32_t milliseconds) {
+    HAL_Delay(milliseconds);
+}
