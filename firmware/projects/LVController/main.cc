e9d81dab1543c456f2fee65dbd86900035b637a2
BlakeFreer <freerb@mcmaster.ca>
2024-04-21 18:13:05 -0400

    LV controller gpio

--- firmware/projects/LVController/main.cc
+++ firmware/projects/LVController/main.cc
@@ -1,25 +1,101 @@
 /// @author Blake Freer
 /// @date 2023-12-25
 
+#include <cstdint>
+#include <regex>
+
 #include "app.h"
+#include "shared/periph/gpio.h"
+#include "shared/periph/pwm.h"
+#include "shared/util/mappers/mapper.h"
 
 namespace bindings {
-extern shared::periph::DigitalInput& button_di;
-extern shared::periph::DigitalOutput& indicator_do;
+
+extern shared::periph::DigitalOutput& tsal_en;
+extern shared::periph::DigitalOutput& raspberry_pi_en;
+extern shared::periph::DigitalOutput& front_controller_en;
+extern shared::periph::DigitalOutput& speedgoat_en;
+extern shared::periph::DigitalOutput& accumulator_en;
+extern shared::periph::DigitalOutput& motor_ctrl_precharge_en;
+extern shared::periph::DigitalOutput& motor_ctrl_en;
+extern shared::periph::DigitalOutput& imu_gps_en;
+
+extern shared::periph::DigitalOutput& dcdc_en;
+extern shared::periph::DigitalInput& dcdc_valid;
+extern shared::periph::DigitalOutput& dcdc_led_en;
+extern shared::periph::DigitalOutput& powertrain_pump_en;
+extern shared::periph::DigitalOutput& powertrain_fan_en;
+extern shared::periph::PWMOutput& powertrain_fan_pwm;
+extern shared::util::Mapper<float>& powertrain_fan_power_to_duty;
+
+extern Status dcdc_status;
+
 extern void Initialize();
 }  // namespace bindings
 
-Button button{bindings::button_di};
-Indicator indicator{bindings::indicator_do};
+extern void DelayMS(uint32_t ms);
 
-bool btn_value;
+Subsystem tsal{bindings::tsal_en, false};
+Subsystem raspberry_pi{bindings::raspberry_pi_en, false};
+Subsystem front_controller{bindings::front_controller_en, false};
+Subsystem speedgoat{bindings::speedgoat_en, false};
+Subsystem accumulator{bindings::accumulator_en, false};
+Subsystem motor_ctrl_precharge{bindings::motor_ctrl_precharge_en, false};
+Subsystem motor_ctrl{bindings::motor_ctrl_en, false};
+Subsystem imu_gps{bindings::imu_gps_en, false};
+
+Subsystem dcdc{bindings::dcdc_en, false};
+Status dcdc_status{bindings::dcdc_valid, true};
+Subsystem powertrain_pump{bindings::powertrain_pump_en, false};
+Fan powertrain_fan{
+    bindings::powertrain_fan_en,
+    false,
+    bindings::powertrain_fan_pwm,
+    bindings::powertrain_fan_power_to_duty,
+};
+
+Subsystem all_subsystems[] = {
+    tsal,       raspberry_pi, front_controller,
+    speedgoat,  accumulator,  motor_ctrl_precharge,
+    motor_ctrl, imu_gps,      powertrain_fan,
+};
+
+void DoPowerupSequence() {
+    tsal.Enable();
+    DelayMS(50);
+    raspberry_pi.Enable();
+    DelayMS(50);
+    front_controller.Enable();
+    DelayMS(100);
+    speedgoat.Enable();
+    DelayMS(100);
+    motor_ctrl_precharge.Enable();
+    DelayMS(2000);
+    motor_ctrl.Enable();
+    DelayMS(50);
+    motor_ctrl_precharge.Disable();
+    DelayMS(50);
+    imu_gps.Enable();
+}
+
+void DoPowertrainEnableSequence() {
+    dcdc.Enable();
+}
+
+void DoPowertraiDisableSequence() {}
 
 int main(void) {
     bindings::Initialize();
 
-    while (1) {
-        btn_value = button.Read();
-        indicator.Set(btn_value);
+    // Ensure all subsystems are disabled to start.
+    for (auto sys : all_subsystems) {
+        sys.Disable();
+    }
+
+    // Powerup sequence
+    DoPowerupSequence();
+
+    while (true) {
     }
 
     return 0;
--- firmware/projects/LVController/platforms/windows/bindings.cc
+++ firmware/projects/LVController/platforms/windows/bindings.cc
@@ -1,23 +1,54 @@
 /// @author Blake Freer
 /// @date 2023-12-25
 
+#include <chrono>
+#include <cstdint>
 #include <iostream>
+#include <thread>
 
 #include "mcal/windows/periph/gpio.h"
 #include "shared/periph/gpio.h"
+#include "shared/util/mappers/identity.h"
 
 namespace mcal {
-periph::DigitalInput button_di{"Button"};
-periph::DigitalOutput indicator_do{"Indicator"};
+using namespace windows::periph;
+
+DigitalOutput tsal_on_enable_en{"TSAL Enable"};
+DigitalOutput raspberry_pi_en{"RASPI Enable"};
+DigitalOutput front_controller_en{"FRONT CONTROLLER Enable"};
+DigitalOutput speedgoat_en{"SPEEDGOAT Enable"};
+DigitalOutput accumulator_en{"ACCUMULATOR Enable"};
+DigitalOutput motor_ctrl_precharge_en{"MOTOR CTRL LV PRECHARGE Enable"};
+DigitalOutput motor_ctrl_en{"MOTOR CTRL LV Enable"};
+DigitalOutput imu_gps_en{"IMU GPS Enable"};
+
+shared::util::IdentityMap<float> powertrain_fan_power_to_duty{};
+
 }  // namespace mcal
 
 namespace bindings {
 
-const shared::periph::DigitalInput& button_di = mcal::button_di;
-const shared::periph::DigitalOutput& indicator_do = mcal::indicator_do;
+shared::periph::DigitalOutput& tsal_en = mcal::tsal_on_enable_en;
+shared::periph::DigitalOutput& raspberry_pi_en = mcal::raspberry_pi_en;
+shared::periph::DigitalOutput& front_controller_en = mcal::front_controller_en;
+shared::periph::DigitalOutput& speedgoat_en = mcal::speedgoat_en;
+shared::periph::DigitalOutput& accumulator_en = mcal::accumulator_en;
+shared::periph::DigitalOutput& motor_ctrl_precharge_en =
+    mcal::motor_ctrl_precharge_en;
+shared::periph::DigitalOutput& motor_ctrl_en = mcal::motor_ctrl_en;
+shared::periph::DigitalOutput& imu_gps_en = mcal::imu_gps_en;
+
+shared::util::Mapper<float>& powertrain_fan_power_to_duty =
+    mcal::powertrain_fan_power_to_duty;
 
 void Initialize() {
     std::cout << "Initializing Windows..." << std::endl;
 }
 
 }  // namespace bindings
+
+void DelayMS(uint32_t milliseconds) {
+    std::cout << "[Delaying for " << milliseconds << " milliseconds]"
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
