26903c9dca28c007f5cd4bd037c761023dcec7ad
BlakeFreer <freerb@mcmaster.ca>
2024-04-24 21:11:17 -0400

    addresses github comments, changes PWM GetDutyCycle method on stm

--- firmware/mcal/stm32f767/periph/pwm.h
+++ firmware/mcal/stm32f767/periph/pwm.h
@@ -24,14 +24,36 @@ public:
     }
 
     void SetDutyCycle(float duty_cycle) override {
-        duty_cycle = shared::util::Clamper<float>::Evaluate(duty_cycle, 0, 100);
+        // notice this sets the instance variable based on the argument
+        duty_cycle_ =
+            shared::util::Clamper<float>::Evaluate(duty_cycle, 0, 100);
 
-        uint32_t pulse = uint32_t(duty_cycle / 100.0f *
+        uint32_t pulse = uint32_t(duty_cycle_ / 100.0f *
                                   float(__HAL_TIM_GetAutoreload(htim_)));
         __HAL_TIM_SetCompare(htim_, channel_, pulse);
     }
 
+    /**
+     * @brief Quickly get the duty cycle which was last sent to this device.
+     * @note Compared to GetHardwareDutyCycle, this method is faster (doesn't
+     * read registers) and is less likely to cause issues when performing
+     * arithmetic since the hardware duty cycle is limited by integer
+     * resolution.
+     *
+     * @return float
+     */
     float GetDutyCycle() override {
+        return duty_cycle_;
+    }
+
+    /**
+     * @brief Get the duty cycle from the PWM registers.
+     * @note Compared to GetDutyCycle(), this method returns the "most accurate"
+     * current duty cycle.
+     *
+     * @return float
+     */
+    float GetHardwareDutyCycle() {
         uint32_t pulse = __HAL_TIM_GetCompare(htim_, channel_);
         uint32_t period = __HAL_TIM_GetAutoreload(htim_);
 
@@ -41,6 +63,7 @@ public:
 private:
     TIM_HandleTypeDef* htim_;
     uint32_t channel_;
+    float duty_cycle_ = 0;
 };
 
 }  // namespace mcal::stm32f767::periph
