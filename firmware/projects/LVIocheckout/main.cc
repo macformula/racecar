69d8093dfcacbd5572d1cd3584235e1366ef1ae8
samparent97 <samuelparent97@gmail.com>
2024-05-25 13:11:54 -0700

    Update main logic

--- firmware/projects/LVIocheckout/main.cc
+++ firmware/projects/LVIocheckout/main.cc
@@ -14,40 +14,36 @@
 int main(void) {
     bindings::Initialize();
 
-    // bindings::tsal_en.SetHigh();
-    // bindings::raspberry_pi_en.SetHigh();
-    bindings::front_controller_en.SetHigh();
-    // bindings::speedgoat_en.SetHigh();
-    bindings::motor_ctrl_precharge_en.SetHigh();
-    // bindings::motor_ctrl_en.SetHigh();
-    // bindings::imu_gps_en.SetHigh();
-    bindings::shutdown_circuit_en.SetHigh();
-    bindings::inverter_en.SetHigh();
-
     while (true) {
         // bool dcdc_valid = bindings::dcdc_valid.Read();
         // TODO: set a digital out to this value.
         bindings::accumulator_en.SetHigh();
         bindings::dcdc_en.SetHigh();
+        bindings::tsal_en.SetHigh();
+        bindings::raspberry_pi_en.SetHigh();
+        bindings::front_controller_en.SetHigh();
+        bindings::speedgoat_en.SetHigh();
+        bindings::motor_ctrl_precharge_en.SetHigh();
+        bindings::motor_ctrl_en.SetHigh();
+        bindings::imu_gps_en.SetHigh();
+        bindings::shutdown_circuit_en.SetHigh();
+        bindings::inverter_en.SetHigh();
 
-        bindings::DelayMS(2000);
+        bindings::DelayMS(1000);
 
         bindings::accumulator_en.SetLow();
         bindings::dcdc_en.SetLow();
+        bindings::tsal_en.SetLow();
+        bindings::raspberry_pi_en.SetLow();
+        bindings::front_controller_en.SetLow();
+        bindings::speedgoat_en.SetLow();
+        bindings::motor_ctrl_precharge_en.SetLow();
+        bindings::motor_ctrl_en.SetLow();
+        bindings::imu_gps_en.SetLow();
+        bindings::shutdown_circuit_en.SetLow();
+        bindings::inverter_en.SetLow();
 
-        bindings::DelayMS(2000);
-
-        // bindings::tsal_en.SetLow();
-        // bindings::raspberry_pi_en.SetLow();
-        // bindings::front_controller_en.SetLow();
-        // bindings::speedgoat_en.SetLow();
-        // bindings::accumulator_en.SetLow();
-        // bindings::motor_ctrl_precharge_en.SetLow();
-        // bindings::motor_ctrl_en.SetLow();
-        // bindings::imu_gps_en.SetLow();
-        // bindings::shutdown_circuit_en.SetLow();
-
-        // bindings::DelayMS(2000);
+        bindings::DelayMS(1000);
     }
 
     return 0;
