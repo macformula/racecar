macformula/racecar

commit 675f0d0dd6480606c02b6c79f0b3f3a3f8bdc31c

STM32 Can implementation; DemoCan Example (#110)
* Remove pywin32 from requirements.txt

* Fix mcal target includes

* Remove -e from echo command into makefile

* Add foo and bar demo can projects

* Remove print

* Add postbuild to raspi platform

* Revert postbuild change

* Remove all uneeded info for demo can dbc

* Add Dashboard enable to ioc

* Move tick blocking to project specific binding

* Add tick timestamp to rx messages

* Add temp debug msg

* Remove os lib from cmake for can demo projects

* Remove line added by clangd

* Fix missing start var

* Update main.cc

* Remove debug msg

* Remove iostream import

* Remove os remenants

* Fix all clang format checks

* build: :construction_worker: Improve the CubeMX autogeneration code. (#111)

* build: :construction_worker: Improve the CubeMX autogeneration code.

Provide a build option to bypass CubeMX generation (set AUTOGEN_CUBEMX=OFF in the top-level make command). Change method for appending custom targets to the cubemx Makefile to use a ile copy instead of string printing.

* build: :construction_worker: Enable building indepdent of cubemx generation.

Instead of appending targets to the CubeMX makefile, the targets are appended to a different copy ("CustomMakefile.mk"). This lets us leverage Make's prerequisite solving to independently generate the code and append the targets as necessary.

* Various fixes

* Fix postbuild

* Move the delay in projects

---------

Co-authored-by: Blake Freer <59676067+BlakeFreer@users.noreply.github.com>

firmware/mcal/stm32f767/os/tick.h
@@ -3,11 +3,14 @@
 
 #pragma once
 
+#include <sys/types.h>
+
 #include <cstdint>
 
 #include "cmsis_os2.h"
 #include "shared/os/os.h"
 #include "shared/os/tick.h"
+#include "stm32f7xx_hal.h"
 
 // TODO: Add comments and handle errors more robustly
 namespace os {

firmware/projects/TMS/platforms/stm32f767/CMakeLists.txt
@@ -5,7 +5,3 @@ add_subdirectory(cubemx)
 
 target_sources(bindings PUBLIC bindings.cc)
 target_link_libraries(bindings PUBLIC driver mcal-stm32f767)
-
-add_library(os)
-target_sources(os PUBLIC os.cc)
-target_link_libraries(os PUBLIC driver mcal-stm32f767)

firmware/mcal/stm32f767/CMakeLists.txt
@@ -3,6 +3,6 @@ add_library(mcal-stm32f767 INTERFACE)
 add_subdirectory(periph)
 add_subdirectory(os)
 
-target_include_directories(mcal-stm32f767 INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/..)
+target_include_directories(mcal-stm32f767 INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/../..)
 
 target_link_libraries(mcal-stm32f767 INTERFACE shared)

firmware/mcal/windows/CMakeLists.txt
@@ -1,6 +1,6 @@
 add_library(mcal-windows INTERFACE)
 
-target_include_directories(mcal-windows INTERFACE ${SMAKE_CURRENT_SOURCE_DIR}/..)
+target_include_directories(mcal-windows INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/../..)
 
 add_subdirectory(periph)
 target_link_libraries(mcal-windows INTERFACE shared)

firmware/projects/DemoCan/platforms/raspi/CMakeLists.txt
@@ -1,6 +1,0 @@
-target_sources(bindings
-	PRIVATE
-	bindings.cc
-)
-
-target_include_directories(bindings PUBLIC ${CMAKE_SOURCE_DIR})

firmware/projects/DemoCan/platforms/windows/CMakeLists.txt
@@ -1,6 +1,0 @@
-target_sources(bindings
-PRIVATE
-	bindings.cc
-)
-
-target_link_libraries(bindings PUBLIC mcal-windows)

firmware/projects/DemoCanBar/CMakeLists.txt
@@ -17,7 +17,7 @@ list(APPEND CAN_DEPENDENCIES "${CMAKE_CURRENT_SOURCE_DIR}/config.yaml")
 
 add_custom_target(
     generated_can
-	COMMAND ${Python3_EXECUTABLE} "${CMAKE_SOURCE_DIR}/../scripts/cangen/main.py" "--project=\"DemoCan\""
+	COMMAND ${Python3_EXECUTABLE} "${CMAKE_SOURCE_DIR}/../scripts/cangen/main.py" "--project=\"DemoCanBar\""
 	DEPENDS ${CAN_DEPENDENCIES}
 	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 	COMMENT Generating CAN code from DBCs

firmware/projects/DemoCanBar/platforms/raspi/CMakeLists.txt
@@ -1,0 +1,6 @@
+target_sources(bindings
+	PRIVATE
+	bindings.cc
+)
+
+target_link_libraries(bindings PUBLIC mcal-raspi)

commit 3c53e8100e2d1492c9deb262657ce8487005eb42

fix: :bug: Fix OS compiler errors.
675f0d0d introduced a breaking change for the TMS and DemoProjectRTOS by removing the CMake commands which compiled and linked the freertos code, as well as added an unnecessary include (sys/types.h) to mcal/stm/os/tick.h (this is likely clangd's fault). This commit restores these files so that the projects can compile again.

firmware/mcal/stm32f767/os/tick.h
@@ -3,8 +3,6 @@
 
 #pragma once
 
-#include <sys/types.h>
-
 #include <cstdint>
 
 #include "cmsis_os2.h"

firmware/projects/DemoProjectRTOS/platforms/stm32f767/CMakeLists.txt
@@ -5,3 +5,7 @@ add_subdirectory(cubemx)
 
 target_sources(bindings PUBLIC bindings.cc)
 target_link_libraries(bindings PUBLIC driver mcal-stm32f767)
+
+add_library(os)
+target_sources(os PUBLIC os.cc)
+target_link_libraries(os PUBLIC driver mcal-stm32f767)

firmware/projects/DemoProjectRTOS/platforms/stm32f767/cubemx/board_config.ioc
@@ -34,8 +34,8 @@ Mcu.PinsNb=5
 Mcu.ThirdPartyNb=0
 Mcu.UserConstants=
 Mcu.UserName=STM32F767ZITx
-MxCube.Version=6.8.1
-MxDb.Version=DB.6.0.81
+MxCube.Version=6.11.0
+MxDb.Version=DB.6.0.110
 NVIC.BusFault_IRQn=true\:0\:0\:false\:false\:true\:false\:true\:false\:false
 NVIC.DebugMonitor_IRQn=true\:0\:0\:false\:false\:true\:false\:true\:false\:false
 NVIC.ForceEnableDMAVector=true
@@ -94,6 +94,8 @@ ProjectManager.RegisterCallBack=
 ProjectManager.StackSize=0x400
 ProjectManager.TargetToolchain=Makefile
 ProjectManager.ToolChainLocation=
+ProjectManager.UAScriptAfterPath=
+ProjectManager.UAScriptBeforePath=
 ProjectManager.UnderRoot=false
 ProjectManager.functionlistsort=1-SystemClock_Config-RCC-false-HAL-false,2-MX_GPIO_Init-GPIO-false-HAL-true,0-MX_CORTEX_M7_Init-CORTEX_M7-false-HAL-true
 RCC.48MHZClocksFreq_Value=24000000

firmware/projects/FrontController/platforms/stm32f767/CMakeLists.txt
@@ -7,3 +7,7 @@ target_sources(bindings PUBLIC bindings.cc)
 target_link_libraries(bindings PUBLIC driver mcal-stm32f767)
 
 target_include_directories(bindings PUBLIC ${DIR_PROJECT}/inc) # access to bindings.h
+
+add_library(os)
+target_sources(os PUBLIC os.cc)
+target_link_libraries(os PUBLIC driver mcal-stm32f767)

firmware/projects/FrontController/platforms/stm32f767/cubemx/board_config.ioc
@@ -44,23 +44,27 @@ CAN3.CalculateTimeBit=2000
 CAN3.CalculateTimeQuantum=125.0
 CAN3.IPParameters=CalculateTimeQuantum,CalculateTimeBit,CalculateBaudRate,Prescaler,BS1,BS2,ABOM
 CAN3.Prescaler=1
+FREERTOS.FootprintOK=true
+FREERTOS.IPParameters=Tasks01,FootprintOK
+FREERTOS.Tasks01=Task5MS,24,128,Task_5ms,As weak,NULL,Dynamic,NULL,NULL;Task500MS,24,128,Task_500ms,As weak,NULL,Dynamic,NULL,NULL
 File.Version=6
 KeepUserPlacement=false
 Mcu.CPN=STM32F767ZIT6
 Mcu.Family=STM32F7
 Mcu.IP0=ADC1
 Mcu.IP1=CAN1
-Mcu.IP10=USART2
-Mcu.IP11=USART3
+Mcu.IP10=TIM2
+Mcu.IP11=USART2
+Mcu.IP12=USART3
 Mcu.IP2=CAN2
 Mcu.IP3=CAN3
 Mcu.IP4=CORTEX_M7
-Mcu.IP5=NVIC
-Mcu.IP6=RCC
-Mcu.IP7=SPI4
-Mcu.IP8=SYS
-Mcu.IP9=TIM2
-Mcu.IPNb=12
+Mcu.IP5=FREERTOS
+Mcu.IP6=NVIC
+Mcu.IP7=RCC
+Mcu.IP8=SPI4
+Mcu.IP9=SYS
+Mcu.IPNb=13
 Mcu.Name=STM32F767ZITx
 Mcu.Package=LQFP144
 Mcu.Pin0=PE2
@@ -73,14 +77,14 @@ Mcu.Pin14=PG6
 Mcu.Pin15=PA8
 Mcu.Pin16=PA11
 Mcu.Pin17=PA12
-Mcu.Pin18=PA13
-Mcu.Pin19=PA14
+Mcu.Pin18=PA15
+Mcu.Pin19=PD5
 Mcu.Pin2=PE5
-Mcu.Pin20=PA15
-Mcu.Pin21=PD5
-Mcu.Pin22=PD6
-Mcu.Pin23=PB5
-Mcu.Pin24=PB6
+Mcu.Pin20=PD6
+Mcu.Pin21=PB5
+Mcu.Pin22=PB6
+Mcu.Pin23=VP_FREERTOS_VS_CMSIS_V2
+Mcu.Pin24=VP_SYS_VS_tim4
 Mcu.Pin25=VP_TIM2_VS_ClockSourceINT
 Mcu.Pin3=PE6
 Mcu.Pin4=PC0
@@ -95,22 +99,28 @@ Mcu.UserConstants=
 Mcu.UserName=STM32F767ZITx
 MxCube.Version=6.11.0
 MxDb.Version=DB.6.0.110
-NVIC.BusFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
-NVIC.CAN1_RX0_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
-NVIC.CAN2_RX0_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
-NVIC.CAN3_RX0_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
-NVIC.DebugMonitor_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.BusFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
+NVIC.CAN1_RX0_IRQn=true\:5\:0\:false\:false\:true\:true\:true\:true\:true
+NVIC.CAN2_RX0_IRQn=true\:5\:0\:false\:false\:true\:true\:true\:true\:true
+NVIC.CAN3_RX0_IRQn=true\:5\:0\:false\:false\:true\:true\:true\:true\:true
+NVIC.DebugMonitor_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
 NVIC.ForceEnableDMAVector=true
-NVIC.HardFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
-NVIC.MemoryManagement_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
-NVIC.NonMaskableInt_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
-NVIC.PendSV_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.HardFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
+NVIC.MemoryManagement_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
+NVIC.NonMaskableInt_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
+NVIC.PendSV_IRQn=true\:15\:0\:false\:false\:false\:true\:false\:false\:false
 NVIC.PriorityGroup=NVIC_PRIORITYGROUP_4
-NVIC.SPI4_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
-NVIC.SVCall_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
-NVIC.SysTick_IRQn=true\:15\:0\:false\:false\:true\:false\:true\:false
-NVIC.USART2_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
-NVIC.UsageFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.SPI4_IRQn=true\:5\:0\:false\:false\:true\:true\:true\:true\:true
+NVIC.SVCall_IRQn=true\:0\:0\:false\:false\:false\:false\:false\:false\:false
+NVIC.SavedPendsvIrqHandlerGenerated=true
+NVIC.SavedSvcallIrqHandlerGenerated=true
+NVIC.SavedSystickIrqHandlerGenerated=true
+NVIC.SysTick_IRQn=true\:15\:0\:false\:false\:false\:true\:false\:true\:false
+NVIC.TIM4_IRQn=true\:15\:0\:false\:false\:true\:false\:false\:true\:true
+NVIC.TimeBase=TIM4_IRQn
+NVIC.TimeBaseIP=TIM4
+NVIC.USART2_IRQn=true\:5\:0\:false\:false\:true\:true\:true\:true\:true
+NVIC.UsageFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false\:false
 PA11.GPIOParameters=GPIO_Label
 PA11.GPIO_Label=PT2_CAN_RX
 PA11.Locked=true
@@ -121,10 +131,6 @@ PA12.GPIO_Label=PT1_CAN_TX
 PA12.Locked=true
 PA12.Mode=CAN_Activate
 PA12.Signal=CAN1_TX
-PA13.Mode=Serial_Wire
-PA13.Signal=SYS_JTMS-SWDIO
-PA14.Mode=Serial_Wire
-PA14.Signal=SYS_JTCK-SWCLK
 PA15.GPIOParameters=GPIO_Label
 PA15.GPIO_Label=VEH_CAN_TX
 PA15.Locked=true
@@ -332,6 +338,11 @@ USART2.IPParameters=VirtualMode-Asynchronous
 USART2.VirtualMode-Asynchronous=VM_ASYNC
 USART3.IPParameters=VirtualMode-Asynchronous
 USART3.VirtualMode-Asynchronous=VM_ASYNC
+VP_FREERTOS_VS_CMSIS_V2.Mode=CMSIS_V2
+VP_FREERTOS_VS_CMSIS_V2.Signal=FREERTOS_VS_CMSIS_V2
+VP_SYS_VS_tim4.Mode=TIM4
+VP_SYS_VS_tim4.Signal=SYS_VS_tim4
 VP_TIM2_VS_ClockSourceINT.Mode=Internal
 VP_TIM2_VS_ClockSourceINT.Signal=TIM2_VS_ClockSourceINT
 board=custom
+rtos.0.ip=FREERTOS

firmware/projects/TMS/platforms/stm32f767/CMakeLists.txt
@@ -5,3 +5,7 @@ add_subdirectory(cubemx)
 
 target_sources(bindings PUBLIC bindings.cc)
 target_link_libraries(bindings PUBLIC driver mcal-stm32f767)
+
+add_library(os)
+target_sources(os PUBLIC os.cc)
+target_link_libraries(os PUBLIC driver mcal-stm32f767)

firmware/projects/TMS/platforms/stm32f767/cubemx/board_config.ioc
@@ -125,6 +125,8 @@ ProjectManager.RegisterCallBack=
 ProjectManager.StackSize=0x400
 ProjectManager.TargetToolchain=Makefile
 ProjectManager.ToolChainLocation=
+ProjectManager.UAScriptAfterPath=
+ProjectManager.UAScriptBeforePath=
 ProjectManager.UnderRoot=false
 ProjectManager.functionlistsort=1-SystemClock_Config-RCC-false-HAL-false,2-MX_GPIO_Init-GPIO-false-HAL-true,3-MX_ADC1_Init-ADC1-false-HAL-true,4-MX_TIM4_Init-TIM4-false-HAL-true,0-MX_CORTEX_M7_Init-CORTEX_M7-false-HAL-true
 RCC.48MHZClocksFreq_Value=24000000