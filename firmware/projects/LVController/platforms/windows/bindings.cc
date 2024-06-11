af702c174a2027559b12779e71036e72d114ce8e
samparent97 <samuelparent97@gmail.com>
2024-06-10 22:11:42 -0700

    Fix windows build

--- firmware/projects/LVController/platforms/windows/bindings.cc
+++ firmware/projects/LVController/platforms/windows/bindings.cc
@@ -14,33 +14,56 @@
 #include "shared/periph/gpio.h"
 #include "shared/util/mappers/identity.h"
 
-namespace bindings {
-using namespace mcal::windows::periph;
-
 // clang-format off
-shared::periph::DigitalOutput&& tsal_en = DigitalOutput{"TSAL Enable"};
-shared::periph::DigitalOutput&& raspberry_pi_en = DigitalOutput{"RASPI Enable"};
-shared::periph::DigitalOutput&& front_controller_en = DigitalOutput{"FRONT CONTROLLER Enable"};
-shared::periph::DigitalOutput&& speedgoat_en = DigitalOutput{"SPEEDGOAT Enable"};
-shared::periph::DigitalOutput&& accumulator_en = DigitalOutput{"ACCUMULATOR Enable"};
-shared::periph::DigitalOutput&& motor_ctrl_precharge_en = DigitalOutput{"MOTOR CTRL LV PRECHARGE Enable"};
-shared::periph::DigitalOutput&& motor_ctrl_en = DigitalOutput{"MOTOR CTRL LV Enable"};
-shared::periph::DigitalOutput&& imu_gps_en = DigitalOutput{"IMU GPS Enable"};
-shared::periph::DigitalOutput&& shutdown_circuit_en = DigitalOutput{"SHUTDOWN CIRCUIT Enable"};
-shared::periph::DigitalOutput&& inverter_switch_en = DigitalOutput{"INVERTER SWITCH Enable"};
+namespace mcal {
+using namespace mcal::windows::periph;
+DigitalOutput tsal_en = DigitalOutput{"TSAL Enable"};
+DigitalOutput raspberry_pi_en = DigitalOutput{"RASPI Enable"};
+DigitalOutput front_controller_en = DigitalOutput{"FRONT CONTROLLER Enable"};
+DigitalOutput speedgoat_en = DigitalOutput{"SPEEDGOAT Enable"};
+DigitalOutput accumulator_en = DigitalOutput{"ACCUMULATOR Enable"};
+DigitalOutput motor_ctrl_precharge_en = DigitalOutput{"MOTOR CTRL LV PRECHARGE Enable"};
+DigitalOutput motor_ctrl_en = DigitalOutput{"MOTOR CTRL LV Enable"};
+DigitalOutput imu_gps_en = DigitalOutput{"IMU GPS Enable"};
+DigitalOutput shutdown_circuit_en = DigitalOutput{"SHUTDOWN CIRCUIT Enable"};
+DigitalOutput inverter_switch_en = DigitalOutput{"INVERTER SWITCH Enable"};
 
-shared::periph::DigitalOutput&& dcdc_en = DigitalOutput{"DCDC Enable"};
-shared::periph::DigitalInput&& dcdc_valid = DigitalInput{"DCDC Valid"};
-shared::periph::DigitalOutput&& dcdc_led_en = DigitalOutput{"DCDC LED Enable"};
-shared::periph::DigitalOutput&& powertrain_fan_en = DigitalOutput{"POWERTRAIN FAN Enable"};
-shared::periph::DigitalOutput&& powertrain_pump_en = DigitalOutput{"POWERTRAIN PUMP Enable"};
-shared::periph::PWMOutput&& powertrain_fan_pwm = PWMOutput{"POWERTRAIN FAN PWM"};
+DigitalOutput dcdc_en = DigitalOutput{"DCDC Enable"};
+DigitalInput dcdc_valid = DigitalInput{"DCDC Valid"};
+DigitalOutput dcdc_led_en = DigitalOutput{"DCDC LED Enable"};
+DigitalOutput powertrain_fan_en = DigitalOutput{"POWERTRAIN FAN Enable"};
+DigitalOutput powertrain_pump_en = DigitalOutput{"POWERTRAIN PUMP Enable"};
+PWMOutput powertrain_fan_pwm = PWMOutput{"POWERTRAIN FAN PWM"};
 
-shared::periph::CanBase&& veh_can_base = CanBase{"can0"};
+CanBase veh_can_base = CanBase{"can0"};
+}  // namespace mcal
+
+
+namespace bindings {
+shared::periph::DigitalOutput& tsal_en = mcal::tsal_en;
+shared::periph::DigitalOutput& raspberry_pi_en = mcal::raspberry_pi_en;
+shared::periph::DigitalOutput& front_controller_en = mcal::front_controller_en;
+shared::periph::DigitalOutput& speedgoat_en = mcal::speedgoat_en;
+shared::periph::DigitalOutput& accumulator_en = mcal::accumulator_en;
+shared::periph::DigitalOutput& motor_ctrl_precharge_en = mcal::motor_ctrl_precharge_en;
+shared::periph::DigitalOutput& motor_ctrl_en = mcal::motor_ctrl_en;
+shared::periph::DigitalOutput& imu_gps_en = mcal::imu_gps_en;
+shared::periph::DigitalOutput& shutdown_circuit_en = mcal::shutdown_circuit_en;
+shared::periph::DigitalOutput& inverter_switch_en = mcal::inverter_switch_en;
+
+shared::periph::DigitalOutput& dcdc_en = mcal::dcdc_en;
+shared::periph::DigitalInput& dcdc_valid = mcal::dcdc_valid;
+shared::periph::DigitalOutput& dcdc_led_en = mcal::dcdc_led_en;
+shared::periph::DigitalOutput& powertrain_fan_en = mcal::powertrain_fan_en;
+shared::periph::DigitalOutput& powertrain_pump_en = mcal::powertrain_pump_en;
+shared::periph::PWMOutput& powertrain_fan_pwm = mcal::powertrain_fan_pwm;
+
+shared::periph::CanBase& veh_can_base = mcal::veh_can_base;
 // clang-format on
 
 void Initialize() {
     std::cout << "Initializing Windows..." << std::endl;
+    mcal::veh_can_base.Setup();
 }
 
 void DelayMS(uint32_t milliseconds) {
