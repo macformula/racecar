db2a92fe40e032fb0b84b4d9b1d48efd5d5f63ae
BlakeFreer <freerb@mcmaster.ca>
2024-06-10 22:12:50 -0400

    adds all outputs from simulink, uses placeholders where I didn't know what to fill in

--- firmware/projects/FrontController/inc/bindings.h
+++ firmware/projects/FrontController/inc/bindings.h
@@ -4,17 +4,22 @@
 #pragma once
 
 #include "shared/periph/adc.h"
+#include "shared/periph/can.h"
 #include "shared/periph/gpio.h"
 
 namespace bindings {
 
 extern shared::periph::DigitalOutput&& driver_speaker;
 extern shared::periph::DigitalOutput&& brake_light;
+extern shared::periph::DigitalOutput&& status_light;
 extern shared::periph::ADCInput&& accel_pedal_1;
 extern shared::periph::ADCInput&& accel_pedal_2;
 extern shared::periph::ADCInput&& brake_pedal;
 extern shared::periph::ADCInput&& steering_wheel;
 extern shared::periph::DigitalInput&& driver_button;
+extern shared::periph::CanBase&& motor_left_can;
+extern shared::periph::CanBase&& motor_right_can;
+extern shared::periph::CanBase&& contactor_can;
 
 extern void Initialize();
 }  // namespace bindings
