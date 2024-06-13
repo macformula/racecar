fbfb83abb900677e64a843783cde063bac4a3167
BlakeFreer <freerb@mcmaster.ca>
2024-06-13 12:20:41 -0400

    got gpio working, convert away from && to & in bindings

--- firmware/projects/LVIocheckout/inc/bindings.h
+++ firmware/projects/LVIocheckout/inc/bindings.h
@@ -12,7 +12,7 @@ namespace bindings {
 
 extern shared::periph::DigitalOutput&& tsal_en;
 extern shared::periph::DigitalOutput&& raspberry_pi_en;
-extern shared::periph::DigitalOutput& front_controller_en;
+extern shared::periph::DigitalOutput&& front_controller_en;
 extern shared::periph::DigitalOutput&& speedgoat_en;
 extern shared::periph::DigitalOutput&& accumulator_en;
 extern shared::periph::DigitalOutput&& motor_ctrl_precharge_en;
