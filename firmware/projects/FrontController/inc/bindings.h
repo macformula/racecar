09a5e13ee2df83408d8ad2e612640ff8ab265bad
BlakeFreer <freerb@mcmaster.ca>
2024-06-13 13:23:43 -0400

    converts to & from &&

--- firmware/projects/FrontController/inc/bindings.h
+++ firmware/projects/FrontController/inc/bindings.h
@@ -9,17 +9,17 @@
 
 namespace bindings {
 
-extern shared::periph::DigitalOutput&& driver_speaker;
-extern shared::periph::DigitalOutput&& brake_light;
-extern shared::periph::DigitalOutput&& status_light;
-extern shared::periph::ADCInput&& accel_pedal_1;
-extern shared::periph::ADCInput&& accel_pedal_2;
-extern shared::periph::ADCInput&& brake_pedal_front;
-extern shared::periph::ADCInput&& brake_pedal_rear;
-extern shared::periph::ADCInput&& steering_wheel;
-extern shared::periph::DigitalInput&& start_button;
-extern shared::periph::CanBase&& veh_can_base;
-extern shared::periph::CanBase&& pt_can_base;
+extern shared::periph::DigitalOutput& driver_speaker;
+extern shared::periph::DigitalOutput& brake_light;
+extern shared::periph::DigitalOutput& status_light;
+extern shared::periph::ADCInput& accel_pedal_1;
+extern shared::periph::ADCInput& accel_pedal_2;
+extern shared::periph::ADCInput& brake_pedal_front;
+extern shared::periph::ADCInput& brake_pedal_rear;
+extern shared::periph::ADCInput& steering_wheel;
+extern shared::periph::DigitalInput& start_button;
+extern shared::periph::CanBase& veh_can_base;
+extern shared::periph::CanBase& pt_can_base;
 
 extern void Initialize();
 }  // namespace bindings
