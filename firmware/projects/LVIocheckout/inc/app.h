03f8a752f3b19693d42c7793905b34677d6da625
BlakeFreer <freerb@mcmaster.ca>
2024-06-13 11:52:18 -0400

    compare app vs raw

---
+++ firmware/projects/LVIocheckout/inc/app.h
@@ -0,0 +1,24 @@
+#include "shared/periph/gpio.h"
+
+class Subsystem {
+public:
+    /**
+     * @brief Construct a new Subsystem object
+     *
+     * @param enable_output Digital Output which enables the subsystem.
+     * enabled).
+     */
+    Subsystem(shared::periph::DigitalOutput& enable_output)
+        : enable_output_(enable_output) {}
+
+    inline virtual void Enable() const {
+        enable_output_.SetHigh();
+    }
+
+    inline virtual void Disable() const {
+        enable_output_.SetLow();
+    }
+
+private:
+    shared::periph::DigitalOutput& enable_output_;
+};
