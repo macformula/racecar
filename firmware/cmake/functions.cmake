bec6c6f5389b44e7066b6d37953c772bd5026d12
BlakeFreer <freerb@mcmaster.ca>
2024-08-26 20:15:20 -0400

    feat: :construction_worker: Update Makefile.
    Raise an error if parameters are omitted. Makes CMake path-existence checks more succinct.

--- firmware/cmake/functions.cmake
+++ firmware/cmake/functions.cmake
@@ -22,3 +22,35 @@ function(file_exists output file)
         set(${output} FALSE PARENT_SCOPE)
     endif()
 endfunction()
+
+function(assert_exists)
+    # Exit if a path does not exist.
+    set(options DIR FILE)
+    set(one_value_args PATH)
+    set(multi_value_args START PASS FAIL)
+
+    cmake_parse_arguments(
+        ASSERT_EXISTS
+        "${options}"
+        "${one_value_args}"
+        "${multi_value_args}"
+        ${ARGN}
+    )
+
+    message(CHECK_START ${ASSERT_EXISTS_START})
+    
+    if(${ASSERT_EXISTS_FILE})
+        file_exists(out ${ASSERT_EXISTS_PATH})
+    elseif(${ASSERT_EXISTS_DIR})
+        directory_exists(out ${ASSERT_EXISTS_PATH})
+    else()
+        message(FATAL_ERROR "mode must be FILE or DIR")
+    endif()
+
+    if(${out})
+        message(CHECK_PASS ${ASSERT_EXISTS_PASS})
+    else()
+        message(CHECK_PASS ${ASSERT_EXISTS_FAIL})
+        message(FATAL_ERROR "Exiting due to an error.")
+    endif()
+endfunction()
