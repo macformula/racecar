fff9e19480fde8cd5dbb0cc56040b5b01d16e760
BlakeFreer <freerb@mcmaster.ca>
2024-09-06 17:24:54 -0400

    CAN for pedal debugging

--- firmware/projects/EV5/debug/MotorDebug/inc/bindings.h
+++ firmware/projects/EV5/debug/MotorDebug/inc/bindings.h
@@ -3,14 +3,16 @@
 #include <cstdint>
 
 #include "shared/periph/adc.h"
+#include "shared/periph/can.h"
 
 namespace bindings {
 
 extern shared::periph::ADCInput&& accel_pedal_1;
 extern shared::periph::ADCInput&& accel_pedal_2;
+extern shared::periph::CanBase&& vehicle_can_base;
 
 extern void Initialize();
 
 extern void DelayMS(uint32_t ms);
 
-} // namespace bindings
+}  // namespace bindings
--- firmware/projects/EV5/debug/MotorDebug/main.cc
+++ firmware/projects/EV5/debug/MotorDebug/main.cc
@@ -1,5 +1,25 @@
-#include "bindings.h"
 #include <stdint.h>
+
+#include "bindings.h"
+#include "generated/can/vehicle_can_messages.h"
+#include "generated/can/vehicle_msg_registry.h"
+#include "projects/EV5/debug/MotorDebug/generated/can/vehicle_msg_registry.h"
+#include "shared/comms/can/can_bus.h"
+#include "shared/periph/can.h"
+
+namespace bindings {
+
+extern shared::periph::CanBase& vehicle_can_base;
+
+}  // namespace bindings
+
+generated::can::VehicleMsgRegistry veh_can_registry{};
+shared::can::CanBus vehicle_can_bus{
+    bindings::vehicle_can_base,
+    veh_can_registry,
+};
+
+generated::can::APPS pedal_message{};
 
 int main() {
     bindings::Initialize();
@@ -9,6 +29,9 @@ int main() {
         uint32_t accel_pedal_pos_2 = bindings::accel_pedal_2.Read();
 
         // Send over CAN
+        pedal_message.apps1 = accel_pedal_pos_1;
+        pedal_message.apps2 = accel_pedal_pos_2;
+        vehicle_can_bus.Send(pedal_message);
 
         bindings::DelayMS(25);
     }
