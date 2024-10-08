32ed9c2d9d7f7aa2301fbb8915c02aa4f7853563
luaibash <luai.hawa@gmail.com>
2024-10-08 19:54:17 -0400

    Added contents for dbc file to define 64 errors

--- firmware/projects/Demo/CANErrors/bindings.h
+++ firmware/projects/Demo/CANErrors/bindings.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#include "shared/periph/gpio.h"
-
 namespace bindings {
-
-extern shared::periph::DigitalOutput& indicator;
-
-extern void DelayMS(unsigned int ms);
-extern void Initialize();
 
 }  // namespace bindings
