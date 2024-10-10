ba75eece58a7a2bc12f0b5df6f871fa2476837e4
BlakeFreer <freerb@mcmaster.ca>
2024-10-10 00:25:10 -0400

    isolates tests into functions

--- firmware/projects/Demo/AnalogPWM/main.cc
+++ firmware/projects/Demo/AnalogPWM/main.cc
@@ -1,36 +1,57 @@
 #include "bindings.h"
 
+using namespace bindings;
+
+/**
+ * @brief Tests the PWMOutput class using various frequencies and duty cycles.
+ */
+void DemoPWM() {
+    pwm.Start();
+    DelayMS(1000);
+
+    pwm.SetFrequency(100);
+    pwm.SetDutyCycle(50.);
+    DelayMS(1000);
+
+    pwm.GetFrequency();
+    pwm.GetDutyCycle();
+    DelayMS(1000);
+
+    pwm.SetFrequency(100);
+    DelayMS(1000);
+
+    pwm.GetFrequency();
+    DelayMS(1000);
+
+    pwm.SetFrequency(-5);
+    pwm.GetFrequency();
+    DelayMS(1000);
+
+    pwm.GetFrequency();
+    DelayMS(1000);
+
+    pwm.Stop();
+}
+
+/**
+ * @brief Tests the AnalogOutput class using various voltages.
+ */
+void DemoAnalog() {
+    analog_out.SetVoltage(3.0f);
+    DelayMS(1000);
+    analog_out.SetVoltage(50.);
+}
+
 int main() {
-    bindings::Initialize();
+    Initialize();
 
     while (true) {
+        DemoPWM();
+        DelayMS(1000);
 
-        // tests the PWMOutput class using various frequencies
-        bindings::pwm.Start();
-        bindings::DelayMS(1000);
-        bindings::pwm.SetFrequency(0);
-        bindings::DelayMS(1000);
-        bindings::pwm.GetFrequency();
-        bindings::DelayMS(1000);
-        bindings::pwm.SetFrequency(100);
-        bindings::DelayMS(1000);
-        bindings::pwm.GetFrequency();
-        bindings::DelayMS(1000);
-        bindings::pwm.SetFrequency(-5);
-        bindings::DelayMS(1000);
-        bindings::pwm.GetFrequency();
-        bindings::DelayMS(1000);
-        bindings::pwm.Stop();
-
-        // tests the AnalogOutput class using various voltages
-        bindings::DelayMS(1000);
-        bindings::analog_out.SetVoltage(3);
-        bindings::DelayMS(1000);
-        bindings::analog_out.SetVoltage(50);
-        bindings::DelayMS(1000);
-
+        DemoAnalog();
+        DelayMS(1000);
     }
 
     return 0;
 }
-
