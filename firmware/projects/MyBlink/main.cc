08bcea15abd4edde82b55244123abd0c7522bb0a
Nagatarun25 <nagatarunmoturi@gmail.com>
2024-10-13 15:17:57 -0400

    Implemented initial cross-platform interrupt handler for Linux and STM32

---
+++ firmware/projects/MyBlink/main.cc
@@ -0,0 +1,24 @@
+#include "bindings.h"
+
+
+
+int main() {
+    bindings::Initialize();
+    
+
+    while (true) {
+        bindings:: setup_interupt();
+
+        if (bindings::toggle){
+            bindings::indicator.SetHigh();
+        } else{
+            bindings::indicator.SetLow();
+        }
+     
+        
+       
+        bindings::DelayMS(1000);
+    }
+
+    return 0;
+}
