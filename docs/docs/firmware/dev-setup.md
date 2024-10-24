b43bc39dadf8d1856b31d2687537e13a63dadcf0
BlakeFreer <freerb@mcmaster.ca>
2024-10-24 19:20:31 -0400

    Update cmake to use CANgen and change install docs

--- docs/docs/firmware/dev-setup.md
+++ docs/docs/firmware/dev-setup.md
@@ -168,12 +168,12 @@ Create a Python virtual environment for CANgen. Navigate into `racecar/firmware`
     source .env/bin/activate
     ```
 
-The second command "activates" the virtual environment. You will see `(.env)` beside your terminal prompt when it is activated, and it must be activated before building any project.
+The second command "activates" the virtual environment. You will see `(.env)` beside your terminal prompt when it is activated. __It must be activated before building any project.__
 
-Finally, with the environment activated, install the CANgen requirements.
+Finally, with the environment activated, change into `racecar/` and install CANgen:
 
 ```bash
-pip install -r ../scripts/cangen/requirements.txt
+pip install scripts/cangen
 ```
 
 You can now start developing in `racecar`! However, I recommend you configure your IDE with `clangd`, so continue to the next section.
--- firmware/cmake/cangen.cmake
+++ firmware/cmake/cangen.cmake
@@ -1,22 +1,14 @@
-find_package(Python3 COMPONENTS Interpreter)
-message(${Python3_EXECUTABLE})
-
-if(NOT ${Python3_FOUND})
-	message(FATAL_ERROR "Python 3 executable not found")
-endif()
-
-FILE(GLOB_RECURSE CAN_DEPENDENCIES CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/../scripts/cangen/*" "${CMAKE_SOURCE_DIR}/dbcs/*")
-
 list(APPEND CAN_DEPENDENCIES "${CMAKE_CURRENT_SOURCE_DIR}/config.yaml")
 
 cmake_language(GET_MESSAGE_LOG_LEVEL LOG_LEVEL)
 
+# cangen is provided by racecar/scripts/cangen
 add_custom_target(
     generated_can
-	COMMAND ${Python3_EXECUTABLE} "${CMAKE_SOURCE_DIR}/../scripts/cangen/main.py" "--project=\"${CMAKE_CURRENT_SOURCE_DIR}\"" "--log-level=${LOG_LEVEL}"
-	DEPENDS ${CAN_DEPENDENCIES}
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	COMMENT Generating CAN code from DBCs
+    COMMAND "cangen" ${CMAKE_CURRENT_SOURCE_DIR} "--log-level=${LOG_LEVEL}"
+    DEPENDS ${CAN_DEPENDENCIES}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT Generating CAN code from DBCs
 )
 
 add_dependencies(main generated_can)
