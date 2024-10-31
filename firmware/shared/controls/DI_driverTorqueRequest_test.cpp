0e263e13db98a358f28ca0052cc32fbff023d6d3
BlakeFreer <freerb@mcmaster.ca>
2024-10-31 19:31:47 -0400

    formatting

--- firmware/shared/controls/DI_driverTorqueRequest_test.cpp
+++ firmware/shared/controls/DI_driverTorqueRequest_test.cpp
@@ -9,12 +9,20 @@
 int main() {
     // Write your test cases.
 
-    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(false, true, 2.5), 0.0);      // should return 0
-    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(true, false, 2.5), 1.0);      // should return AccelPedPos = 1 (edge case)
-    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(true, false, 0.6f), 1.0f);      // should return round(AccelPedPos) = 1
-    ASSERT_NCLOSE(ctrl::DI_driverTorqueRequest(true, false, 0.4), 2.0);      // should return AccelPedPos = 0
+    // should return 0
+    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(false, true, 2.5), 0.0);
 
-    ASSERT_EQ(ctrl::DI_driverTorqueRequest(true, false, -1), 0);            //should return AccelPedPos = 0 (negative position case)
+    // should return AccelPedPos = 1 (edge case)
+    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(true, false, 2.5), 1.0);
+
+    // should return round(AccelPedPos) = 1
+    ASSERT_CLOSE(ctrl::DI_driverTorqueRequest(true, false, 0.6f), 1.0f);
+
+    // should return AccelPedPos = 0
+    ASSERT_NCLOSE(ctrl::DI_driverTorqueRequest(true, false, 0.4), 2.0);
+
+    // should return AccelPedPos = 0 (negative position case)
+    ASSERT_EQ(ctrl::DI_driverTorqueRequest(true, false, -1), 0);
 
     // This statement will not be reached if an assert fails.
     std::cout << "All tests passed" << std::endl;
