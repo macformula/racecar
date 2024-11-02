7684edb9bb2858e88cff9d49551a9500c3971e8e
Everson <shene7@mcmaster.ca>
2024-11-02 13:01:25 -0400

    main rewrite, not fully complete

--- firmware/projects/Demo/FSM/main.cc
+++ firmware/projects/Demo/FSM/main.cc
@@ -1,14 +1,13 @@
 #include <conio.h>
-
-#include <iostream>
-
 #include "bindings.h"
+#include "etl/queue.h"
 #include "etl/fsm.h"
 #include "etl/message_packet.h"
-#include "etl/vector.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
-const etl::message_router_id_t MOTOR_CONTROL = 0;
+
 
 // Define your state identifiers
 struct EventId {  // Initiates actions, transitions, or signals FSM, triggers
@@ -32,147 +31,128 @@ struct StateId {  // Defines actions and responses to events
 class Start : public etl::message<EventId::START> {
 public:
     Start() {
-        cout << "Start event triggered.\n";
+        cout << "Start state triggered.\n";
     }
 };
 
-class Running : public etl::message<EventId::STOPPED> {
+class Stopped : public etl::message<EventId::STOPPED> {
 public:
-    Running() {
-        cout << "Running event triggered.\n";
+    Stopped() {
+        cout << "Winding down state triggered.\n";
     }
 };
 
 class Stop : public etl::message<EventId::STOP> {
 public:
     Stop() {
-        cout << "Stop event triggered.\n";
+        cout << "Stop state triggered.\n";
     }
 };
 
 // Base state class for all motor states
 class MotorControl : public etl::fsm {
 public:
-    MotorControl() : fsm(MOTOR_CONTROL), current_state(StateId::IDLE) {}
-    int current_state;
 
-    virtual void on_enter() {}  // Virtual to allow overriding in derived states
+    MotorControl() : fsm(1){} // constructor
 
-    // Generic on_event method that can be specialized per event type
-    virtual void on_event(const Start&) {}
-    virtual void on_event(const Running&) {}
-    virtual void on_event(const Stop&) {}
     void LogUnknownEvent(etl::imessage& msg) {
         cout << "Error, unknown event";
     }
 
-    void transitionUp() {
-        if (current_state == StateId::IDLE) {
-            current_state = StateId::RUNNING;
-        } else if (current_state == StateId::RUNNING) {
-            current_state = StateId::WINDING_DOWN;
-        }
-        // If already in WINDING_DOWN, stay there
-    }
+    void receive(const etl::imessage& msg_) override{
+    if (accepts(msg_))
+    {
+      // Place in queue.
+      queue.emplace(msg_);
 
-    // Transition to the previous state (down arrow)
-    void transitionDown() {
-        if (current_state == StateId::WINDING_DOWN) {
-            current_state = StateId::RUNNING;
-        } else if (current_state == StateId::RUNNING) {
-            current_state = StateId::IDLE;
-        }
-        // If already in IDLE, stay there
+      std::cout << "Queueing message " << int(msg_.get_message_id()) << std::endl;
     }
-
-    void handleInput() {
-        while (true) {
-            int ch = _getch();
-
-            switch (ch) {
-                case 'w':  // Up key (W)
-                case 'W':  // Handle uppercase W as well
-                    transitionUp();
-                    break;
-                case 's':  // Down key (S)
-                case 'S':  // Handle uppercase S as well
-                    transitionDown();
-            }
-        }
+    else
+    {
+      std::cout << "Ignoring message " << int(msg_.get_message_id()) << std::endl;
     }
+  }
+
+  //***************************************************************************
+  // The method to call to handle any queued messages.
+  //***************************************************************************
+  void process_queue()
+  {
+    while (!queue.empty())
+    {
+      message_packet& packet = queue.front();
+      etl::imessage& msg = packet.get();
+      std::cout << "Processing message " << int(msg.get_message_id()) << std::endl;
+
+      // Call the base class's receive function.
+      // This will route it to the correct 'on_event' handler.
+      etl::fsm::receive(msg);
+
+      queue.pop();
+    }
+  }
+
+private:
+
+  typedef etl::message_packet<Start, Stop, Stopped> message_packet;
+
+  // The queue of message items.
+  etl::queue<message_packet, 10> queue;
+
+    // void transitionUp() {
+    //     if (current_state == StateId::IDLE) {
+    //         current_state = StateId::RUNNING;
+    //     } else if (current_state == StateId::RUNNING) {
+    //         current_state = StateId::WINDING_DOWN;
+    //     }
+    //     // If already in WINDING_DOWN, stay there
+    // }
+
+    // // Transition to the previous state (down arrow)
+    // void transitionDown() {
+    //     if (current_state == StateId::WINDING_DOWN) {
+    //         current_state = StateId::RUNNING;
+    //     } else if (current_state == StateId::RUNNING) {
+    //         current_state = StateId::IDLE;
+    //     }
+    //     // If already in IDLE, stay there
+    // }
+
+    // void handleInput() {
+    //     while (true) {
+    //         int ch = _getch();
+
+    //         switch (ch) {
+    //             case 'w':  // Up key (W)
+    //             case 'W':  // Handle uppercase W as well
+    //                 transitionUp();
+    //                 break;
+    //             case 's':  // Down key (S)
+    //             case 'S':  // Handle uppercase S as well
+    //                 transitionDown();
+    //         }
+    //     }
+    // }
+
+    
 };
 
 class IdleState
-    : public etl::fsm_state<MotorControl, IdleState, StateId::IDLE, Start> {
+    : public etl::fsm_state<MotorControl, IdleState, StateId::IDLE, Start, Stop, Stopped> {
 public:
-    IdleState() {
-        cout << "Initializing Stop State\n";
-    }
 
-    etl::fsm_state_id_t on_entry(const Stop& event) {
-        get_fsm_context().current_state = StateId::IDLE;
+    etl::fsm_state_id_t on_enter_state() override {
         cout << "Now in Idle State\n";
-        return STATE_ID;
+        return StateId::IDLE;
     }
 
-    etl::fsm_state_id_t on_exit(const Stop& event) {
+    void on_exit_state() override {
         cout << "Exiting Idle State, transitioning to Running\n";
-        return StateId::RUNNING;
     }
 
-    etl::fsm_state_id_t on_event_unknown(
-        etl::imessage& event) {  // an event represents an action or signal that
-                                 // can trigger a change in the state of a
-                                 // finite state machine (FSM).
-        get_fsm_context().LogUnknownEvent(event);
-        return STATE_ID;
-    }
-};
-
-// Running State
-class RunningState
-    : public etl::fsm_state<MotorControl, Running, StateId::RUNNING, Stop> {
-public:
-    RunningState() {
-        cout << "Initializing Running State\n";
-    }
-
-    etl::fsm_state_id_t on_entry(const Running& event) {
-        get_fsm_context().current_state = StateId::RUNNING;
-        cout << "Now in Running State\n";
-        return STATE_ID;
-    }
-
-    etl::fsm_state_id_t on_exit(const Running& event) {
-        cout << "Exiting Running State, transitioning to Winding Down\n";
-        return StateId::WINDING_DOWN;
-    }
-
-    etl::fsm_state_id_t on_event_unknown(
-        etl::imessage& event) {  // an event represents an action or signal that
-                                 // can trigger a change in the state of a
-                                 // finite state machine (FSM).
-        get_fsm_context().LogUnknownEvent(event);
-        return STATE_ID;
-    }
-};
-
-// Winding Down State
-class Winding_DownState : public etl::fsm_state<MotorControl, Winding_DownState,
-                                                StateId::WINDING_DOWN, Stop> {
-public:
-    Winding_DownState() {
-        cout << "Initializing Start State\n";
-    }
-
-    etl::fsm_state_id_t on_entry(const Start& event) {
-        get_fsm_context().current_state = StateId::WINDING_DOWN;
-        cout << "Now in Winding Down State\n";
-        return STATE_ID;
-    }
-
-    etl::fsm_state_id_t on_exit(const Start& event) {
-        cout << "Exiting Winding Down State, transitioning to Idle\n";
+    etl::fsm_state_id_t on_event(const Stop& Stop){
+        cout << "  S1 : Received message " << int(Stop.get_message_id()) << " : '" << EventId::STOPPED << "'" << std::endl;
+        cout.flush();
         return StateId::IDLE;
     }
 
@@ -180,9 +160,90 @@ public:
         etl::imessage& event) {  // an event represents an action or signal that
                                  // can trigger a change in the state of a
                                  // finite state machine (FSM).
-        get_fsm_context().LogUnknownEvent(event);
-        return STATE_ID;
+        std::cout << "  S1 : Received unknown message " << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::IDLE;
     }
+
+    etl::fsm_state_id_t on_exit_state(const Start& Start){
+        cout << "  S1 : Received message " << int(Start.get_message_id()) << " : '" << EventId::START << "'" << std::endl;
+        cout.flush();
+        return StateId::RUNNING;
+
+    }
+};
+
+// Running State
+class RunningState
+    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start, Stop, Stopped> {
+public:
+        etl::fsm_state_id_t on_enter_state() override {
+        cout << "Now in Running State\n";
+        return StateId::RUNNING;
+    }
+
+    void on_exit_state() override {
+        cout << "Exiting Running State, transitioning to Winding Down\n";
+    }
+
+    etl::fsm_state_id_t on_event(const Stop& Stop){
+        cout << "  S2 : Received message " << int(Stop.get_message_id()) << " : '" << EventId::START << "'" << std::endl;
+        cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_event_unknown(
+        etl::imessage& event) {  // an event represents an action or signal that
+                                 // can trigger a change in the state of a
+                                 // finite state machine (FSM).
+        std::cout << "  S1 : Received unknown message " << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_exit_state(const Start& Stop){
+        cout << "  S1 : Received message " << int(Stop.get_message_id()) << " : '" << EventId::STOP << "'" << std::endl;
+        cout.flush();
+        return StateId::WINDING_DOWN;
+
+    }
+
+};
+
+// Winding Down State
+class Winding_DownState : public etl::fsm_state<MotorControl, Winding_DownState, StateId::WINDING_DOWN, Start, Stop, Stopped> {
+public:
+        etl::fsm_state_id_t on_enter_state() override {
+        cout << "Now in Running State\n";
+        return StateId::RUNNING;
+    }
+
+    void on_exit_state() override {
+        cout << "Exiting Running State, transitioning to Winding Down\n";
+    }
+
+    etl::fsm_state_id_t on_event(const Stop& Stop){
+        cout << "  S2 : Received message " << int(Stop.get_message_id()) << " : '" << EventId::START << "'" << std::endl;
+        cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_event_unknown(
+        etl::imessage& event) {  // an event represents an action or signal that
+                                 // can trigger a change in the state of a
+                                 // finite state machine (FSM).
+        std::cout << "  S1 : Received unknown message " << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_exit_state(const Start& Stop){
+        cout << "  S1 : Received message " << int(Stop.get_message_id()) << " : '" << EventId::STOP << "'" << std::endl;
+        cout.flush();
+        return StateId::WINDING_DOWN;
+
+    }
+
 };
 
 
