1cae95c8036fe14ff1161a15af8030d32ed00d7a
Ojeifo Esene <ojeifo12@hotmail.com>
2024-11-02 01:54:47 -0400

    Fixed the template, the function behavior, Makefile issues, and the test file.

--- firmware/shared/controls/DI_driverTorqueRequest.h
+++ firmware/shared/controls/DI_driverTorqueRequest.h
@@ -1,25 +1,14 @@
 #pragma once
 #include <cmath>
-#include <iostream>
-
 namespace ctrl {
 
 // Templating means this function will work on any types that support addition.
-template <typename T, typename B>
-T DI_driverTorqueRequest(T READY_TO_DRIVE, T DI_Error, B AccelPedPos) {
-    T u1 = 0.0;
-    if ((!READY_TO_DRIVE) == 1 || DI_Error == 1) {
-        return u1;
+template <typename B>
+B DI_driverTorqueRequest(bool READY_TO_DRIVE, bool DI_Error, B AccelPedPos) {
+    if ((!READY_TO_DRIVE) || DI_Error) {
+        return 0;
     } else {
-        // AccelPedPos (n-d Lookup Table)
-        if (AccelPedPos > 1) {
-            return 1.0;
-        } else if (AccelPedPos >= 0) {
-            B r_AccelPedPos = round(AccelPedPos);
-            return r_AccelPedPos;
-        } else {
-            return u1;
-        }
+        return AccelPedPos;
     }
 }
 
