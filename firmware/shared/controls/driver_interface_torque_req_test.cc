c76d940815e36c9294e2ce509031a0230973f24b
Ojeifo Esene <ojeifo12@hotmail.com>
2024-11-02 14:28:03 -0400

    Changed the test conditions + added detailed explanations

--- firmware/shared/controls/driver_interface_torque_req_test.cc
+++ firmware/shared/controls/driver_interface_torque_req_test.cc
@@ -9,21 +9,22 @@
 int main() {
     // Write your test cases.
 
-    // should return 0
+    // should return 0 because ready_to_drive is false
     ASSERT_CLOSE(ctrl::driver_interface_torque_req(false, false, 2.5), 0);
 
-    // should return accel_ped_pos = 2.5
+    // should return accel_ped_pos = 2.5 because ready_to_drive true and
+    // driver_int_error is false
     ASSERT_CLOSE(ctrl::driver_interface_torque_req(true, false, 2.5), 2.5);
 
     // should return accel_ped_pos = 0.0006f
     ASSERT_CLOSE(ctrl::driver_interface_torque_req(true, false, 0.0006f),
                  0.0006f);
 
-    // should return accel_ped_pos = 0.4
-    ASSERT_NCLOSE(ctrl::driver_interface_torque_req(false, true, 0.4), 0.5);
+    // should return 0 because driver_int_error is true
+    ASSERT_CLOSE(ctrl::driver_interface_torque_req(true, true, 0.4), 0);
 
-    // should return 0
-    ASSERT_EQ(ctrl::driver_interface_torque_req(true, true, 1), 0);
+    // should return int 0
+    ASSERT_EQ(ctrl::driver_interface_torque_req(false, true, 1), 0);
 
     // This statement will not be reached if an assert fails.
     std::cout << "All tests passed" << std::endl;
