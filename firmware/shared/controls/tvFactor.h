3624d14fbc31a7b2370f584ca898a900f66f7898
Everson <shene7@mcmaster.ca>
2024-11-03 13:43:31 -0500

    before formatting

--- firmware/shared/controls/tvFactor.h
+++ firmware/shared/controls/tvFactor.h
@@ -23,13 +23,13 @@ T open_loop_tv_lookUp(T DI_p_steeringAngle){
     // breakpoints = {0, 5, 10, 15, 20, 25}; 
     // tableData = {1, 0.934, 0.87, 0.808, 0.747, 0.683};
 
-    const T steeringAngle_lut_data[][2] = {
-    {0, 1},
-	{5, 0.934},
-	{10, 0.87},
-	{15, 0.808},
-	{20, 0.747},
-	{25, 0.683},
+    const float steeringAngle_lut_data[][2] = {
+    {0.0, 1.0f},
+	{5.0, 0.934f},
+	{10.0, 0.87f},
+	{15.0, 0.808f},
+	{20.0, 0.747f},
+	{25.0, 0.683f},
     };
 
     constexpr int steeringAngle_lut_length =
@@ -37,25 +37,28 @@ T open_loop_tv_lookUp(T DI_p_steeringAngle){
 
     shared::util::LookupTable<steeringAngle_lut_length> steeringAngle_adc_lut{steeringAngle_lut_data};
 
-    T result = steeringAngle_adc_lut.get(DI_p_steeringAngle);
+    T result = steeringAngle_adc_lut.Evaluate(Angle);
+    return result;
 }
 
+
+        
 template <typename T>
 std::tuple<T,T> tvFactoring(T p_steeringAngle, T tvFactor){    //don't pass by reference (struct or tuple)
     T p_tvFactorLeft;
     T p_tvFactorRight; 
 
     if (p_steeringAngle > 0){
-        p_tvFactorLeft = 1;
+        p_tvFactorLeft = T(1);
         p_tvFactorRight = tvFactor;
     }
     else if (p_steeringAngle < 0){
-        p_tvFactorRight = 1;
+        p_tvFactorRight = T(1);
         p_tvFactorLeft = tvFactor;
     }
     else{
-        p_tvFactorLeft = 1;
-        p_tvFactorRight = 1;
+        p_tvFactorLeft = T(1);
+        p_tvFactorRight = T(1);
     }
 
     return std::make_tuple(p_tvFactorLeft, p_tvFactorRight);
---
+++ firmware/shared/controls/tvFactor_test.cc
@@ -0,0 +1,47 @@
+#include <iostream> 
+
+#include "tvFactor.h"
+
+#include "testing.h"
+int main (){
+    // Use these for float / double types
+    // #define ASSERT_CLOSE(x, y) assert(std::abs((x) - (y)) < (decltype(x))TOLERANCE)
+    // #define ASSERT_NCLOSE(x, y) assert(std::abs((x) - (y)) > (decltype(x))TOLERANCE)
+
+    using namespace ctrl;
+
+    ASSERT_CLOSE(tvEnable(26.3, 0.2), 26.3);   // Should return 30.0 because DI_b_tvEnable > 0
+    ASSERT_CLOSE(tvEnable(21.3, 0.0), 0.0);    // Should return 0.0 because DI_b_tvEnable <= 0
+    ASSERT_CLOSE(tvEnable(-20.0, 1.5), -20.0); // Should return -20.0 because DI_b_tvEnable > 0
+    ASSERT_CLOSE(tvEnable(12.0, -6.2), 0.0);   // Should return 0.0 because DI_b_tvEnable <= 0
+
+    ASSERT_CLOSE(open_loop_tv_lookUp(5.0), 0.934);
+    ASSERT_CLOSE(open_loop_tv_lookUp(10.0), 0.87);
+    ASSERT_CLOSE(open_loop_tv_lookUp(13.2), 0.83032);
+    ASSERT_CLOSE(open_loop_tv_lookUp(23.3), 0.70476);
+    ASSERT_CLOSE(open_loop_tv_lookUp(27.3), 0.683);
+
+    auto [leftFactor1, rightFactor1] = tvFactoring(15.2, 3.6);
+    ASSERT_CLOSE(leftFactor1, 1.0); 
+    ASSERT_CLOSE(rightFactor1, 3.6);
+
+
+    auto [leftFactor2, rightFactor2] = tvFactoring(-13.6, 2.2); 
+    ASSERT_CLOSE(leftFactor2, 2.2);  
+    ASSERT_CLOSE(rightFactor2, 1); 
+
+    auto [leftFactor3, rightFactor3] = tvFactoring(0.0, 6.0); 
+    ASSERT_CLOSE(leftFactor3, 1);  
+    ASSERT_CLOSE(rightFactor3, 1); 
+
+
+
+
+
+
+    std::cout << "All tests passed" << std::endl;
+    
+    return 0;
+}
+
+
