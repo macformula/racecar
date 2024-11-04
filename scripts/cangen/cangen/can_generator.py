13966d04a634b7486fd92cef07c7dc247b53766b
AndrewI26 <105824138+AndrewI26@users.noreply.github.com>
2024-11-04 15:25:38 -0500

    formated with ruff

--- scripts/cangen/cangen/can_generator.py
+++ scripts/cangen/cangen/can_generator.py
@@ -242,7 +242,7 @@ def _prepare_output_directory(output_dir):
     os.makedirs(output_dir, exist_ok=True)
     for file in os.listdir(output_dir):
         os.unlink(file)
-    
+
     gitignore_path = os.path.join(output_dir, ".gitignore")
     with open(gitignore_path, "w") as f:
         f.write("*")
