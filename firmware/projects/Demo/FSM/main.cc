ae7217ea3a9322dc2b6f1c1c0516536fedcfc997
Everson <shene7@mcmaster.ca>
2024-11-08 22:45:23 -0500

    added hfsm states, but bugs exist within the input handling

--- firmware/projects/Demo/FSM/main.cc
+++ firmware/projects/Demo/FSM/main.cc
@@ -14,6 +14,9 @@ struct EventId {  // Initiates actions, transitions, or signals FSM, triggers
         START = 0,
         STOP = 1,
         STOPPED = 2,
+        SWITCH_D1 = 3,
+        SWITCH_D2 = 4,
+        SWITCH_D3 = 5,
     };
 };
 
@@ -22,6 +25,9 @@ struct StateId {  // Defines actions and responses to events
         IDLE = 0,
         RUNNING = 1,
         WINDING_DOWN = 2,
+        GEAR_D1 = 3,
+        GEAR_D2 = 4,
+        GEAR_D3 = 5,
     };
 };
 
@@ -42,6 +48,21 @@ class Stop : public etl::message<EventId::STOP> {
 public:
     Stop() {
     }
+};
+
+class Switch_D1 : public etl::message<EventId::SWITCH_D1> {
+public:
+    Switch_D1() {}
+};
+
+class Switch_D2 : public etl::message<EventId::SWITCH_D2> {
+public:
+    Switch_D2() {}
+};
+
+class Switch_D3 : public etl::message<EventId::SWITCH_D3> {
+public:
+    Switch_D3() {}
 };
 
 // Base state class for all motor states
@@ -88,11 +109,12 @@ public:
   }
 
 private:
+    typedef etl::message_packet<Start, Stop, Stopped, Switch_D1, Switch_D2,
+                                Switch_D3>
+        message_packet;
 
-  typedef etl::message_packet<Start, Stop, Stopped> message_packet;
-
-  // The queue of message items.
-  etl::queue<message_packet, 10> queue;
+    // The queue of message items.
+    etl::queue<message_packet, 10> queue;
 };
 
 class IdleState
@@ -135,9 +157,10 @@ public:
 
 // Running State
 class RunningState
-    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start, Stop, Stopped> {
+    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start,
+                            Stop, Stopped, Switch_D1, Switch_D2, Switch_D3> {
 public:
-        etl::fsm_state_id_t on_enter_state() override {
+    etl::fsm_state_id_t on_enter_state() override {
         cout << "Now in Running State\n";
         return StateId::RUNNING;
     }
@@ -158,8 +181,29 @@ public:
         return StateId::RUNNING;
     }
 
-        etl::fsm_state_id_t on_event(const Stopped& event){
+    etl::fsm_state_id_t on_event(const Stopped& event) {
         std::cout << "  S2 : Received incompatible message " << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D1& event) {
+        std::cout << "  S2 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D2& event) {
+        std::cout << "  S2 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::RUNNING;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D3& event) {
+        std::cout << "  S2 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
         cout.flush();
         return StateId::RUNNING;
     }
@@ -168,6 +212,165 @@ public:
         std::cout << "  S2 : Received unknown message " << int(event.get_message_id()) << std::endl;
         std::cout.flush();
         return StateId::RUNNING;
+    }
+};
+
+class GEAR_D1
+    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start,
+                            Stop, Stopped, Switch_D1, Switch_D2, Switch_D3> {
+    etl::fsm_state_id_t on_event(const Start& event) {
+        std::cout << "  SS1 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event(const Stop& event) {
+        std::cout << "  SS1 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::WINDING_DOWN;
+    }
+
+    etl::fsm_state_id_t on_event(const Stopped& event) {
+        std::cout << "  SS1 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D1& event) {
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D2& event) {
+        std::cout << "  SS1 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D2;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D3& event) {
+        std::cout
+            << "  SS1 : Received incompatible message "
+            << int(event.get_message_id())
+            << std::endl;  // Can't go from D1 to D3, has to be D1 -> D2 -> D3
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event_unknown(const etl::imessage& event) {
+        std::cout << "  SS1 : Received unknown message "
+                  << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::GEAR_D1;
+    }
+};
+
+class GEAR_D2
+    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start,
+                            Stop, Stopped, Switch_D1, Switch_D2, Switch_D3> {
+    etl::fsm_state_id_t on_event(const Start& event) {
+        std::cout << "  SS2 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D2;
+    }
+
+    etl::fsm_state_id_t on_event(const Stop& event) {
+        std::cout << "  SS2 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::WINDING_DOWN;
+    }
+
+    etl::fsm_state_id_t on_event(const Stopped& event) {
+        std::cout << "  SS2 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D2;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D1& event) {
+        std::cout << "  SS2 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D1;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D2& event) {
+        std::cout << "  SS2 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D2;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D3& event) {
+        std::cout << "  SS2 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D3;
+    }
+
+    etl::fsm_state_id_t on_event_unknown(const etl::imessage& event) {
+        std::cout << "  SS2 : Received unknown message "
+                  << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::GEAR_D2;
+    }
+};
+
+class GEAR_D3
+    : public etl::fsm_state<MotorControl, RunningState, StateId::RUNNING, Start,
+                            Stop, Stopped, Switch_D1, Switch_D2, Switch_D3> {
+    etl::fsm_state_id_t on_event(const Start& event) {
+        std::cout << "  SS3 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D3;
+    }
+
+    etl::fsm_state_id_t on_event(const Stop& event) {
+        std::cout << "  SS3 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::WINDING_DOWN;
+    }
+
+    etl::fsm_state_id_t on_event(const Stopped& event) {
+        std::cout << "  SS3 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D3;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D1& event) {
+        std::cout << "  SS3 : Received incompatible message "
+                  << int(event.get_message_id()) << std::endl;
+        cout.flush();
+        return StateId::GEAR_D3;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D2& event) {
+        std::cout << "  SS3 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D2;
+    }
+
+    etl::fsm_state_id_t on_event(const Switch_D3& event) {
+        std::cout << "  SS3 : Received message " << int(event.get_message_id())
+                  << std::endl;
+        cout.flush();
+        return StateId::GEAR_D3;
+    }
+
+    etl::fsm_state_id_t on_event_unknown(const etl::imessage& event) {
+        std::cout << "  SS3 : Received unknown message "
+                  << int(event.get_message_id()) << std::endl;
+        std::cout.flush();
+        return StateId::GEAR_D3;
     }
 };
 
@@ -229,6 +432,24 @@ void handleInput(MotorControl& motorControlFSM) {
                 etl::send_message(motorControlFSM, stopMsg);
                 std::cout << "Sent Stop message\n";
             } break;
+            case 'd':  // 'd' or 'D' to go into hfsm
+            case 'D': {
+                Switch_D1 gearD1;
+                etl::send_message(motorControlFSM, gearD1);
+                std::cout << "Sent D1 Gear Switch message\n";
+            } break;
+            case 'e':  //
+            case 'E': {
+                Switch_D2 gearD2;
+                etl::send_message(motorControlFSM, gearD2);
+                std::cout << "Sent D2 Gear Switch message\n";
+            } break;
+            case 'f':  //
+            case 'F': {
+                Switch_D3 gearD3;
+                etl::send_message(motorControlFSM, gearD3);
+                std::cout << "Sent D3 Gear Switch message\n";
+            } break;
             case 'a':  // 'x' key to send Stopped event
             case 'A': {
                 Stopped stoppedMsg;
@@ -258,13 +479,22 @@ int main() {
     RunningState runningState;
     Winding_DownState windingDownState;
 
+    GEAR_D1 gearD1;
+    GEAR_D2 gearD2;
+    GEAR_D3 gearD3;
+
     Start startMsg;          // Trigger start state
     Stopped stoppedMsg;      // Trigger stopped state
     Stop stopMsg;            // Trigger stop state
 
-    etl::ifsm_state* state_list[] = {&idleState, &runningState,
-                                     &windingDownState};
-    motorControlFSM.set_states(state_list, 3);
+    etl::ifsm_state* state_list[] = {&idleState,        &runningState,
+                                     &windingDownState, &gearD1,
+                                     &gearD2,           &gearD3};
+
+    etl::ifsm_state* childStates[] = {&gearD1, &gearD2, &gearD3};
+
+    motorControlFSM.set_states(state_list, 6);
+    runningState.set_child_states(childStates, 3);
     motorControlFSM.start();
 
     // Queue messages to control the FSM
