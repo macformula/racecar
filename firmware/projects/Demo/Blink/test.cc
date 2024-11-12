04c4cc296d777e3f4a16f3c6abfb936649befcf7
Cameron Beneteau <cameron.beneteau@gmail.com>
2024-11-12 16:44:02 -0500

    Add output on fail

--- firmware/projects/Demo/Blink/test.cc
+++ firmware/projects/Demo/Blink/test.cc
@@ -3,7 +3,7 @@
 #include "main.cc" // Include or import the function to test directly
 
 void test_add() {
-    assert(add(2, 3) == 6);
+    assert(add(2, 3) == 5);
     assert(add(-1, 1) == 0);
     assert(add(0, 0) == 0);
 }
