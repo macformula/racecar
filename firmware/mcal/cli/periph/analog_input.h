d1db8a9bf95ffdd3e15df682f6805a7456f9bd97
Samuel Shi <samuelshiwork@gmail.com>
2024-11-17 00:21:40 -0500

    primitive analog_input.h

---
+++ firmware/mcal/cli/periph/analog_input.h
@@ -0,0 +1,28 @@
+/// @author Samuel Shi 
+/// @date 2024-11-17
+
+#pragma once
+
+#include <iostream>
+#include "shared/periph/analog_input.h"
+
+namespace mcal::cli::periph{
+
+class AnalogInput : public shared::periph::AnalogInput {
+    void Start() override {
+        std::cout << "Reading ADC " << name_ << "..." << std::endl;
+    }
+
+    float Read() override {
+        Start();
+        float adc_val;
+        std::cout << " | Enter an unsigned 32-bit value: ";
+        std::cin >> adc_val;
+        std::cout << " | Obtained value " << adc_val << std::endl;
+        return adc_val;
+    }
+    private:
+        std::string name_;
+};
+
+}
