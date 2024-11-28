6f042d2198421f70458bee6f0e3c47ccb6751323
BlakeFreer <freerb@mcmaster.ca>
2024-11-27 20:00:00 -0500

    Separate toolchain script to avoid Makefile bug

---
+++ firmware/cmake/custom_toolchain.sh
@@ -0,0 +1,12 @@
+# Modify the CubeMX-generated toolchain file to work with out build system.
+# These commands are not in the Makefile since Make messes with the ${} characters
+# in the `sed` replacement string.
+
+# CubeMX forces CMake to skip its automatic compiler detection, which messes
+# with our clangd setup. Remove the line that does this.
+sed -i '/set(CMAKE.*COMPILER_FORCED.*$$/s/^/# /g' $1
+
+# CubeMX incorrectly assumes that our top-level CMakeLists.txt is in the same
+# directory as the .ioc file. This means that the linker script directory is wrong.
+# Find the "-T ** .ld" line and replace the CMAKE variable with the current directory.
+sed -i "/-T.*\.ld/s|\${CMAKE_SOURCE_DIR}|${PWD}|g" $1
