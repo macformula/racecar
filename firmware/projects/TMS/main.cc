9945c54bcb7fe3de24ee89c34ebf88eeae4ff4a1
BlakeFreer <freerb@mcmaster.ca>
2024-11-30 01:19:06 -0500

    refactor: Remove BmsBroadcaster Object
    Create the BmsBroadcast message in a standalone function so that we don't need to maintain another class. Moves the avg / low / high temperature calculations into this Pack() method to cut down the number of arguments.

--- firmware/projects/TMS/main.cc
+++ firmware/projects/TMS/main.cc
@@ -24,6 +24,16 @@ extern "C" {
 void UpdateTask(void* argument);
 }
 
+TempSensor temp_sensors[] = {
+    TempSensor{bindings::temp_sensor_adc_1, tempsensor::volt_stm_to_degC},
+    TempSensor{bindings::temp_sensor_adc_2, tempsensor::volt_stm_to_degC},
+    TempSensor{bindings::temp_sensor_adc_3, tempsensor::volt_stm_to_degC},
+    TempSensor{bindings::temp_sensor_adc_4, tempsensor::volt_stm_to_degC},
+    TempSensor{bindings::temp_sensor_adc_5, tempsensor::volt_stm_to_degC},
+    TempSensor{bindings::temp_sensor_adc_6, tempsensor::volt_stm_to_degC},
+};
+constexpr int kSensorCount = sizeof(temp_sensors) / sizeof(temp_sensors[0]);
+
 /// Spin the fan faster when the acculumator is hotter.
 const float fan_lut_data[][2] = {
     {19, 0},
@@ -35,60 +45,69 @@ constexpr int fan_lut_length =
     (sizeof(fan_lut_data) / (sizeof(fan_lut_data[0])));
 shared::util::LookupTable<fan_lut_length> fan_temp_lut{fan_lut_data};
 
-/***************************************************************
-    Create CAN objects
-***************************************************************/
+FanContoller fan_controller{bindings::fan_controller_pwm, fan_temp_lut, 2.0f};
 
 VehBus veh_can_bus{bindings::veh_can_base};
 
-/***************************************************************
-    Create app objects
-***************************************************************/
-FanContoller fan_controller{bindings::fan_controller_pwm, fan_temp_lut, 2.0f};
+TxBmsBroadcast PackBmsBroadcast(float temperatures[]) {
+    // This is a constant defined by Orion. It was discovered by
+    // decoding the CAN traffic coming from the Orion Thermal Expansion
+    // Pack.
+    const uint8_t kBmsChecksumConstant = 0x41;
+    const uint8_t kThermistorModuleNumber = 0;
 
-TempSensor temp_sensors[] = {
-    TempSensor{bindings::temp_sensor_adc_1, tempsensor::volt_stm_to_degC},
-    TempSensor{bindings::temp_sensor_adc_2, tempsensor::volt_stm_to_degC},
-    TempSensor{bindings::temp_sensor_adc_3, tempsensor::volt_stm_to_degC},
-    TempSensor{bindings::temp_sensor_adc_4, tempsensor::volt_stm_to_degC},
-    TempSensor{bindings::temp_sensor_adc_5, tempsensor::volt_stm_to_degC},
-    TempSensor{bindings::temp_sensor_adc_6, tempsensor::volt_stm_to_degC},
-};
-constexpr int kSensorCount = sizeof(temp_sensors) / sizeof(temp_sensors[0]);
+    uint8_t low_index;
+    int8_t low_temp =
+        static_cast<int8_t>(shared::util::GetMinimum<float, kSensorCount>(
+            temperatures, &low_index));
 
-BmsBroadcaster bms_broadcaster(veh_can_bus, kSensorCount);
+    uint8_t high_index;
+    int8_t high_temp =
+        static_cast<int8_t>(shared::util::GetMaximum<float, kSensorCount>(
+            temperatures, &high_index));
+
+    int8_t temp_avg = static_cast<int8_t>(
+        shared::util::GetAverage<float, kSensorCount>(temperatures));
+
+    uint8_t checksum = kThermistorModuleNumber + low_temp + high_temp +
+                       temp_avg + kSensorCount + high_index + low_index +
+                       kBmsChecksumConstant;
+
+    return TxBmsBroadcast{
+        .therm_module_num = kThermistorModuleNumber,
+        .low_therm_value = low_temp,
+        .high_therm_value = high_temp,
+        .avg_therm_value = temp_avg,
+        .num_therm_en = kSensorCount,
+        .high_therm_id = high_index,
+        .low_therm_id = low_index,
+        .checksum = checksum,
+    };
+}
 
 /***************************************************************
     Program Logic
 ***************************************************************/
-
 void Update() {
+    // Read the temperature sensors
     float temperatures[kSensorCount];
     for (int i = 0; i < kSensorCount; i++) {
         temperatures[i] = temp_sensors[i].Update();
     }
 
-    uint8_t low_thermistor_idx;
-    float temp_min = shared::util::GetMinimum<float, kSensorCount>(
-        temperatures, &low_thermistor_idx);
+    // Send the temperatures to the BMS
+    TxBmsBroadcast bms_broadcast = PackBmsBroadcast(temperatures);
+    veh_can_bus.Send(bms_broadcast);
 
-    uint8_t high_thermistor_idx;
-    float temp_max = shared::util::GetMaximum<float, kSensorCount>(
-        temperatures, &high_thermistor_idx);
-
-    float temp_avg =
+    // Adjust the fan speed based on the average temperature
+    float avg_temperature =
         shared::util::GetAverage<float, kSensorCount>(temperatures);
-
-    bms_broadcaster.SendBmsBroadcast(
-        high_thermistor_idx, static_cast<int8_t>(temp_max), low_thermistor_idx,
-        static_cast<int8_t>(temp_min), static_cast<int8_t>(temp_avg));
-
-    fan_controller.Update(temp_avg);
+    fan_controller.Update(avg_temperature);
 
     // Toggle the green LED to indicate the program is running
-    static bool debug_green = false;
-    bindings::debug_led_green.Set(debug_green);
-    debug_green = !debug_green;
+    static bool toggle = true;
+    bindings::debug_led_green.Set(toggle);
+    toggle = !toggle;
 }
 
 void UpdateTask(void* argument) {
