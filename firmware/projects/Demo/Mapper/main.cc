f1709eacf331a3c119a39cc2cc83948adfe20b9f
BlakeFreer <freerb@mcmaster.ca>
2024-12-28 20:24:21 -0500

    Templates LookupTable type. Adds LUT to Demo/Mapper

--- firmware/projects/Demo/Mapper/main.cc
+++ firmware/projects/Demo/Mapper/main.cc
@@ -1,10 +1,12 @@
 /// @author Blake Freer
 /// @date 2024-01-22
 
+#include <format>
 #include <iostream>  // for demo only, will not work on other platforms
 
 #include "shared/util/mappers/clamper.hpp"
 #include "shared/util/mappers/linear_map.hpp"
+#include "shared/util/mappers/lookup_table.hpp"
 #include "shared/util/mappers/mapper.hpp"
 
 using namespace shared::util;  // for demo only
@@ -15,15 +17,28 @@ Clamper<float> zero_to_ten{5, 10};
 CompositeMap comp1{zero_to_ten, double_plus_one};
 CompositeMap comp2{double_plus_one, comp1};
 
+// Lookup table example
+const float lut_data[][2]{
+    {0.f, 0.f},
+    {5.f, 2.f},
+    {8.f, 4.f},
+};
+constexpr int lut_length = (sizeof(lut_data)) / (sizeof(lut_data[0]));
+LookupTable<lut_length, float> lut{lut_data};
+
 int main(void) {
     for (float i = 0; i < 7; i += 0.7) {
-        float v1 = double_plus_one.Evaluate(i);
-        float v2 = comp1.Evaluate(i);
-        float v3 = comp2.Evaluate(i);
-
-        std::cout << i << " (x2 + 1) " << v1 << " (clamp) " << v2
-                  << " (x2 + 1) " << v3 << std::endl;
+        std::cout
+            << std::format(
+                   "{:.1f} (x2 + 1) {:.1f} (clamp) {:.1f} (x2 + 1) {:.1f}", i,
+                   double_plus_one.Evaluate(i), comp1.Evaluate(i),
+                   comp2.Evaluate(i))
+            << std::endl;
     }
 
+    for (float f = 0; f <= 10; f += 1.f) {
+        std::cout << std::format("{:.2f} -> LUT -> {:.2f}", f, lut.Evaluate(f))
+                  << std::endl;
+    }
     return 0;
 }
