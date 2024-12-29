de050dcef949bdb922b5aee42a33f9432055838a
BlakeFreer <freerb@mcmaster.ca>
2024-12-28 21:09:46 -0500

    Remove unnecessary static Evaluate() methods. Reorder inputs to CompositeMap constructor

--- firmware/shared/util/mappers/constant.hpp
+++ firmware/shared/util/mappers/constant.hpp
@@ -7,7 +7,7 @@
 
 namespace shared::util {
 
-/// @brief Evaluates the constant function `f(x) = c)`.
+/// @brief Evaluates the constant function `f(x) = c`.
 /// @tparam T Output type.
 /// @tparam U Input type.
 /// @note Always evaluates to the same value. Included for completeness.
@@ -16,13 +16,11 @@ class ConstantMap : public Mapper<T, U> {
 public:
     ConstantMap(T c) : c_(c) {}
 
-    static inline T Evaluate(U x, T c) {
-        return c;
-    }
-
     inline T Evaluate(U x) const override {
         return c_;
     }
+
+    // No static `Evaluate()` for obvious reasons
 
 private:
     const T c_;
