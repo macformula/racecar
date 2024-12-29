449eee0e3b52f7b047a245759f10830059aca175
BlakeFreer <freerb@mcmaster.ca>
2024-12-28 20:53:21 -0500

    Change to  comment style. Fix misleading include

--- firmware/shared/util/mappers/constant.hpp
+++ firmware/shared/util/mappers/constant.hpp
@@ -7,12 +7,10 @@
 
 namespace shared::util {
 
-/**
- * @brief Evaluates the constant function `f(x) = c)`.
- * @tparam T Output type.
- * @tparam U Input type.
- * @note Always evaluates to the same value. Included for completeness.
- */
+/// @brief Evaluates the constant function `f(x) = c)`.
+/// @tparam T Output type.
+/// @tparam U Input type.
+/// @note Always evaluates to the same value. Included for completeness.
 template <typename T, typename U = T>
 class ConstantMap : public Mapper<T, U> {
 public:
--- firmware/shared/util/mappers/mapper.hpp
+++ firmware/shared/util/mappers/mapper.hpp
@@ -3,16 +3,13 @@
 
 #pragma once
 
-#include <concepts>
-
+#include <type_traits>
 namespace shared::util {
 
-/**
- * @brief Evaluates a function.
- * @tparam T Output type, by default `float`.
- * @tparam U Input type, by default equal to `T`.
- * @note `T` and `U` must be numeric types.
- */
+/// @brief Evaluates a function.
+/// @tparam T Output type, by default `float`.
+/// @tparam U Input type, by default equal to `T`.
+/// @note `T` and `U` must be numeric types.
 template <typename T, typename U = T>
     requires(std::is_arithmetic_v<T>)
 class Mapper {
@@ -20,26 +17,20 @@ public:
     virtual T Evaluate(U x) const = 0;
 };
 
-/**
- * @brief Evaluates composed functions
- * @tparam Tf Output type of `f()`.
- * @tparam Tg Output type of `g()`.
- * @tparam U Input type to `g()`.
- */
+/// @brief Evaluates composed functions
+/// @tparam Tf Output type of `f()`.
+/// @tparam Tg Output type of `g()`.
+/// @tparam U Input type to `g()`.
 template <typename Tf, typename Tg = Tf, typename U = Tg>
 class CompositeMap : public Mapper<Tf, U> {
 public:
-    /**
-     * @param f Outer function.
-     * @param g Inner function.
-     * @note Evaulates `f(g(x))`, so `g` is applied before `f`.
-     */
+    /// @param f Outer function.
+    /// @param g Inner function.
+    /// @note Evaulates `f(g(x))`, so `g` is applied before `f`.
     CompositeMap(const Mapper<Tf, Tg>& f, const Mapper<Tg, U>& g)
         : f_(f), g_(g) {}
 
-    /**
-     * @brief Evaluates `f(g(x))`.
-     */
+    /// @brief Evaluates `f(g(x))`.
     static inline Tf Evaluate(Tg x, const Mapper<Tf, Tg>& f,
                               const Mapper<Tg, U>& g) {
         return f.Evaluate(g.Evaluate(x));
