21d387c774c8868a8d6032a969a6215fcb1c5c72
Samuel Shi <samuelshiwork@gmail.com>
2025-01-14 14:35:45 -0500

    Added string format cli

--- firmware/mcal/cli/periph/analog_input.hpp
+++ firmware/mcal/cli/periph/analog_input.hpp
@@ -1,20 +1,21 @@
-/// @author Samuel Shi 
+/// @author Samuel Shi
 /// @date 2024-11-17
 
 #pragma once
 
-#include <iostream>
-#include "shared/periph/analog_input.hpp"
 #include <format>
+#include <iostream>
 
-namespace mcal::cli::periph{
+#include "shared/periph/analog_input.hpp"
+
+namespace mcal::cli::periph {
 
 class AnalogInput : public shared::periph::AnalogInput {
 public:
     AnalogInput(std::string name) : name_(name) {}
 
     void Start() override {
-        std::cout << "Reading ADC " << name_ << "..." << std::endl;
+        std::cout << std::format("Reading ADC {} ...", name_) << std::endl;
     }
 
     float Read() override {
@@ -22,11 +23,13 @@ public:
         float voltage;
         std::cout << " | Enter a voltage level: ";
         std::cin >> voltage;
-        std::cout << std::format( " | Obtained value {:.3f}",voltage )  << std::endl;
-        return  voltage;
+        std::cout << std::format(" | Obtained value {:.3f} V", voltage)
+                  << std::endl;
+        return voltage;
     }
-    private:
-        std::string name_;
+
+private:
+    std::string name_;
 };
 
-}
+}  // namespace mcal::cli::periph
