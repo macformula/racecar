b0a17776cccd182a704cbb207371da6c1a44fd59
Tegh25 <teghveera@gmail.com>
2025-01-14 14:57:50 -0600

    Update tvFactor test cases

--- firmware/shared/controls/tvFactor_test.cc
+++ firmware/shared/controls/tvFactor_test.cc
@@ -14,20 +14,32 @@ int main() {
     ASSERT_CLOSE(CreateTorqueVectoringFactor(27.3), 0.683);
 
     {
-        TorqueVector<double> tv = AdjustTorqueVectoring(15.2);
+        TorqueVector<double> tv = AdjustTorqueVectoring(15.0);
         ASSERT_CLOSE(tv.left, 1.0);
-        ASSERT_CLOSE(tv.right, 3.6);
+        ASSERT_CLOSE(tv.right, 0.808);
     }
 
     {
-        TorqueVector<double> tv = AdjustTorqueVectoring(-13.6);
-        ASSERT_CLOSE(tv.left, 2.2);
+        TorqueVector<double> tv = AdjustTorqueVectoring(-10.0);
+        ASSERT_CLOSE(tv.left, 0.87);
         ASSERT_CLOSE(tv.right, 1);
     }
 
     {
         TorqueVector<double> tv = AdjustTorqueVectoring(0.0);
         ASSERT_CLOSE(tv.left, 1);
+        ASSERT_CLOSE(tv.right, 1);
+    }
+
+    {
+        TorqueVector<double> tv = AdjustTorqueVectoring(13.8);
+        ASSERT_CLOSE(tv.left, 1);
+        ASSERT_CLOSE(tv.right, 0.82288);
+    }
+
+    {
+        TorqueVector<double> tv = AdjustTorqueVectoring(-6.7);
+        ASSERT_CLOSE(tv.left, 0.91224);
         ASSERT_CLOSE(tv.right, 1);
     }
 
