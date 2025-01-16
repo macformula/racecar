1235f6c18604d29551b95c12a94a411e2c79bfbf
luaibash <luai.hawa@gmail.com>
2025-01-15 22:36:59 -0500

    Added majority of state machine logic

--- firmware/projects/AmkBlock/inc/amk_block.hpp
+++ firmware/projects/AmkBlock/inc/amk_block.hpp
@@ -3,7 +3,7 @@
 #include "../generated/can/pt_can_messages.hpp"
 #include <concepts>
 
-// MiStatus enum class used in AmkInput
+// MiStatus enum class used in AmkOutput
 enum class MiStatus {
     UNKNOWN,
     INIT,
@@ -101,9 +101,9 @@ enum class AmkStates {
     STARTUP,
     READY,
     RUNNING,
+    SHUTDOWN,
     ERROR_DETECTED,
-    ERROR_RESET,
-    SHUTDOWN
+    ERROR_RESET
 };
 
 class AmkBlock {
@@ -131,33 +131,97 @@ private:
     template<AmkActualValues1 V1, AmkActualValues2 V2, SetPoint SP>
     UpdateMotorOutput<SP> UpdateMotor(const V1 val1, const V2 val2, const MotorInput motor_input, const MiCmd cmd) {
         UpdateMotorOutput<SP> update_motor_output;
+        SP set_point;
 
         switch (amk_state) {
             case AmkStates::MOTOR_OFF_WAITING_FOR_GOV:
+                update_motor_output.status = MiStatus::OFF;
+                update_motor_output.inverter = 0;
+
+                set_point.amk_b_inverter_on = 0;
+                set_point.amk_b_dc_on = 0;
+                set_point.amk_b_enable = 0;
+                set_point.amk_b_error_reset = 0;
+                set_point.amk__target_velocity = 0;
+                set_point.amk__torque_limit_positiv = 0;
+                set_point.amk__torque_limit_negativ = 0;
+
+                if (cmd == MiCmd::STARTUP) {
+                    amk_state = AmkStates::STARTUP;
+                }
 
                 break;
 
             case AmkStates::STARTUP:
+                // Create new function for nested state machine block
+                
+                if (val1.amk_b_error == 1) { // Check if this is correct logic since arrow has no logic
+                    amk_state = AmkStates::ERROR_DETECTED;
+                }
+                else if (val1.amk_b_inverter_on == 1) {
+                    amk_state = AmkStates::READY;
+                }
 
                 break;
 
             case AmkStates::READY:
+                update_motor_output.status = MiStatus::READY;
+                update_motor_output.inverter = 0;
+                
+                if (val1.amk_b_error == 1) {
+                    amk_state = AmkStates::ERROR_DETECTED;
+                }
+                else if (val1.amk_b_quit_inverter_on == 1) {
+                    amk_state = AmkStates::RUNNING;
+                }
 
                 break;
 
             case AmkStates::RUNNING:
+                update_motor_output.status = MiStatus::RUNNING;
 
-                break;
+                set_point.amk__target_velocity = motor_input.speed_request;
+                set_point.amk__torque_limit_positiv = motor_input.torque_limit_positive;
+                set_point.amk__torque_limit_negativ = motor_input.torque_limit_negative;
 
-            case AmkStates::ERROR_DETECTED:
-
-                break;
-
-            case AmkStates::ERROR_RESET:
+                if (val1.amk_b_error == 1) {
+                    amk_state = AmkStates::ERROR_DETECTED;
+                }
+                else if (cmd == MiCmd::SHUTDOWN) {
+                    amk_state == AmkStates::SHUTDOWN;
+                }
 
                 break;
 
             case AmkStates::SHUTDOWN:
+                update_motor_output.inverter = 0;
+
+                set_point.amk__target_velocity = 0;
+                set_point.amk__torque_limit_positiv = 0;
+                set_point.amk__torque_limit_negativ = 0;
+                // Rest of setpoint values do not get set
+                // I'm thinking we have to have private var hold previous output, set SP set_point = previous_set_point at start of UpdateMotor
+                // This way on every call of UpdateMotor, if an output value is not set, it's cause the prev is retained
+
+                // Set some time condition with after(500, msec)
+
+                break;
+
+            case AmkStates::ERROR_DETECTED:
+                update_motor_output.status = MiStatus::ERROR;
+
+                if (cmd == MiCmd::ERR_RESET) {
+                    amk_state = AmkStates::ERROR_RESET;
+                }
+
+                break;
+
+            case AmkStates::ERROR_RESET:
+                // Create new function for nested state machine block
+
+                if (val1.amk_b_system_ready == 1) {
+                    amk_state = AmkStates::MOTOR_OFF_WAITING_FOR_GOV;
+                }
 
                 break;
         }
