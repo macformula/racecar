0812aabd8d2fbdb32c65552c27d00fcdf4801a83
luaibash <luai.hawa@gmail.com>
2025-01-16 22:16:02 -0500

    Added concepts enforcement, formatting, and new amk states

--- firmware/projects/AmkBlock/inc/amk_block.hpp
+++ firmware/projects/AmkBlock/inc/amk_block.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "../generated/can/pt_can_messages.hpp"
 #include <concepts>
+
+#include "../generated/can/pt_can_messages.hpp"
 
 // MiStatus enum class used in AmkOutput
 enum class MiStatus {
@@ -24,7 +25,7 @@ enum class MiCmd {
     ERR_RESET
 };
 
-// MotorInput struct used in AmkInput, this is defined to encapsulate identical values for left and right
+// MotorInput struct used in AmkInput to group identical left/right values
 struct MotorInput {
     double speed_request;
     double torque_limit_negative;
@@ -50,45 +51,49 @@ struct AmkOutput {
     bool inverter;
 };
 
-// AmkActualValues1 concept, combines AMK0_ActualValues1 and AMK1_ActualValues1 into one common type for easier use
+// AmkActualValues1 concept, combines AMK0_ActualValues1 and AMK1_ActualValues1
+// into one common type for easier use
 template <typename MsgType>
 concept AmkActualValues1 = requires(MsgType msg) {
-    { msg.amk_b_system_ready }; // TODO: Add the std::same_as<bool> check, currently it is erroring out
-    { msg.amk_b_error };
-    { msg.amk_b_warn };
-    { msg.amk_b_quit_dc_on };
-    { msg.amk_b_dc_on };
-    { msg.amk_b_quit_inverter_on };
-    { msg.amk_b_inverter_on };
-    { msg.amk_b_derating };
-    { msg.amk__actual_velocity };
-    { msg.amk__torque_current };
-    { msg.amk__magnetizing_current };
+    { msg.amk_b_system_ready } -> std::convertible_to<bool>;
+    { msg.amk_b_error } -> std::convertible_to<bool>;
+    { msg.amk_b_warn } -> std::convertible_to<bool>;
+    { msg.amk_b_quit_dc_on } -> std::convertible_to<bool>;
+    { msg.amk_b_dc_on } -> std::convertible_to<bool>;
+    { msg.amk_b_quit_inverter_on } -> std::convertible_to<bool>;
+    { msg.amk_b_inverter_on } -> std::convertible_to<bool>;
+    { msg.amk_b_derating } -> std::convertible_to<bool>;
+    { msg.amk__actual_velocity } -> std::convertible_to<int16_t>;
+    { msg.amk__torque_current } -> std::convertible_to<int16_t>;
+    { msg.amk__magnetizing_current } -> std::convertible_to<int16_t>;
 };
 
-// AmkActualValues2 concept, combines AMK0_ActualValues2 and AMK1_ActualValues2 into one common type for easier use
-template<typename MsgType>
+// AmkActualValues2 concept, combines AMK0_ActualValues2 and AMK1_ActualValues2
+// into one common type for easier use
+template <typename MsgType>
 concept AmkActualValues2 = requires(MsgType msg) {
-    { msg.amk__temp_motor };
-    { msg.amk__temp_inverter };
-    { msg.amk__error_info };
-    { msg.amk__temp_igbt };
+    { msg.amk__temp_motor } -> std::convertible_to<float>;
+    { msg.amk__temp_inverter } -> std::convertible_to<float>;
+    { msg.amk__error_info } -> std::convertible_to<uint16_t>;
+    { msg.amk__temp_igbt } -> std::convertible_to<float>;
 };
 
-// SetPoint concept, combines AMK0_SetPoints1 and AMK1_SetPoints1 into one common type for easier use
-template<typename MsgType>
+// SetPoint concept, combines AMK0_SetPoints1 and AMK1_SetPoints1 into one
+// common type for easier use
+template <typename MsgType>
 concept SetPoint = requires(MsgType msg) {
-    { msg.amk_b_inverter_on };
-    { msg.amk_b_dc_on };
-    { msg.amk_b_enable };
-    { msg.amk_b_error_reset };
-    { msg.amk__target_velocity };
-    { msg.amk__torque_limit_positiv };
-    { msg.amk__torque_limit_negativ };
+    { msg.amk_b_inverter_on } -> std::convertible_to<bool>;
+    { msg.amk_b_dc_on } -> std::convertible_to<bool>;
+    { msg.amk_b_enable } -> std::convertible_to<bool>;
+    { msg.amk_b_error_reset } -> std::convertible_to<bool>;
+    { msg.amk__target_velocity } -> std::convertible_to<int16_t>;
+    { msg.amk__torque_limit_positiv } -> std::convertible_to<int16_t>;
+    { msg.amk__torque_limit_negativ } -> std::convertible_to<int16_t>;
 };
 
-// UpdateMotorOutput struct, used to create a return type for UpdateMotor such that it does not matter if set_point is for left or right
-template<SetPoint SP>
+// UpdateMotorOutput struct, used to create a return type for UpdateMotor such
+// that it does not matter if set_point is for left or right
+template <SetPoint SP>
 struct UpdateMotorOutput {
     SP set_point;
     MiStatus status;
@@ -99,27 +104,43 @@ struct UpdateMotorOutput {
 enum class AmkStates {
     MOTOR_OFF_WAITING_FOR_GOV,
     STARTUP,
+    STARTUP_SYS_READY,
+    STARTUP_TOGGLE_D_CON,
+    STARTUP_ENFORCE_SETPOINTS_ZERO,
+    STARTUP_COMMAND_ON,
     READY,
     RUNNING,
     SHUTDOWN,
     ERROR_DETECTED,
-    ERROR_RESET
+    ERROR_RESET,
+    ERROR_RESET_ENFORCE_SETPOINTS_ZERO,
+    ERROR_RESET_TOGGLE_ENABLE,
+    ERROR_RESET_SEND_RESET,
+    ERROR_RESET_TOGGLE_RESET
 };
 
 class AmkBlock {
 public:
-    AmkBlock(AmkStates initial_state) : amk_state(initial_state) {}
+    AmkBlock(AmkStates initial_amk_state = AmkStates::MOTOR_OFF_WAITING_FOR_GOV)
+        : amk_state(initial_amk_state) {}
 
     AmkOutput update(const AmkInput& input, int time_ms) {
-        UpdateMotorOutput<generated::can::AMK0_SetPoints1> left_update_motor_output = UpdateMotor<generated::can::AMK0_ActualValues1, generated::can::AMK0_ActualValues2, generated::can::AMK0_SetPoints1>(input.amk_actual_values_1_left, input.amk_actual_values_2_left, input.left_motor_input, input.cmd);
-        UpdateMotorOutput<generated::can::AMK1_SetPoints1> right_update_motor_output = UpdateMotor<generated::can::AMK1_ActualValues1, generated::can::AMK1_ActualValues2, generated::can::AMK1_SetPoints1>(input.amk_actual_values_1_right, input.amk_actual_values_2_right, input.right_motor_input, input.cmd);
+        using namespace generated::can;
 
-        AmkOutput output {
-            .status = process_output_status(left_update_motor_output.status, right_update_motor_output.status),
-            .amk_setpoints_1_left = left_update_motor_output.set_point,
-            .amk_setpoints_1_right = right_update_motor_output.set_point,
-            .inverter = left_update_motor_output.inverter && right_update_motor_output.inverter
-        };
+        auto left = UpdateMotor<AMK0_ActualValues1, AMK0_ActualValues2,
+                                AMK0_SetPoints1>(
+            input.amk_actual_values_1_left, input.amk_actual_values_2_left,
+            input.left_motor_input, input.cmd);
+        auto right = UpdateMotor<AMK1_ActualValues1, AMK1_ActualValues2,
+                                 AMK1_SetPoints1>(
+            input.amk_actual_values_1_right, input.amk_actual_values_2_right,
+            input.right_motor_input, input.cmd);
+
+        AmkOutput output{
+            .status = ProcessOutputStatus(left.status, right.status),
+            .amk_setpoints_1_left = left.set_point,
+            .amk_setpoints_1_right = right.set_point,
+            .inverter = left.inverter && right.inverter};
 
         return output;
     }
@@ -128,8 +149,10 @@ private:
     AmkStates amk_state;
     MiStatus previous_state_status = MiStatus::OFF;
 
-    template<AmkActualValues1 V1, AmkActualValues2 V2, SetPoint SP>
-    UpdateMotorOutput<SP> UpdateMotor(const V1 val1, const V2 val2, const MotorInput motor_input, const MiCmd cmd) {
+    template <AmkActualValues1 V1, AmkActualValues2 V2, SetPoint SP>
+    UpdateMotorOutput<SP> UpdateMotor(const V1 val1, const V2 val2,
+                                      const MotorInput motor_input,
+                                      const MiCmd cmd) {
         UpdateMotorOutput<SP> update_motor_output;
         SP set_point;
 
@@ -153,12 +176,10 @@ private:
                 break;
 
             case AmkStates::STARTUP:
-                // Create new function for nested state machine block
-                
-                if (val1.amk_b_error == 1) { // Check if this is correct logic since arrow has no logic
+
+                if (val1.amk_b_error == 1) {
                     amk_state = AmkStates::ERROR_DETECTED;
-                }
-                else if (val1.amk_b_inverter_on == 1) {
+                } else if (val1.amk_b_inverter_on == 1) {
                     amk_state = AmkStates::READY;
                 }
 
@@ -167,11 +188,10 @@ private:
             case AmkStates::READY:
                 update_motor_output.status = MiStatus::READY;
                 update_motor_output.inverter = 0;
-                
+
                 if (val1.amk_b_error == 1) {
                     amk_state = AmkStates::ERROR_DETECTED;
-                }
-                else if (val1.amk_b_quit_inverter_on == 1) {
+                } else if (val1.amk_b_quit_inverter_on == 1) {
                     amk_state = AmkStates::RUNNING;
                 }
 
@@ -181,13 +201,14 @@ private:
                 update_motor_output.status = MiStatus::RUNNING;
 
                 set_point.amk__target_velocity = motor_input.speed_request;
-                set_point.amk__torque_limit_positiv = motor_input.torque_limit_positive;
-                set_point.amk__torque_limit_negativ = motor_input.torque_limit_negative;
+                set_point.amk__torque_limit_positiv =
+                    motor_input.torque_limit_positive;
+                set_point.amk__torque_limit_negativ =
+                    motor_input.torque_limit_negative;
 
                 if (val1.amk_b_error == 1) {
                     amk_state = AmkStates::ERROR_DETECTED;
-                }
-                else if (cmd == MiCmd::SHUTDOWN) {
+                } else if (cmd == MiCmd::SHUTDOWN) {
                     amk_state == AmkStates::SHUTDOWN;
                 }
 
@@ -199,11 +220,6 @@ private:
                 set_point.amk__target_velocity = 0;
                 set_point.amk__torque_limit_positiv = 0;
                 set_point.amk__torque_limit_negativ = 0;
-                // Rest of setpoint values do not get set
-                // I'm thinking we have to have private var hold previous output, set SP set_point = previous_set_point at start of UpdateMotor
-                // This way on every call of UpdateMotor, if an output value is not set, it's cause the prev is retained
-
-                // Set some time condition with after(500, msec)
 
                 break;
 
@@ -217,7 +233,6 @@ private:
                 break;
 
             case AmkStates::ERROR_RESET:
-                // Create new function for nested state machine block
 
                 if (val1.amk_b_system_ready == 1) {
                     amk_state = AmkStates::MOTOR_OFF_WAITING_FOR_GOV;
@@ -229,15 +244,13 @@ private:
         return update_motor_output;
     }
 
-    MiStatus process_output_status(MiStatus left_motor_status, MiStatus right_motor_status) {
+    MiStatus ProcessOutputStatus(MiStatus left_status, MiStatus right_status) {
         MiStatus output_status;
-        if (left_motor_status == MiStatus::ERROR || right_motor_status == MiStatus::ERROR) {
+        if (left_status == MiStatus::ERROR || right_status == MiStatus::ERROR) {
             output_status = MiStatus::ERROR;
-        }
-        else if (left_motor_status == right_motor_status) {
-            output_status = left_motor_status;
-        }
-        else {
+        } else if (left_status == right_status) {
+            output_status = left_status;
+        } else {
             output_status = previous_state_status;
         }
 
