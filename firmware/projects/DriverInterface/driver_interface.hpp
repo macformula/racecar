413eae436f50a7bd53a0ce261660219c5f4ec8ed
luaibash <luai.hawa@gmail.com>
2025-01-19 14:49:33 -0500

    Fixed struct definitions for driver interface header, co-authored by ManushPatell

--- firmware/projects/DriverInterface/driver_interface.hpp
+++ firmware/projects/DriverInterface/driver_interface.hpp
@@ -1,44 +1,36 @@
 #pragma once
 
-enum class DiCmd {
-    GOV_E_DICMD = 8
-};
-
-enum class DiSts {
-    GOV_E_DISTS = 7
-};
-
-// The simulink block has multiple inputs and outputs. We use `struct` to bind
-// them all together. Define the input and output structs
+#include "shared/controls/driver_interface_fsm.hpp"
 
 struct DiInput {
     DiCmd di_cmd;
-    float brake_pedal_position;
+    float rear_brake_pedal_position;
+    float front_brake_pedal_position;
     bool driver_button;
-    float steering_angle;
     float accel_pedal_pos1;
     float accel_pedal_pos2;
+    float steering_angle;
 };
 
 struct DiOutput {
+    DiSts di_sts;
     float driver_torque_req;
     float steering_angle;
     float brake_pedal_position;
     bool driver_speaker;
     bool brake_light_en;
-    // fill this out
 };
 
+// Todo: check if this is needed or not
 struct DriverInterfaceOutput {
     bool ready_to_drive;
     bool driver_speaker;
-    float brake_pedal_position =
-        std::max(input.front_brake_pressure, input.rear_brake_pressure);
-}
+    float brake_pedal_position;
+};
 
 class DriverInterface {
 public:
-    // Do not change the public interface
+    DriverInterface();
     DiOutput Update(DiInput input, int time_ms);
 
 private:
