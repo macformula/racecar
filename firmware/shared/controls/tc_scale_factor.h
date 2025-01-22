162e3d2e0c2f1f175a263180eb56723ad6917ada
Tegh25 <teghveera@gmail.com>
2025-01-21 21:16:21 -0500

    Fixed clang formatting tc_scale_factor.h

--- firmware/shared/controls/tc_scale_factor.h
+++ firmware/shared/controls/tc_scale_factor.h
@@ -13,7 +13,9 @@ T CalculateActualSlip(T left_rear_wheel_speed, T right_rear_wheel_speed,
     T idle_wheel_spd = (left_front_wheel_speed + right_front_wheel_speed) / 2.0;
     T actual_slip;
 
-    actual_slip = std::max(left_rear_wheel_speed, right_rear_wheel_speed) / idle_wheel_spd - 1;
+    actual_slip = std::max(left_rear_wheel_speed, right_rear_wheel_speed) /
+                      idle_wheel_spd -
+                  1;
 
     if (actual_slip < 0) {
         actual_slip = 0;
