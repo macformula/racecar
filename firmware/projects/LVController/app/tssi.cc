6efaba52cbd1b7cba551d2bfd700cb8d6c15e7d9
Blake Freer <59676067+BlakeFreer@users.noreply.github.com>
2025-01-31 00:49:14 -0500

    LV for EV6 (#343)
    * Remove LVIoCheckout project
    
    * Minimal LV Update for new CAN
    
    * refactor: Clean up LV Controller
    
    Remove some old abstractions like Indicator and StateBroadcaster to simplify the code. Move state broadcasts out of loops to prevent message floods. Update IsContactors functions to be more functional on the optional datatype to highlight how they may have empty data.
    
    Replace auto in lambda expr with explicit CAN Msg type
    
    Replace transform with if statement since it is easier to read
    
    * Fix incorrect state broadcast
    
    * Break out app/inc.hpp into component header & src files
    
    * Removed unused Log function
    
    * Make inverter_cmd more explicit
    
    * fix: Update IOC for EV6
    
    Actually enables the Accumulator (fr this time). Remove DCDC LED - status will be shown on dashboard. Renames `Inverter` to `Motor Control Switch`. Continually sends status messages while waiting on DCDC and other blockers.
    
    * refactor: Implement LV as a statemachine.
    
    Replace the DoPowerup...() functions with a state machine. Enables simultaneous behaviour like checking the inverter message since blocking delays are no longer used. Rewrites the Fan driver to use absolute time instead of elapsed time.
    
    * Combine LV switches. Describe the tradeoff in a comment.
    
    * Start updating for EV6. Rename TSAL -> TSSI
    
    * Add suspension travel sensors
    
    * LV Controller for EV6. Subsystems and statemachine
    
    * Add float specifier
    
    * Invert TSSI light logic
    
    * Remove SIL Platform for now
    
    * Impelment logic for ev5. Use dummy IO when pins don't exist.
    
    * Implement CLI. Only disable power on first run of
    `PWRUP_START`.
    
    * Explicitly construct unused peripherals from ev5 ioc
    
    * Reset CAN baud to 500k

---
+++ firmware/projects/LVController/app/tssi.cc
@@ -0,0 +1,37 @@
+#include "tssi.hpp"
+
+TSSI::TSSI(shared::periph::DigitalOutput& enable,
+           shared::periph::DigitalOutput& green,
+           shared::periph::DigitalOutput& red)
+    : enable_(enable), green_(green), red_(red) {}
+
+void TSSI::Enable() {
+    enable_.SetHigh();
+}
+void TSSI::Disable() {
+    enable_.SetLow();
+}
+
+/// @brief See FSAE 2025 Rules EV.5.11.5
+void TSSI::Update(bool bms_fault, bool imd_fault, int time_ms) {
+    const float kFlashFrequencyHz = 4.0f;  // must be between 2Hz - 5 Hz
+    // /2 since one period is two toggles
+    const int kTogglePeriodMs = 1000.f / kFlashFrequencyHz / 2.f;
+
+    bool fault = bms_fault || imd_fault;
+
+    // Both red and green are inverted logic
+    if (!fault) {
+        red_.SetHigh();
+        green_.SetLow();
+        state_entered_time_ = time_ms;
+        flash_toggle_ = false;
+    } else {
+        green_.SetHigh();
+        red_.Set(flash_toggle_);
+        if (time_ms - state_entered_time_ > kTogglePeriodMs) {
+            state_entered_time_ = time_ms;
+            flash_toggle_ = !flash_toggle_;
+        }
+    }
+}
