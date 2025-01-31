8e60700b6b12c248c9fb8703a6e9b2bff6ce6704
BlakeFreer <freerb@mcmaster.ca>
2025-01-31 01:25:13 -0500

    TMS Merge fixes

--- firmware/projects/TMS/platforms/cli/bindings.cc
+++ firmware/projects/TMS/platforms/cli/bindings.cc
@@ -1,7 +1,8 @@
+#include "../../bindings.hpp"
+
 #include <iostream>
 
-#include "../../bindings.hpp"
-#include "mcal/cli/periph/adc.hpp"
+#include "mcal/cli/periph/analog_input.hpp"
 #include "mcal/cli/periph/can.hpp"
 #include "mcal/cli/periph/gpio.hpp"
 #include "mcal/cli/periph/pwm.hpp"
@@ -9,12 +10,12 @@
 namespace mcal {
 using namespace cli::periph;
 
-ADCInput temp_sensor_adc_1("Temp Sensor 1");
-ADCInput temp_sensor_adc_2("Temp Sensor 2");
-ADCInput temp_sensor_adc_3("Temp Sensor 3");
-ADCInput temp_sensor_adc_4("Temp Sensor 4");
-ADCInput temp_sensor_adc_5("Temp Sensor 5");
-ADCInput temp_sensor_adc_6("Temp Sensor 6");
+AnalogInput temp_sensor_adc_1("Temp Sensor 1");
+AnalogInput temp_sensor_adc_2("Temp Sensor 2");
+AnalogInput temp_sensor_adc_3("Temp Sensor 3");
+AnalogInput temp_sensor_adc_4("Temp Sensor 4");
+AnalogInput temp_sensor_adc_5("Temp Sensor 5");
+AnalogInput temp_sensor_adc_6("Temp Sensor 6");
 
 PWMOutput fan_controller_pwm("Fan");
 DigitalOutput debug_led_green("Green LED");
@@ -26,12 +27,12 @@ CanBase veh_can_base("Vehicle CAN");
 
 namespace bindings {
 
-shared::periph::ADCInput& temp_sensor_adc_1 = mcal::temp_sensor_adc_1;
-shared::periph::ADCInput& temp_sensor_adc_2 = mcal::temp_sensor_adc_2;
-shared::periph::ADCInput& temp_sensor_adc_3 = mcal::temp_sensor_adc_3;
-shared::periph::ADCInput& temp_sensor_adc_4 = mcal::temp_sensor_adc_4;
-shared::periph::ADCInput& temp_sensor_adc_5 = mcal::temp_sensor_adc_5;
-shared::periph::ADCInput& temp_sensor_adc_6 = mcal::temp_sensor_adc_6;
+shared::periph::AnalogInput& temp_sensor_adc_1 = mcal::temp_sensor_adc_1;
+shared::periph::AnalogInput& temp_sensor_adc_2 = mcal::temp_sensor_adc_2;
+shared::periph::AnalogInput& temp_sensor_adc_3 = mcal::temp_sensor_adc_3;
+shared::periph::AnalogInput& temp_sensor_adc_4 = mcal::temp_sensor_adc_4;
+shared::periph::AnalogInput& temp_sensor_adc_5 = mcal::temp_sensor_adc_5;
+shared::periph::AnalogInput& temp_sensor_adc_6 = mcal::temp_sensor_adc_6;
 
 shared::periph::PWMOutput& fan_controller_pwm = mcal::fan_controller_pwm;
 shared::periph::DigitalOutput& debug_led_green = mcal::debug_led_green;
