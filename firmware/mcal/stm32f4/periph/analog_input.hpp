1222322e0526368dac6e28ca4222e36087ecf71a
BlakeFreer <freerb@mcmaster.ca>
2025-02-05 17:13:05 -0500

    actually add the files

---
+++ firmware/mcal/stm32f4/periph/analog_input.hpp
@@ -0,0 +1,60 @@
+
+
+#pragma once
+
+#include <cstdint>
+
+#include "shared/periph/analog_input.hpp"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_adc.h"
+
+namespace mcal::stm32f4::periph {
+
+class AnalogInput : public shared::periph::AnalogInput {
+public:
+    AnalogInput(ADC_HandleTypeDef* hadc, uint32_t adc_channel,
+                float system_volts = 3.3f)
+        : hadc_(hadc), adc_channel_(adc_channel), system_volts_(system_volts){};
+
+    float ReadVoltage() override {
+        Start();
+        HAL_ADC_PollForConversion(hadc_, 1000);
+        uint32_t adc_value = HAL_ADC_GetValue(hadc_);
+        HAL_ADC_Stop(hadc_);
+
+        return adc_value / GetDivisor() * system_volts_;
+    }
+
+private:
+    ADC_HandleTypeDef* hadc_;
+    uint32_t adc_channel_;
+    const float system_volts_;
+
+    void Start() {
+        ADC_ChannelConfTypeDef adc_config = {
+            .Channel = adc_channel_,
+            .Rank = ADC_REGULAR_RANK_1,
+            .SamplingTime = ADC_SAMPLETIME_28CYCLES,
+            .Offset = 0};
+
+        HAL_ADC_ConfigChannel(hadc_, &adc_config);
+        HAL_ADC_Start(hadc_);
+    }
+
+    float GetDivisor() {
+        switch (ADC_GET_RESOLUTION(hadc_)) {
+            case ADC_RESOLUTION_6B:
+                return 63.0f;
+            case ADC_RESOLUTION_8B:
+                return 255.0f;
+            case ADC_RESOLUTION_10B:
+                return 1023.0f;
+            case ADC_RESOLUTION_12B:
+                return 4095.0f;
+            default:
+                return 4095.0f;
+        }
+    };
+};
+
+}  // namespace mcal::stm32f4::periph
---
+++ firmware/mcal/stm32f4/periph/gpio.hpp
@@ -0,0 +1,47 @@
+/// @author Blake Freer
+/// @date 2023-11-08
+
+#pragma once
+
+#include <cstdint>
+
+#include "shared/periph/gpio.hpp"
+#include "stm32f4xx_hal.h"
+
+namespace mcal::stm32f4::periph {
+
+class DigitalInput : public shared::periph::DigitalInput {
+private:
+    GPIO_TypeDef* port_;
+    uint16_t pin_;
+
+public:
+    DigitalInput(GPIO_TypeDef* gpio_port, uint16_t pin)
+        : port_(gpio_port), pin_(pin) {}
+
+    bool Read() override {
+        return HAL_GPIO_ReadPin(port_, pin_);
+    }
+};
+
+class DigitalOutput : public shared::periph::DigitalOutput {
+private:
+    GPIO_TypeDef* port_;
+    uint16_t pin_;
+
+public:
+    DigitalOutput(GPIO_TypeDef* gpio_port, uint16_t pin)
+        : port_(gpio_port), pin_(pin) {}
+
+    void SetHigh() override {
+        HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_SET);
+    }
+    void SetLow() override {
+        HAL_GPIO_WritePin(port_, pin_, GPIO_PIN_RESET);
+    }
+    void Set(bool value) override {
+        HAL_GPIO_WritePin(port_, pin_, static_cast<GPIO_PinState>(value));
+    }
+};
+
+}  // namespace mcal::stm32f4::periph
