6ca5823c8229205745654c36010e35dd89387b21
Samuel Parent <108418256+samparent97@users.noreply.github.com>
2025-02-06 11:35:29 -0500

    Move dbcs out of the project folder (#7)

--- firmware/shared/util/device.hpp
+++ firmware/shared/util/device.hpp
@@ -5,18 +5,6 @@
 
 namespace shared::util {
 
-/**
- * This class provides a generic parent that all devices will inherit from.
- * It is currently empty but it still included for future use.
- *
- * Inheritance is required by the concept constraint
- * `std::is_base_of_v<util::Device, T>;`
- * See `shared/device/dac.hpp` for an example.
- *
- * Inheritance is performed in concept implementation
- * `class DigitalAnalogConverter : public shared::util::Device`
- * See `shared/device/dac.hpp` for an example
- */
 class Device {};
 
 }  // namespace shared::util
