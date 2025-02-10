744f8b726c007aac48461bc2a1928a7c0e445f32
luaibash <luai.hawa@gmail.com>
2025-02-10 00:25:09 -0500

    Added majority of update logic for control blocks

--- firmware/projects/FrontController/main.cc
+++ firmware/projects/FrontController/main.cc
@@ -50,47 +50,6 @@ AnalogInput steering_wheel{bindings::steering_angle_sensor,
                            &steering_wheel_map};
 
 /***************************************************************
-    Control System
-***************************************************************/
-
-auto pedal_to_torque = LinearMap<float, float>{1, 0};
-
-using MotorIface = MotorInterface<RxAMK0_ActualValues1, RxAMK1_ActualValues1,
-                                  TxAMK0_SetPoints1, TxAMK0_SetPoints1>;
-MotorIface mi;
-BatteryMonitor bm;
-Governor gov;
-DriverInterface di;
-VehicleDynamics vd{pedal_to_torque};
-
-Governor::Input gov_in;
-
-void UpdateControls() {
-    // cmd always from governor to module
-    // // sts always from module to gov
-
-    // MotorIface::Input mi_in{
-    //     // cmd from gov
-    //     .left_actual1 = pt_can_bus.GetRxAMK0_ActualValues1().value(),
-    //     //.request come from DI and VD
-    // };
-
-    // int time_ms = os::GetTickCount();
-    // // gov update
-    // Governor::Output out = gov.Update(gov_in, tims_ms);
-    // // battery_monitor
-    // // di
-    // DriverInterface::Input di_in{
-    //     .di_cmd = out.di_cmd,
-    //     .brake_pedal_pos = brake_pedal_front.Update(),
-    // };
-    // auto di_out = di.Update(di_in, time_ms);
-    // // vd
-    // // mi
-    // gov_in = {.di_sts = di_out.di_sts};
-}
-
-/***************************************************************
     RTOS
 ***************************************************************/
 extern "C" {
@@ -104,6 +63,97 @@ extern uint32_t GetTickCount();
 extern void InitializeKernel();
 extern void StartKernel();
 }  // namespace os
+
+/***************************************************************
+    Control System
+***************************************************************/
+
+auto pedal_to_torque = LinearMap<float, float>{1, 0};
+
+using MotorIface = MotorInterface<RxAMK0_ActualValues1, RxAMK1_ActualValues1,
+                                  TxAMK0_SetPoints1, TxAMK0_SetPoints1>;
+MotorIface mi;
+BatteryMonitor bm;
+Governor gov;
+DriverInterface di;
+VehicleDynamics vd{pedal_to_torque};
+
+// Global Governer input defined
+Governor::Input gov_in{};
+
+void UpdateControls() {
+    // NOTE #1: For defining inputs, I commented out inputs where I didn't know what to set it to
+    // NOTE #2: Some binding values like brake_pedal_front and accel_pedal are defined above, I used those and used Update(), idk if thats the correct thing to do or not
+    // NOTE #3: There are some warnings for type conversions, I don't know if those are mistakes from me or some conversions from simulink to c++ were wrong
+
+    // Capture time to use in Update call for each block
+    int time_ms = os::GetTickCount();
+
+    // Governer update
+    // Question: do I just use the default values for the gov input at first, or do I set some value?
+    Governor::Output gov_out = gov.Update(gov_in, time_ms);
+
+    // Battery Monitor update
+    BatteryMonitor::Input bm_in = {
+        .cmd = gov_out.bm_cmd
+        // .precharge_contactor_states = ,
+        // .hv_pos_contactor_states = ,
+        // .hv_neg_contactor_states = ,
+        // .hvil_status = ,
+        // .pack_soc = 
+    };
+    BatteryMonitor::Output bm_out = bm.Update(bm_in, time_ms);
+
+    // Driver Interface update
+    DriverInterface::Input di_in = {
+        .di_cmd = gov_out.di_cmd,
+        .brake_pedal_pos = brake_pedal_front.Update(),
+        // .driver_button = ,
+        .accel_pedal_pos1 = accel_pedal_1.Update(),
+        .accel_pedal_pos2 = accel_pedal_2.Update(),
+        .steering_angle = bindings::steering_angle_sensor.ReadVoltage()
+    };
+    DriverInterface::Output di_out = di.Update(di_in, time_ms);
+
+    // Vehicle Dynamics update
+    VehicleDynamics::Input vd_in = {
+        .driver_torque_request = di_out.driver_torque_req,
+        .brake_pedal_postion = di_out.brake_pedal_position,
+        .steering_angle = di_out.steering_angle,
+        // These are all connected to GND in simulink, assuming that means set to 0
+        .wheel_speed_lr = 0,
+        .wheel_speed_rr = 0,
+        .wheel_speed_lf = 0,
+        .wheel_speed_rf = 0,
+        .tv_enable = 0 
+    };
+    VehicleDynamics::Output vd_out = vd.update(vd_in, time_ms); // update should be capitalized
+
+    // Motor Interface Update
+    MotorIface::Input mi_in = {
+        .cmd = gov_out.mi_cmd,
+        .left_actual1 = pt_can_bus.GetRxAMK0_ActualValues1().value(),
+        .right_actual1 = pt_can_bus.GetRxAMK1_ActualValues1().value(),
+        .left_motor_input = {
+            .speed_request = vd_out.left_motor_speed_request,
+            .torque_limit_positive = vd_out.lm_torque_limit_positive,
+            .torque_limit_negative = vd_out.lm_torque_limit_negative
+        },
+        .right_motor_input = {
+            .speed_request = vd_out.right_motor_speed_request,
+            .torque_limit_positive = vd_out.rm_torque_limit_positive,
+            .torque_limit_negative = vd_out.rm_torque_limit_negative
+        }
+    };
+    MotorIface::Output mi_out = mi.Update(mi_in, time_ms);
+
+    // Craft global Governer input to use for next UpdateControls call
+    gov_in = {
+        .bm_sts = bm_out.status,
+        .mi_sts = mi_out.status,
+        .di_sts = di_out.di_sts
+    };
+}
 
 /***************************************************************
     RTOS Tasks
