be82636876dce75d0493683d27c1ec91bed4d0f7
BlakeFreer <freerb@mcmaster.ca>
2025-02-28 23:20:35 -0500

    STM32 Dashboard cleanup
    - Replace MX_FMC commands with the BSP SDRAM commands.
    - Remove unused source files from CMakeLists

--- firmware/projects/lv_port_stm32f469_disco/platforms/stm32/bindings.cc
+++ firmware/projects/lv_port_stm32f469_disco/platforms/stm32/bindings.cc
@@ -6,9 +6,7 @@
 #include "dma.h"
 #include "dma2d.h"
 #include "dsihost.h"
-#include "fmc.h"
 #include "gpio.h"
-// #include "i2c.h"
 #include "ltdc.h"
 #include "usart.h"
 
@@ -17,28 +15,13 @@
 #include "mcal/stm32f4/periph/gpio.hpp"
 
 extern "C" {
+#include "BSP/STM32469I-Discovery/stm32469i_discovery_sdram.h"
 #include "screen_driver.h"
-// #include "touch_sensor_driver.h"
 extern void SystemClock_Config(void);
 }
 
-#define SDRAM_MODEREG_BURST_LENGTH_1 ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_LENGTH_2 ((uint16_t)0x0001)
-#define SDRAM_MODEREG_BURST_LENGTH_4 ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8 ((uint16_t)0x0004)
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED ((uint16_t)0x0008)
-#define SDRAM_MODEREG_CAS_LATENCY_2 ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3 ((uint16_t)0x0030)
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE ((uint16_t)0x0200)
-
-/**
- * @brief  SDRAM refresh counter (90 MHz SD clock)
- *
- * @brief  defines copied from the STM32F469I-Disco Board support package
- */
+/// @brief  SDRAM refresh counter (90 MHz SD clock)
+/// @brief  defines copied from the STM32F469I-Disco Board support package
 #define REFRESH_COUNT ((uint32_t)0x0569)
 #define SDRAM_TIMEOUT ((uint32_t)0xFFFF)
 
@@ -48,7 +31,6 @@ using namespace stm32f4::periph;
 CanBase veh_can_base{&hcan1};
 DigitalInput button_scroll{BUTTON_SCROLL_GPIO_Port, BUTTON_SCROLL_Pin};
 DigitalInput button_select{BUTTON_SELECT_GPIO_Port, BUTTON_SELECT_Pin};
-// DigitalOutput debug{DEBUG_GPIO_Port, DEBUG_Pin};
 
 }  // namespace mcal
 
@@ -57,89 +39,26 @@ namespace bindings {
 shared::periph::CanBase& veh_can_base = mcal::veh_can_base;
 shared::periph::DigitalInput& button_scroll = mcal::button_scroll;
 shared::periph::DigitalInput& button_select = mcal::button_select;
-// shared::periph::DigitalOutput& debug = mcal::debug;
 
 void Initialize() {
     HAL_Init();
-
-    /* USER CODE BEGIN Init */
-
-    /* USER CODE END Init */
-
-    /* Configure the system clock */
     SystemClock_Config();
-
-    /* USER CODE BEGIN SysInit */
-
-    /* USER CODE END SysInit */
 
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
     MX_DMA_Init();
     MX_DMA2D_Init();
     MX_DSIHOST_DSI_Init();
-    MX_FMC_Init();
     MX_CAN1_Init();
 
-    /* USER CODE BEGIN FMC_Init 2 */
-    static FMC_SDRAM_CommandTypeDef Command;
+    // Don't call MX_FMC_Init() - it is replaced by BSP_SDRAM...()
+    BSP_SDRAM_Init();
+    BSP_SDRAM_Initialization_sequence(REFRESH_COUNT);
 
-    __IO uint32_t tmpmrd = 0;
-
-    /* Step 1: Configure a clock configuration enable command */
-    Command.CommandMode = FMC_SDRAM_CMD_CLK_ENABLE;
-    Command.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
-    Command.AutoRefreshNumber = 1;
-    Command.ModeRegisterDefinition = 0;
-
-    /* Send the command */
-    HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-    /* Step 2: Insert 100 us minimum delay */
-    /* Inserted delay is equal to 1 ms due to systick time base unit (ms) */
-    HAL_Delay(1);
-
-    /* Step 3: Configure a PALL (precharge all) command */
-    Command.CommandMode = FMC_SDRAM_CMD_PALL;
-    Command.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
-    Command.AutoRefreshNumber = 1;
-    Command.ModeRegisterDefinition = 0;
-
-    /* Send the command */
-    HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-    /* Step 4: Configure an Auto Refresh command */
-    Command.CommandMode = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
-    Command.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
-    Command.AutoRefreshNumber = 8;
-    Command.ModeRegisterDefinition = 0;
-
-    /* Send the command */
-    HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-    /* Step 5: Program the external memory mode register */
-    tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1 |
-             SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL | SDRAM_MODEREG_CAS_LATENCY_3 |
-             SDRAM_MODEREG_OPERATING_MODE_STANDARD |
-             SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
-
-    Command.CommandMode = FMC_SDRAM_CMD_LOAD_MODE;
-    Command.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
-    Command.AutoRefreshNumber = 1;
-    Command.ModeRegisterDefinition = tmpmrd;
-
-    /* Send the command */
-    HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-    /* Step 6: Set the refresh rate counter */
-    /* Set the device refresh rate */
-    HAL_SDRAM_ProgramRefreshRate(&hsdram1, REFRESH_COUNT);
-    // MX_I2C1_Init();
     MX_LTDC_Init();
     MX_USART3_UART_Init();
 
     screen_driver_init();
-    // touch_sensor_driver_init();
 
     mcal::veh_can_base.Setup();
 }
