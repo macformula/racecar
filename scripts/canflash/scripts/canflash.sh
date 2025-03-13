c51a8cb71b754e436d0b816c841b7c48c561920e
Aiden Robinson <aiden.robinson1@gmail.com>
2025-03-13 16:31:46 -0400

    changing GPIO to 5

--- scripts/canflash/scripts/canflash.sh
+++ scripts/canflash/scripts/canflash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# connect GPIO pin from RPi GPIO 21 to STM CN11 Pin 7
+# connect GPIO pin from RPi GPIO 5 to STM CN11 Pin 7
 #can prog is assumed to be installed with python-can 3.3.2
 #can hat must be attached and can bus 0 is wired
-# to run: sudo bash OTA.sh your_binary.bin
+# to run: sudo bash OTA.sh your_binary.bin 
 
 
 
@@ -19,9 +19,9 @@ BINARY_FILE=$1  # Store the first command line argument
 
 cd /
 cd sys/class/gpio/
-echo 21 > export
-echo out > gpio21/direction #gain control of GPIO pin
-echo 1 > gpio21/value # GPIO signal high
+echo 5 > export
+echo out > gpio5/direction #gain control of GPIO pin
+echo 1 > gpio5/value # GPIO signal high
 sleep 1
 
 ##can prog 
@@ -32,8 +32,8 @@ sudo ip link set can0 up
 canprog -n can0 -f bin stm32 write "$BINARY_FILE" -a 0x08000000 -ve #erases memory on STM and writes binary
 
 sleep 1
-echo 0 > gpio21/value #GPIO signal low
-echo 21 > unexport #release control of GPIO pin
+echo 0 > gpio5/value #GPIO signal low
+echo 5 > unexport #release control of GPIO pin
 
 
 
