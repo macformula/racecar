aaa231455173ec30e985f0c1444ae6988db246d4
Aiden Robinson <aiden.robinson1@gmail.com>
2025-03-13 16:04:19 -0400

    adding can flash bash script

---
+++ scripts/canflash/scripts/canflash.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# connect GPIO pin from RPi GPIO 21 to STM CN11 Pin 7
+#can prog is assumed to be installed with python-can 3.3.2
+#can hat must be attached and can bus 0 is wired
+# to run: sudo bash OTA.sh your_binary.bin
+
+
+
+# Check if a binary file was provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Error: No binary file specified"
+    echo "Usage: $0 <binary_file.bin>"
+    exit 1
+fi
+
+BINARY_FILE=$1  # Store the first command line argument
+
+
+cd /
+cd sys/class/gpio/
+echo 21 > export
+echo out > gpio21/direction #gain control of GPIO pin
+echo 1 > gpio21/value # GPIO signal high
+sleep 1
+
+##can prog 
+sudo ip link set can0 down
+sudo ip link set can0 up type can bitrate 125000
+sudo ip link set can0 up
+
+canprog -n can0 -f bin stm32 write "$BINARY_FILE" -a 0x08000000 -ve #erases memory on STM and writes binary
+
+sleep 1
+echo 0 > gpio21/value #GPIO signal low
+echo 21 > unexport #release control of GPIO pin
+
+
+
+
+
+
+
