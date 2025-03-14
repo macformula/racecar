7c46371745d78f0cf7b3666d6ed8e8ab999ccab1
Cameron Beneteau <cameron.beneteau@gmail.com>
2025-03-13 23:38:41 -0400

    Update scripts and flash logic

---
+++ scripts/canflash/constants.py
@@ -0,0 +1,6 @@
+UPLOAD_DIR = "uploads"
+ECU_CONFIG = {
+    "FrontController": {"gpio_pin": 8, "can_message_id": 0},
+    "LVController": {"gpio_pin": 6, "can_message_id": 1},
+    "TMS": {"gpio_pin": 0, "can_message_id": 2},
+}
