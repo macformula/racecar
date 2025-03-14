0d2c34eecd484887bac64e53747d53add2331afc
Cameron Beneteau <cameron.beneteau@gmail.com>
2025-03-13 23:52:26 -0400

    Clean up flash logic

--- scripts/canflash/flash_logic.py
+++ scripts/canflash/flash_logic.py
@@ -5,19 +5,13 @@ from datetime import datetime
 from constants import ECU_CONFIG, UPLOAD_DIR
 
 
-def generate_ecu_directory(ecu: str) -> str:
-    # timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+def save_uploaded_file(ecu: str, file_name: str, file_content: bytes) -> str:
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     ecu_dir = f"{ecu.lower()}-{timestamp}"
     full_path = os.path.join(UPLOAD_DIR, ecu_dir)
 
-    os.makedirs(full_path, exist_ok=True)  # Create the directory if it doesn't exist
-    return full_path
-
-
-def save_uploaded_file(ecu: str, file_name: str, file_content: bytes) -> str:
-    ecu_dir = generate_ecu_directory(ecu)
-    file_path = os.path.join(ecu_dir, file_name)
+    os.makedirs(full_path, exist_ok=True)
+    file_path = os.path.join(full_path, file_name)
 
     with open(file_path, "wb") as f:
         f.write(file_content)
@@ -26,11 +20,11 @@ def save_uploaded_file(ecu: str, file_name: str, file_content: bytes) -> str:
 
 
 def flash_file(ecu: str, file_path: str):
-    if not ecu or not file_path:
-        raise ValueError("ECU and file must be selected.")
+    gpio_pin = str(ECU_CONFIG[ecu]["gpio_pin"])
+    can_message_id = str(ECU_CONFIG[ecu]["can_message_id"])
 
-    gpio_pin = ECU_CONFIG[ecu]["gpio_pin"]
-    can_message_id = ECU_CONFIG[ecu]["can_message_id"]
-
-    # Flash the file
-    subprocess.run(["bash", "./canflash.sh", file_path, str(gpio_pin), str(can_message_id)], check=True)
+    try:
+        subprocess.run(["bash", "./scripts/canflash.sh", file_path, gpio_pin, can_message_id])
+        return None
+    except Exception as e:
+        return e
