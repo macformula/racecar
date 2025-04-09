7fe515f432ed01ef42aeabfa940f616d157da8d0
BlakeFreer <freerb@mcmaster.ca>
2025-04-09 19:53:30 -0400

    Fix boolean parsing error and simplify generated CAN.
    Error was caused by a right bitshift on a `static_cast<bool>` which
    was knocking off the value since the cast already sends the bit to the
    right. Fixes this by parsing boolean differently than integers.
    
    Removes the `unpack_left/right_shift()` functions and puts the behaviour
    directly inside the generated code.
    
    Only applies the scale and offset if the value uses at least one of them.
    This simplifies most signals since they have Scale=1 and Offset=0 so no
    action is necessary.

--- firmware/projects/Demo/TestCangen/test/test_rx.cc
+++ firmware/projects/Demo/TestCangen/test/test_rx.cc
@@ -30,72 +30,116 @@ protected:
     TestBus bus;
 };
 
-TEST_F(Bus, RxNonAligned) {
-    {
-        uint8_t data[8]{0, 0x00, 0, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{258, true, 4, data});
-        auto msg = bus.PopRxStatusNonAligned();
+struct StatusTestCase {
+    uint8_t data[8];
+    bool expected_imd;
+    bool expected_bms;
+};
+
+class RxNonAlignedStatus : public Bus,
+                           public testing::WithParamInterface<StatusTestCase> {
+};
+
+TEST_P(RxNonAlignedStatus, HandlesCases) {
+    auto [data, expected_imd, expected_bms] = GetParam();
+
+    base.AddToBus(RawMessage{258, false, 2, data});
+    auto msg = bus.PopRxStatusNonAligned();
+
+    ASSERT_TRUE(msg.has_value());
+    EXPECT_EQ(msg->IMDLed(), expected_imd);
+    EXPECT_EQ(msg->BMSLed(), expected_bms);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    RxNonAlignedCases, RxNonAlignedStatus,
+    testing::Values(StatusTestCase{{0, 0x00}, false, false},
+                    StatusTestCase{{0, 0x10}, false, true},
+                    StatusTestCase{{0, 0x08}, true, false},
+                    StatusTestCase{{0, 0x18}, true, true}));
+
+class RxAlignedTest : public Bus,
+                      public testing::WithParamInterface<StatusTestCase> {};
+
+TEST_P(RxAlignedTest, HandleCases) {
+    auto [data, expected_imd, expected_bms] = GetParam();
+
+    base.AddToBus(RawMessage{259, false, 4, data});
+    auto msg = bus.PopRxStatusAligned();
+
+    ASSERT_TRUE(msg.has_value());
+    ASSERT_EQ(msg->IMDLed(), expected_imd);
+    ASSERT_EQ(msg->BMSLed(), expected_bms);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    RxAlignedCases, RxAlignedTest,
+    testing::Values(StatusTestCase{{0, 0, 0, 0}, false, false},
+                    StatusTestCase{{0, 0, 1, 0}, true, false},
+                    StatusTestCase{{0, 0, 0, 1}, false, true},
+                    StatusTestCase{{0, 0, 1, 1}, true, true}));
+
+TEST_F(Bus, LongField) {
+    uint8_t data[8] = {0x08, 0xA2, 0xB3, 0x11, 0x02, 0x03, 0x44};
+    {  // Little Endian
+        base.AddToBus(RawMessage{260, false, 7, data});
+        auto msg = bus.PopRxLongFields();
+
         ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), false);
-        ASSERT_EQ(msg->BMSLed(), false);
+        EXPECT_EQ(msg->byte(), 0x08);
+        EXPECT_EQ(msg->halfword(), 0xB3A2);
+        EXPECT_EQ(msg->word(), 0x44030211);
     }
-    {
-        uint8_t data[8]{0, 0x10, 0, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{258, true, 4, data});
-        auto msg = bus.PopRxStatusNonAligned();
+    {  // Big Endian
+        base.AddToBus(RawMessage{261, false, 7, data});
+        auto msg = bus.PopRxLongFieldsBigEndian();
+
         ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), false);
-        ASSERT_EQ(msg->BMSLed(), true);
-    }
-    {
-        uint8_t data[8]{0, 0x04, 0, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{258, true, 4, data});
-        auto msg = bus.PopRxStatusNonAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), true);
-        ASSERT_EQ(msg->BMSLed(), false);
-    }
-    {
-        uint8_t data[8]{0, 0x14, 0, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{258, true, 4, data});
-        auto msg = bus.PopRxStatusNonAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), true);
-        ASSERT_EQ(msg->BMSLed(), true);
+        EXPECT_EQ(msg->byte(), 0x08);
+        EXPECT_EQ(msg->halfword(), 0xA2B3);
+        EXPECT_EQ(msg->word(), 0x11020344);
     }
 }
 
-TEST_F(Bus, RxAligned) {
-    {
-        uint8_t data[8]{0, 0, 0, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{259, true, 4, data});
-        auto msg = bus.PopRxStatusAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), false);
-        ASSERT_EQ(msg->BMSLed(), false);
-    }
-    {
-        uint8_t data[8]{0, 0, 0, 1, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{259, true, 4, data});
-        auto msg = bus.PopRxStatusAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), false);
-        ASSERT_EQ(msg->BMSLed(), true);
-    }
-    {
-        uint8_t data[8]{0, 0, 1, 0, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{259, true, 4, data});
-        auto msg = bus.PopRxStatusAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), true);
-        ASSERT_EQ(msg->BMSLed(), false);
-    }
-    {
-        uint8_t data[8]{0, 0, 1, 1, 0, 0, 0, 0};
-        base.AddToBus(RawMessage{259, true, 4, data});
-        auto msg = bus.PopRxStatusAligned();
-        ASSERT_TRUE(msg.has_value());
-        ASSERT_EQ(msg->IMDLed(), true);
-        ASSERT_EQ(msg->BMSLed(), true);
-    }
+struct NonAlignedCase {
+    uint8_t data[6];
+    uint16_t exp1;
+    uint8_t exp2;
+    uint32_t exp3;
+    bool exp4;
+    uint8_t exp5;
+};
+
+class RxNonAlignedSignalTest
+    : public Bus,
+      public testing::WithParamInterface<NonAlignedCase> {};
+
+TEST_P(RxNonAlignedSignalTest, HandleCases) {
+    auto p = GetParam();
+
+    base.AddToBus(RawMessage{262, false, 6, p.data});
+    auto msg = bus.PopRxNonAligned();
+
+    ASSERT_TRUE(msg.has_value());
+    EXPECT_EQ(msg->sig1(), p.exp1);
+    EXPECT_EQ(msg->sig2(), p.exp2);
+    EXPECT_EQ(msg->sig3(), p.exp3);
+    EXPECT_EQ(msg->sig4(), p.exp4);
+    EXPECT_EQ(msg->sig5(), p.exp5);
 }
+
+INSTANTIATE_TEST_CASE_P(
+    RxNonAlignedSignalCases, RxNonAlignedSignalTest,
+    testing::Values(NonAlignedCase{{0xf3, 0x25 | 0x80, 0x16 | 0x50, 0xa3, 0x94,
+                                    0x0e | 0x10 | 0xA0},
+                                   0x25f3,
+                                   0x1680 >> 6,
+                                   0xe94a350 >> 5,
+                                   true,
+                                   5},
+                    NonAlignedCase{{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+                                   0x3fff,
+                                   0x7f,
+                                   0x7fffff,
+                                   true,
+                                   0x7}));
