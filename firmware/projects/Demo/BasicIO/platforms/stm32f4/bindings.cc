1a6c07c0df4598f86511b03888ebee92597fd5a5
Tegh25 <teghveera@gmail.com>
2025-04-14 22:19:45 -0500

    Added stm32f4 platform for Demo/BasicIO project
    Issue #383. Stm32f4 platform uses the exact same pins, configuration,
    and bindings as the stm32f767 platform.

---
+++ firmware/projects/Demo/BasicIO/platforms/stm32f4/bindings.cc
@@ -0,0 +1,38 @@
+/// @author Blake Freer
+/// @date 2023-12-25
+
+// cubemx files
+#include "gpio.h"
+#include "main.h"
+
+// fw includes
+#include "../../bindings.hpp"
+#include "mcal/stm32f767/periph/gpio.hpp"
+#include "shared/periph/gpio.hpp"
+
+extern "C" {
+/**
+ * This requires extern since it is not declared in a header, only defined
+ * in cubemx/../main.c
+ */
+void SystemClock_Config();
+}
+
+namespace mcal {
+using namespace stm32f767::periph;
+
+DigitalInput button_di{ButtonPin_GPIO_Port, ButtonPin_Pin};
+DigitalOutput indicator_do{LedPin_GPIO_Port, LedPin_Pin};
+}  // namespace mcal
+
+namespace bindings {
+shared::periph::DigitalInput& button_di = mcal::button_di;
+shared::periph::DigitalOutput& indicator_do = mcal::indicator_do;
+
+void Initialize() {
+    SystemClock_Config();
+    HAL_Init();
+    MX_GPIO_Init();
+}
+
+}  // namespace bindings
