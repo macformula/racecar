48a575ccb56fbb4f7017d52a00db85b467fb5b1e
Tegh25 <teghveera@gmail.com>
2025-04-15 20:39:16 -0500

    Added stm32f4 platform on Demo/CAN/Send project
    Issue #383. Stm32f4 platform uses the exact same gpio pin as the f7, but
    uses CAN1 instead of CAN3 since the latter is not available on the f4.
    Added compile time definitions in respective mcal_conf.cmake files
    for each stm32 platform. This is so clangd knows which platform to
    parse.
    The stm32f4 platform for Demo/CAN/Send has been setup the exact same way
    as the f4 for Demo/CAN/Receive.

---
+++ firmware/projects/Demo/CAN/Send/platforms/stm32f4/bindings.cc
@@ -0,0 +1,47 @@
+/// @author Samuel Parent
+/// @date 2024-04-16
+
+// cubemx files
+#include "can.h"
+#include "gpio.h"
+#include "main.h"
+
+// fw includes
+#include "../../bindings.hpp"
+#include "mcal/stm32f/periph/can.hpp"
+#include "mcal/stm32f/periph/gpio.hpp"
+#include "shared/periph/can.hpp"
+#include "shared/periph/gpio.hpp"
+
+extern "C" {
+/**
+ * This requires extern since it is not declared in a header, only defined
+ * in cubemx/../main.c
+ */
+void SystemClock_Config();
+}
+
+namespace mcal {
+using namespace stm32f::periph;
+
+CanBase demo_can_base{&hcan1};
+DigitalInput button{ButtonPin_GPIO_Port, ButtonPin_Pin};
+}  // namespace mcal
+
+namespace bindings {
+shared::periph::CanBase& demo_can_base = mcal::demo_can_base;
+shared::periph::DigitalInput& button = mcal::button;
+
+void Initialize() {
+    SystemClock_Config();
+    HAL_Init();
+    MX_GPIO_Init();
+    MX_CAN1_Init();
+    mcal::demo_can_base.Setup();
+}
+
+void TickBlocking(uint32_t ticks) {
+    HAL_Delay(ticks);
+}
+
+}  // namespace bindings
