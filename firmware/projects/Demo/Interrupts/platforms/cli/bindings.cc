9b8853d7ccb8d829e30b11e217199ca893387052
BlakeFreer <freerb@mcmaster.ca>
2025-05-10 16:54:10 -0400

    Separate exit instructions for unix and windows

--- firmware/projects/Demo/Interrupts/platforms/cli/bindings.cc
+++ firmware/projects/Demo/Interrupts/platforms/cli/bindings.cc
@@ -18,14 +18,30 @@ namespace bindings {
 
 shared::periph::DigitalOutput& indicator = mcal::indicator;
 
+static void InterruptHandler(int) {
+    AppInterruptHandler(0);
+
+    // We must re-register the SIGINT handler each time. If this wasn't the
+    // case, then this enclosing function would be unnecessary since the
+    // signal() call in Initialize() could directly register AppInterruptHandler
+    signal(SIGINT, InterruptHandler);
+}
+
 void Initialize() {
     std::cout << "Initializing the CLI..." << std::endl;
     std::cout << "Press ctrl+c to simulate the interrupt" << std::endl;
+
+#ifdef __unix__
     std::cout << "Press ctrl+\\ to exit.\n" << std::endl;
+#else
+    std::cout << "To end the program: " << std::endl;
+    std::cout << "  Command Prompt: ctrl+fn+b" << std::endl;
+    std::cout << "  Git Bash: Close the terminal session\n" << std::endl;
+#endif
 
     // register the signal handler for SIGINT (ctrl+c)
     // this is used to simulate the interrupt on the CLI
-    signal(SIGINT, AppInterruptHandler);
+    signal(SIGINT, InterruptHandler);
 }
 
 }  // namespace bindings
