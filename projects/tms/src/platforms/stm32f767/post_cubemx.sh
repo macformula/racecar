9389ab0aaf6fc5d48af5a8c55fa2249b76647376
Blake Freer <bkfreer10@gmail.com>
2025-09-23 07:51:02 -0400

    remove clang-format from postgen script

--- projects/tms/src/platforms/stm32f767/post_cubemx.sh
+++ projects/tms/src/platforms/stm32f767/post_cubemx.sh
@@ -1,8 +1,5 @@
-# Remove linker script for RAM debugging. We run the program from FLASH.
-rm -f *_RAM.ld
-
 # In-Place sort the IOC file. This makes `git diff` clearer.
 IOC=$(find *.ioc) && sort $IOC --version-sort --output=$IOC 
 
-# Format generated code to comply with our style.
-clang-format --style=file -i -- **/*.c **/*.h
+# Remove linker script for RAM debugging. We run the program from FLASH.
+rm -f *_RAM.ld
