6f3067fce8f717942b8d9df14b48ad67f65c45de
Blake Freer <bkfreer10@gmail.com>
2025-10-08 15:39:53 -0400

    SPI master starter code for #515

---
+++ lib/periph/periph/spi.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdlib>
+
+namespace macfe::periph {
+
+class SpiMaster {
+public:
+    /// Transmit `length` bytes from `tx_data`
+    virtual void Transmit(uint8_t* tx_data, size_t length);
+
+    /// Receive `length` bytes and place them into `rx_data`
+    virtual void Receive(uint8_t* rx_data, size_t length);
+
+    /// Transmit `length` bytes from `tx_data` and receive `length` bytes,
+    /// placing them into `rx_data`
+    virtual void TransmitReceive(uint8_t* tx_data, uint8_t* rx_data,
+                                 size_t length);
+};
+
+}  // namespace macfe::periph
