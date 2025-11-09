175034da7f0b69a77a57cd204a926bc5e4039614
SpeakBoy <nevinspeakman@outlook.com>
2025-11-08 19:14:41 -0500

    Check for changes only in certain directories

--- scripts/static_analysis/static_analysis.sh
+++ scripts/static_analysis/static_analysis.sh
@@ -5,7 +5,11 @@ git ls-files -z 'projects/**/platformio.ini' | while IFS= read -r -d '' ini; do
     echo "==== Performing Static Code Analysis on $dir"
     echo "============================================================"
 
-    if ! git diff --quiet --staged --exit-code "$dir"; then
+    SUBDIRS=("$dir/src" "$dir/include")
+    EXCLUDE_PREFIX="stm*"
+
+    # Check for changes in $dir/src and $dir/include, excluding files within $dir/src/platforms/stm*
+    if git diff --name-only --staged -- "${SUBDIRS[@]}" ":!$dir/src/platforms/$EXCLUDE_PREFIX" | grep -q .; then
         echo "Changes detected in $dir → running pio check"
         pio check --project-dir "$dir" --fail-on-defect=high || exit 1
     else
