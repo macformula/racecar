39e8f2ef1a318c4e73fb685af45c867ad0356f8c
SpeakBoy <nevinspeakman@outlook.com>
2025-11-08 23:04:46 -0500

    Reduce number of pre-commit checks

--- scripts/static_analysis/static_analysis.sh
+++ scripts/static_analysis/static_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git ls-files -z 'projects/**/platformio.ini' | while IFS= read -r -d '' ini; do
+git ls-files -z 'projects/**/platformio.ini' | grep -zv 'projects/demo/' | while IFS= read -r -d '' ini; do
     dir=$(dirname "$ini")
     echo "============================================================"
     echo "==== Performing Static Code Analysis on $dir"
@@ -11,7 +11,7 @@ git ls-files -z 'projects/**/platformio.ini' | while IFS= read -r -d '' ini; do
     # Check for changes in $dir/src and $dir/include, excluding files within $dir/src/platforms/stm*
     if git diff --name-only --staged -- "${SUBDIRS[@]}" ":!$dir/src/platforms/$EXCLUDE_PREFIX" | grep -q .; then
         echo "Changes detected in $dir → running pio check"
-        pio check --project-dir "$dir" --fail-on-defect=high || exit 1
+        pio check --project-dir "$dir" --flags "--checks=bugprone-*,clang-analyzer-*,google-*,performance-*" --fail-on-defect=high || exit 1
     else
         echo "No changes in $dir → skipping"
     fi
