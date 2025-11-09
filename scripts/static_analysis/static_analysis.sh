7bb0b753ba6102e7702d2fb549feaa7a26a7a5e8
Nevin Speakman <150295918+SpeakBoy@users.noreply.github.com>
2025-11-09 14:46:59 -0500

    Improve static code analysis time for PRs (#542)
    * Check for changes only in certain directories
    
    * Fix static code analysis workflow
    
    * Fix build-test-projects.yml
    
    * Add test files for PR analysis
    
    * Remove test files from repo
    
    * Reduce number of pre-commit checks
    
    * Reduce number of PR analysis checks

--- scripts/static_analysis/static_analysis.sh
+++ scripts/static_analysis/static_analysis.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-git ls-files -z 'projects/**/platformio.ini' | while IFS= read -r -d '' ini; do
+git ls-files -z 'projects/**/platformio.ini' | grep -zv 'projects/demo/' | while IFS= read -r -d '' ini; do
     dir=$(dirname "$ini")
     echo "============================================================"
     echo "==== Performing Static Code Analysis on $dir"
     echo "============================================================"
 
-    if ! git diff --quiet --staged --exit-code "$dir"; then
+    SUBDIRS=("$dir/src" "$dir/include")
+    EXCLUDE_PREFIX="stm*"
+
+    # Check for changes in $dir/src and $dir/include, excluding files within $dir/src/platforms/stm*
+    if git diff --name-only --staged -- "${SUBDIRS[@]}" ":!$dir/src/platforms/$EXCLUDE_PREFIX" | grep -q .; then
         echo "Changes detected in $dir → running pio check"
-        pio check --project-dir "$dir" --fail-on-defect=high || exit 1
+        pio check --project-dir "$dir" --flags "--checks=bugprone-*,clang-analyzer-*,google-*,performance-*" --fail-on-defect=high || exit 1
     else
         echo "No changes in $dir → skipping"
     fi
