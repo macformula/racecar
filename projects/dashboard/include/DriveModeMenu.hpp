d14f9f3148d2f24f02288482a0e3b2f0f5ac013b
Noah Jaye <noahjaye@gmail.com>
2026-05-30 13:47:57 -0400

    Re-add hpp

--- projects/dashboard/include/DriveModeMenu.hpp
+++ projects/dashboard/include/DriveModeMenu.hpp
@@ -20,12 +20,57 @@ private:
 class Battery {
 public:
     void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
-    void SetPercent(float volt);
+    void SetPercent(float soc);
 
 private:
-    const int kVoltMax = 600.f;
-    const int kVoltMin = 250.f;  // display this as "empty"
+    const int kVoltMax = 600;
+    const int kVoltMin = 250;
 
+    lv_obj_t* label;
+};
+
+class BatteryTemps {
+public:
+    void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
+    void SetTemps(float min_temp, float max_temp);
+
+private:
+    lv_obj_t* label;
+};
+
+class MotorInverterTemps {
+public:
+    void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
+    void SetTemps(float motor_temp, float inverter_temp);
+
+private:
+    lv_obj_t* label;
+};
+
+class HVBatteryVoltageCurrent {
+public:
+    void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
+    void SetValues(float voltage, float current);
+
+private:
+    lv_obj_t* label;
+};
+
+class LVBatteryVoltageCurrent {
+public:
+    void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
+    void SetValues(float voltage, float current);
+
+private:
+    lv_obj_t* label;
+};
+
+class FCStatusMessage {
+public:
+    void Draw(lv_obj_t* parent, lv_align_t align, lv_coord_t x, lv_coord_t y);
+    void SetStatus(const char* status);
+
+private:
     lv_obj_t* label;
 };
 
@@ -37,10 +82,14 @@ public:
     void Update() override;
 
 private:
-    Battery battery_;
     Speedometer speedometer_;
+    Battery battery_;
+    BatteryTemps battery_temps_;
+    MotorInverterTemps motor_inverter_temps_;
+    HVBatteryVoltageCurrent hv_batt_vc_;
+    LVBatteryVoltageCurrent lv_batt_vc_;
+    FCStatusMessage fc_status_;
 
-    // Demo: cycling warnings
     uint32_t last_warning_time_;
     etl::vector<int, 10> active_warning_ids_;
     int warning_cycle_index_;
