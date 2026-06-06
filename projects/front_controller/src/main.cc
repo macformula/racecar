84f8336eaf02a9527b6db695492f280cf2f6cf85
Noah Jaye <99850420+noahjaye@users.noreply.github.com>
2026-06-05 21:01:36 -0400

    FC Rev4 changes (#569)
    * Update fc pinout
    
    * pinouts fc new rev
    
    * new hsd folder
    
    * add to alerts, new signal name for overcurrent
    
    * fixed include
    
    * fix hsd error
    
    * add veh can to hsd update
    
    * fix return
    
    * hsd
    
    * fix bindings
    
    * fix another build error
    
    * adding mcal bindings
    
    * adding mcal bindings
    
    * add thresholds, and mappign
    
    * .
    
    ---------
    
    Co-authored-by: ManushPatell <manush.patelll@gmail.com>

--- projects/front_controller/src/main.cc
+++ projects/front_controller/src/main.cc
@@ -196,7 +196,6 @@ static void Update_100Hz(void) {
                 // should probably check if motors and DI have shut down
                 new_state = START_DASHBOARD;
             }
-            break;
 
         case ERROR: {
             acc_cmd = accumulator::Command::OFF;
@@ -290,6 +289,7 @@ void task_10hz(void* argument) {
         // CheckCanFlash();  // no CAN flash in 2025. pcb needs an external
         // oscillator
         suspension::task_10hz(veh_can_bus);
+        hsd::Update_10Hz(veh_can_bus);
 
         veh_can_bus.Send(TxFcStatus{
             .counter = tx_counter++,
