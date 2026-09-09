5c2f41e95a14fd1e003572ad3d469609d50087c9
Viho <142705965+HUANGV1@users.noreply.github.com>
2026-09-08 20:32:52 -0400

    Porting IMD and BMS Faults to LvAlerts
    Co-authored-by: Manush Patel <77220084+ManushPatell@users.noreply.github.com>

--- projects/lvcontroller/src/main.cc
+++ projects/lvcontroller/src/main.cc
@@ -245,6 +245,8 @@ void task_10hz(void) {
 
     veh_can.Send(TxLvAlerts{
         .hsd_overcurrent = alerts::Get().hsd_overcurrent,
+        .imd_fault = tssi::GetImdFault(),
+        .bms_fault = tssi::GetBmsFault(),
     });
 
     veh_can.Send(TxLvDcdc{
@@ -258,8 +260,6 @@ void task_10hz(void) {
         .lv_state = fsm::state,
         .motor_controller_state = motor_controller::GetState(),
         .motor_controller_switch_closed = motor_controller::GetSwitchClosed(),
-        .imd_fault = tssi::GetImdFault(),
-        .bms_fault = tssi::GetBmsFault(),
     });
 }
 
